950e6a0073827c8130c2f04f8508c0f6c61df0e4
Robin Tang <rtang.cs@gmail.com>
2022-11-19 12:18:51 -0800

    Checkpoint

--- lib/typing/mongo/bson.go
+++ lib/typing/mongo/bson.go
@@ -1,39 +1,89 @@
 package mongo
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/bsoncodec"
 	"go.mongodb.org/mongo-driver/bson/bsonrw"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"reflect"
+	"strconv"
 	"time"
 )
+
+// JSONEToMap will take JSONE data in bytes, parse all the custom types
+// Then from all the custom types,
+func JSONEToMap(val []byte) (map[string]interface{}, error) {
+	var jsonMap map[string]interface{}
+	var bsonDoc bson.D
+	err := bson.UnmarshalExtJSON(val, false, &bsonDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	bytes, err := bson.MarshalExtJSONWithRegistry(createCustomRegistry().Build(),
+		bsonDoc, false, true)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bytes, &jsonMap)
+	return jsonMap, err
+}
 
 var (
 	tDateTime = reflect.TypeOf(primitive.DateTime(0))
 	tOID      = reflect.TypeOf(primitive.ObjectID{})
+	tBinary   = reflect.TypeOf(primitive.Binary{})
 )
 
-func dateTimeEncodeValue(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
-
-	const jDateFormat = "2006-01-02T15:04:05.999Z"
+func dateTimeEncodeValue(_ bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
 	if !val.IsValid() || val.Type() != tDateTime {
 		return bsoncodec.ValueEncoderError{Name: "DateTimeEncodeValue", Types: []reflect.Type{tDateTime}, Received: val}
 	}
 
-	ints := val.Int()
-	t := time.Unix(0, ints*1000000).UTC()
+	ints, err := strconv.Atoi(fmt.Sprint(val))
+	if err != nil {
+		return err
+	}
 
-	return vw.WriteString(t.Format(jDateFormat))
+	t := time.Unix(0, int64(ints)*1000000).UTC()
+
+	return vw.WriteString(t.Format(typing.ISO8601))
 }
 
-func objectIDEncodeValue(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
-
+func objectIDEncodeValue(_ bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
 	if !val.IsValid() || val.Type() != tOID {
 		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue", Types: []reflect.Type{tOID}, Received: val}
 	}
-	s := val.Interface().(primitive.ObjectID).Hex()
-	return vw.WriteString(s)
+
+	s, isOk := val.Interface().(primitive.ObjectID)
+	if !isOk {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue not objectID", Types: []reflect.Type{tOID}, Received: val}
+	}
+
+	return vw.WriteString(s.Hex())
+}
+
+func binaryEncodeValue(_ bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() || val.Type() != tBinary {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue", Types: []reflect.Type{tBinary}, Received: val}
+	}
+
+	s, isOk := val.Interface().(primitive.Binary)
+	if !isOk {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue not Binary", Types: []reflect.Type{tBinary}, Received: val}
+	}
+
+	parsedUUID, err := uuid.FromBytes(s.Data)
+	if err != nil {
+		return err
+	}
+
+	return vw.WriteString(parsedUUID.String())
 }
 
 func createCustomRegistry() *bsoncodec.RegistryBuilder {
@@ -41,8 +91,10 @@ func createCustomRegistry() *bsoncodec.RegistryBuilder {
 	rb := bsoncodec.NewRegistryBuilder()
 	bsoncodec.DefaultValueEncoders{}.RegisterDefaultEncoders(rb)
 	bsoncodec.DefaultValueDecoders{}.RegisterDefaultDecoders(rb)
-	rb.RegisterEncoder(tDateTime, bsoncodec.ValueEncoderFunc(dateTimeEncodeValue))
-	rb.RegisterEncoder(tOID, bsoncodec.ValueEncoderFunc(objectIDEncodeValue))
+
+	rb.RegisterTypeEncoder(tDateTime, bsoncodec.ValueEncoderFunc(dateTimeEncodeValue))
+	rb.RegisterTypeEncoder(tOID, bsoncodec.ValueEncoderFunc(objectIDEncodeValue))
+	rb.RegisterTypeEncoder(tBinary, bsoncodec.ValueEncoderFunc(binaryEncodeValue))
 	primitiveCodecs.RegisterPrimitiveCodecs(rb)
 	return rb
 }
--- lib/typing/mongo/bson_test.go
+++ lib/typing/mongo/bson_test.go
@@ -1,12 +1,11 @@
 package mongo
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/bson"
 	"testing"
 )
 
+// TestMarshal, every single type is listed here: https://github.com/mongodb/specifications/blob/master/source/extended-json.rst#canonical-extended-json-example
 func TestMarshal(t *testing.T) {
 	bsonData := []byte(`
 {
@@ -22,18 +21,24 @@ func TestMarshal(t *testing.T) {
 	"quantity": 1,
 	"product_id": {
 		"$numberLong": "107"
-	}
+	},
+	"unique_id": {
+		"$binary": "hW5W/8uwQR6FWpiwi4dRQA==",
+		"$type": "04"
+	},
+	"full_name": "Robin Tang"
 }
 `)
-	var bsonDoc bson.D
-	err := bson.UnmarshalExtJSON(bsonData, false, &bsonDoc)
+	result, err := JSONEToMap(bsonData)
 	assert.NoError(t, err)
 
-	bytes, err := bson.MarshalExtJSONWithRegistry(createCustomRegistry().Build(),
-		bsonDoc, false, true)
+	assert.Equal(t, result["_id"], float64(10004))
+	assert.Equal(t, result["order_date"], "2016-02-21T00:00:00+00:00")
+	assert.Equal(t, result["product_id"], float64(107))
+	assert.Equal(t, result["quantity"], float64(1))
+	assert.Equal(t, result["unique_id"], "856e56ff-cbb0-411e-855a-98b08b875140")
+	assert.Equal(t, result["full_name"], "Robin Tang")
 
-	fmt.Println("bytes", string(bytes), "error", err)
-
-	assert.True(t, false)
+	assert.True(t, false, result)
 
 }
