3893f800b15ce704ca032fecaed6fc40bb0d2e78
Robin Tang <rtang.cs@gmail.com>
2022-11-22 08:48:07 -0600

    Test Parse String

---
+++ lib/cdc/util/parser_test.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseStringKey(t *testing.T) {
+	_, _, err := ParseStringKey(nil)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "key is nil")
+
+	pkName, pkVal, err := ParseStringKey([]byte("Struct{id=47}"))
+	assert.Nil(t, err)
+	assert.Equal(t, pkName, "id")
+	assert.Equal(t, pkVal, "47")
+
+	pkName, pkVal, err = ParseStringKey([]byte("Struct{uuid=d4a5bc26-9ae6-4dd4-8894-39cbcd2d526c}"))
+	assert.Nil(t, err)
+	assert.Equal(t, pkName, "uuid")
+	assert.Equal(t, pkVal, "d4a5bc26-9ae6-4dd4-8894-39cbcd2d526c")
+
+	_, _, err = ParseStringKey([]byte("{id="))
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "key length too short")
+
+	_, _, err = ParseStringKey([]byte("Struct{id="))
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "key length incorrect")
+}
