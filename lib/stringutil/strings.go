fa534bcc3e0bd5112958ff1a5abd39ee957e028a
Robin Tang <rtang.cs@gmail.com>
2023-02-24 18:20:27 -0800

    Fix BQ as well

--- lib/stringutil/strings.go
+++ lib/stringutil/strings.go
@@ -1,5 +1,10 @@
 package stringutil
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Reverse(val string) string {
 	var reverseParts []rune
 	valRune := []rune(val)
@@ -8,4 +13,11 @@ func Reverse(val string) string {
 	}
 
 	return string(reverseParts)
+}
+
+func Wrap(colVal interface{}) string {
+	// Escape line breaks, JSON_PARSE does not like it.
+	colVal = strings.ReplaceAll(fmt.Sprint(colVal), `\`, `\\`)
+	// The normal string escape is to do for O'Reilly is O\\'Reilly, but Snowflake escapes via \'
+	return fmt.Sprintf("'%s'", strings.ReplaceAll(fmt.Sprint(colVal), "'", `\'`))
 }
