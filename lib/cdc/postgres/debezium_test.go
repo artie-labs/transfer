a96f2720e0d74579547e37168209f0b2ce032ea2
Robin Tang <rtang.cs@gmail.com>
2023-03-13 15:24:06 -0700

    Fix tests

--- lib/cdc/postgres/debezium_test.go
+++ lib/cdc/postgres/debezium_test.go
@@ -3,7 +3,6 @@ package postgres
 import (
 	"context"
 	"fmt"
-	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/artie-labs/transfer/lib/typing/ext"
@@ -31,117 +30,6 @@ func (p *PostgresTestSuite) TestGetPrimaryKeyUUID() {
 	assert.Equal(p.T(), pkName, "uuid")
 	assert.Equal(p.T(), fmt.Sprint(pkVal), "ca0cefe9-45cf-44fa-a2ab-ec5e7e5522a3")
 	assert.Equal(p.T(), err, nil)
-}
-
-func (p *PostgresTestSuite) TestSource_GetExecutionTime() {
-	source := Source{
-		Connector: "postgresql",
-		TsMs:      1665458364942, // Tue Oct 11 2022 03:19:24
-	}
-
-	schemaEventPayload := &SchemaEventPayload{
-		Payload: payload{Source: source},
-	}
-
-	assert.Equal(p.T(), time.Date(2022, time.October,
-		11, 3, 19, 24, 942000000, time.UTC), schemaEventPayload.GetExecutionTime())
-}
-
-func (p *PostgresTestSuite) TestGetDataTestInsert() {
-	after := map[string]interface{}{
-		"pk":           1,
-		"foo":          "bar",
-		"name":         "dusty",
-		"favoriteFood": "jerky",
-	}
-
-	var tc kafkalib.TopicConfig
-	schemaEventPayload := SchemaEventPayload{
-		Payload: payload{
-			Before:    nil,
-			After:     after,
-			Operation: "c",
-		},
-	}
-
-	evtData := schemaEventPayload.GetData(context.Background(), "pk", 1, &tc)
-	assert.Equal(p.T(), len(after), len(evtData), "has deletion flag")
-
-	deletionFlag, isOk := evtData[constants.DeleteColumnMarker]
-	assert.True(p.T(), isOk)
-	assert.False(p.T(), deletionFlag.(bool))
-
-	delete(evtData, constants.DeleteColumnMarker)
-	assert.Equal(p.T(), after, evtData)
-}
-
-func (p *PostgresTestSuite) TestGetDataTestDelete() {
-	tc := &kafkalib.TopicConfig{
-		IdempotentKey: "updated_at",
-	}
-
-	now := time.Now().UTC()
-	schemaEventPayload := SchemaEventPayload{
-		Payload: payload{
-			Before:    nil,
-			After:     nil,
-			Operation: "c",
-			Source:    Source{TsMs: now.UnixMilli()},
-		},
-	}
-
-	evtData := schemaEventPayload.GetData(context.Background(), "pk", 1, tc)
-	shouldDelete, isOk := evtData[constants.DeleteColumnMarker]
-	assert.True(p.T(), isOk)
-	assert.True(p.T(), shouldDelete.(bool))
-
-	assert.Equal(p.T(), 3, len(evtData), evtData)
-	assert.Equal(p.T(), evtData["pk"], 1)
-	assert.Equal(p.T(), evtData[tc.IdempotentKey], now.Format(time.RFC3339))
-
-	tc.IdempotentKey = ""
-	evtData = schemaEventPayload.GetData(context.Background(), "pk", 1, tc)
-	_, isOk = evtData[tc.IdempotentKey]
-	assert.False(p.T(), isOk, evtData)
-}
-
-func (p *PostgresTestSuite) TestGetDataTestUpdate() {
-	before := map[string]interface{}{
-		"pk":           1,
-		"foo":          "bar",
-		"name":         "dusty",
-		"favoriteFood": "apples",
-		"age":          1,
-		"weight_lbs":   25,
-	}
-
-	after := map[string]interface{}{
-		"pk":           1,
-		"foo":          "bar",
-		"name":         "dusty",
-		"favoriteFood": "jerky",
-		"age":          2,
-		"weight_lbs":   33,
-	}
-
-	var tc kafkalib.TopicConfig
-	schemaEventPayload := SchemaEventPayload{
-		Payload: payload{
-			Before:    before,
-			After:     after,
-			Operation: "c",
-		},
-	}
-
-	evtData := schemaEventPayload.GetData(context.Background(), "pk", 1, &tc)
-	assert.Equal(p.T(), len(after), len(evtData), "has deletion flag")
-
-	deletionFlag, isOk := evtData[constants.DeleteColumnMarker]
-	assert.True(p.T(), isOk)
-	assert.False(p.T(), deletionFlag.(bool))
-
-	delete(evtData, constants.DeleteColumnMarker)
-	assert.Equal(p.T(), after, evtData)
 }
 
 func (p *PostgresTestSuite) TestPostgresEvent() {
