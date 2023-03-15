4b9059a2a4c8981753a95638abc726abebbb0b80
Robin Tang <rtang.cs@gmail.com>
2023-03-15 11:28:14 -0700

    Supporting additional Debezium time precision (#69)

--- lib/debezium/types.go
+++ lib/debezium/types.go
@@ -15,6 +15,10 @@ const (
 	Date           SupportedDebeziumType = "io.debezium.time.Date"
 	Time           SupportedDebeziumType = "io.debezium.time.Time"
 	TimeMicro      SupportedDebeziumType = "io.debezium.time.MicroTime"
+
+	DateKafkaConnect     SupportedDebeziumType = "org.apache.kafka.connect.data.Date"
+	TimeKafkaConnect     SupportedDebeziumType = "org.apache.kafka.connect.data.Time"
+	DateTimeKafkaConnect SupportedDebeziumType = "org.apache.kafka.connect.data.Timestamp"
 )
 
 var supportedTypes = []SupportedDebeziumType{
@@ -23,6 +27,9 @@ var supportedTypes = []SupportedDebeziumType{
 	Date,
 	Time,
 	TimeMicro,
+	DateKafkaConnect,
+	TimeKafkaConnect,
+	DateTimeKafkaConnect,
 }
 
 func RequiresSpecialTypeCasting(typeLabel string) (bool, SupportedDebeziumType) {
@@ -38,21 +45,23 @@ func RequiresSpecialTypeCasting(typeLabel string) (bool, SupportedDebeziumType) 
 func FromDebeziumTypeToTime(supportedType SupportedDebeziumType, val int64) (*ext.ExtendedTime, error) {
 	// https://debezium.io/documentation/reference/stable/connectors/postgresql.html#postgresql-temporal-types
 	switch supportedType {
-	case Timestamp:
+	case Timestamp, DateTimeKafkaConnect:
 		// Represents the number of milliseconds since the epoch, and does not include timezone information.
 		return ext.NewExtendedTime(time.UnixMilli(val).In(time.UTC), ext.DateTimeKindType, time.RFC3339Nano)
 	case MicroTimestamp:
 		// Represents the number of microseconds since the epoch, and does not include timezone information.
 		return ext.NewExtendedTime(time.UnixMicro(val).In(time.UTC), ext.DateTimeKindType, time.RFC3339Nano)
-	case Date:
+	case Date, DateKafkaConnect:
+		unix := time.UnixMilli(0).In(time.UTC) // 1970-01-01
 		// Represents the number of days since the epoch.
-		return ext.NewExtendedTime(time.UnixMicro(0).AddDate(0, 0, int(val)).In(time.UTC), ext.DateKindType, "")
-	case Time:
+		return ext.NewExtendedTime(unix.AddDate(0, 0, int(val)), ext.DateKindType, "")
+	case Time, TimeKafkaConnect:
 		// Represents the number of milliseconds past midnight, and does not include timezone information.
 		return ext.NewExtendedTime(time.UnixMilli(val).In(time.UTC), ext.TimeKindType, "")
 	case TimeMicro:
 		// Represents the number of microseconds past midnight, and does not include timezone information.
 		return ext.NewExtendedTime(time.UnixMicro(val).In(time.UTC), ext.TimeKindType, "")
+
 	}
 
 	return nil, fmt.Errorf("supportedType: %s, val: %v failed to be matched", supportedType, val)
