ceb449fee605ac17aaefa88f0dd7614b13bd8fce
Robin Tang <rtang.cs@gmail.com>
2023-05-02 17:31:19 -0700

    Use const

--- lib/jitter/sleep.go
+++ lib/jitter/sleep.go
@@ -10,5 +10,5 @@ const maxMilliSeconds = 3500
 func JitterMs(baseMilliSeconds, attempts int) int {
 	// https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
 	// sleep = random_between(0, min(cap, base * 2 ** attempt))
-	return rand.Intn(int(math.Min(3500, float64(baseMilliSeconds)*math.Pow(2, float64(attempts)))))
+	return rand.Intn(int(math.Min(maxMilliSeconds, float64(baseMilliSeconds)*math.Pow(2, float64(attempts)))))
 }
