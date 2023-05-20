601b4fe40a3337975666c7d50a856d0d31db547b
Robin Tang <rtang.cs@gmail.com>
2023-05-20 12:01:37 -0700

    Fix tests.

--- clients/bigquery/cast_test.go
+++ clients/bigquery/cast_test.go
@@ -14,40 +14,40 @@ func TestCastColVal(t *testing.T) {
 		colVal  interface{}
 		colKind typing.Column
 
-		expectedErr    error
-		expectedString string
+		expectedErr   error
+		expectedValue interface{}
 	}
 
 	testCases := []_testCase{
 		{
-			name:           "escaping string",
-			colVal:         "foo",
-			colKind:        typing.Column{KindDetails: typing.String},
-			expectedString: "foo",
+			name:          "escaping string",
+			colVal:        "foo",
+			colKind:       typing.Column{KindDetails: typing.String},
+			expectedValue: "foo",
 		},
 		{
-			name:           "123 as int",
-			colVal:         123,
-			colKind:        typing.Column{KindDetails: typing.Integer},
-			expectedString: "123",
+			name:          "123 as int",
+			colVal:        123,
+			colKind:       typing.Column{KindDetails: typing.Integer},
+			expectedValue: "123",
 		},
 		{
-			name:           "struct",
-			colVal:         `{"hello": "world"}`,
-			colKind:        typing.Column{KindDetails: typing.Struct},
-			expectedString: `{"hello": "world"}`,
+			name:          "struct",
+			colVal:        `{"hello": "world"}`,
+			colKind:       typing.Column{KindDetails: typing.Struct},
+			expectedValue: `{"hello": "world"}`,
 		},
 		{
-			name:           "array",
-			colVal:         []int{1, 2, 3, 4, 5},
-			colKind:        typing.Column{KindDetails: typing.Array},
-			expectedString: `['1','2','3','4','5']`,
+			name:          "array",
+			colVal:        []int{1, 2, 3, 4, 5},
+			colKind:       typing.Column{KindDetails: typing.Array},
+			expectedValue: []string{"1", "2", "3", "4", "5"},
 		},
 	}
 
 	for _, testCase := range testCases {
 		actualString, actualErr := CastColVal(testCase.colVal, testCase.colKind)
 		assert.Equal(t, testCase.expectedErr, actualErr, testCase.name)
-		assert.Equal(t, testCase.expectedString, *actualString, testCase.name)
+		assert.Equal(t, testCase.expectedValue, actualString, testCase.name)
 	}
 }
