e2ed85c766038b0cff0be66cfca854a10be576e1
Robin Tang <rtang.cs@gmail.com>
2023-05-25 14:16:52 -0700

    Snowflake <> Go SQL connection issue with temporary tables.

--- lib/db/db.go
+++ lib/db/db.go
@@ -22,6 +22,16 @@ func Open(ctx context.Context, driverName, dsn string) Store {
 		}).Fatal("Failed to start a SQL client")
 	}
 
+	if driverName == "snowflake" {
+		// This is required because Golang's SQL driver will spin up additional connections.
+		// However, Snowflake temporary tables only last for the duration of the session.
+		// This means, at scale - we can run into a scenario where one Go-routine has been allocated a new connection
+		// Which does not have visibility into the previously created temporary table.
+		// https://github.com/snowflakedb/gosnowflake/issues/181#issuecomment-411904223
+		db.SetMaxIdleConns(1)
+		db.SetMaxOpenConns(1)
+	}
+
 	err = db.Ping()
 	if err != nil {
 		logger.FromContext(ctx).WithFields(map[string]interface{}{
