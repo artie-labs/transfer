e3f5e99c1c9e812e23584e848934375bb3e7816b
Robin Tang <rtang.cs@gmail.com>
2023-07-02 06:52:40 +0900

    Add tests.

--- lib/optimization/event_update_test.go
+++ lib/optimization/event_update_test.go
@@ -1,11 +1,57 @@
 package optimization
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/artie-labs/transfer/lib/typing"
+
+	"github.com/artie-labs/transfer/lib/typing/columns"
+)
 
 func TestTableData_UpdateInMemoryColumnsFromDestination(t *testing.T) {
 	// Test the following scenarios:
-	// 1. col does not exist.
-	// 2. test backfill (true and false)
-	// 3. test copying the col type over
 	// 4. Test copying `extendedTimeDetails`
+	tableDataCols := &columns.Columns{}
+	tableDataCols.AddColumn(columns.NewColumn("name", typing.String))
+	tableDataCols.AddColumn(columns.NewColumn("bool_backfill", typing.Boolean))
+	tableDataCols.AddColumn(columns.NewColumn("prev_invalid", typing.Invalid))
+	tableData := &TableData{
+		inMemoryColumns: tableDataCols,
+	}
+
+	nonExistentTableCols := []string{"dusty", "the", "mini", "aussie"}
+	var nonExistentCols []columns.Column
+	for _, nonExistentTableCol := range nonExistentTableCols {
+		nonExistentCols = append(nonExistentCols, columns.NewColumn(nonExistentTableCol, typing.String))
+	}
+
+	// Testing to make sure we don't copy over non-existent columns
+	tableData.UpdateInMemoryColumnsFromDestination(nonExistentCols...)
+	for _, nonExistentTableCol := range nonExistentTableCols {
+		_, isOk := tableData.inMemoryColumns.GetColumn(nonExistentTableCol)
+		assert.False(t, isOk, nonExistentTableCol)
+	}
+
+	// Testing to make sure we're copying the kindDetails over.
+	tableData.UpdateInMemoryColumnsFromDestination(columns.NewColumn("prev_invalid", typing.String))
+	prevInvalidCol, isOk := tableData.inMemoryColumns.GetColumn("prev_invalid")
+	assert.True(t, isOk)
+	assert.Equal(t, typing.String, prevInvalidCol.KindDetails)
+
+	// Testing backfill
+	for _, inMemoryCol := range tableData.inMemoryColumns.GetColumns() {
+		assert.False(t, inMemoryCol.Backfilled(), inMemoryCol.Name(nil))
+	}
+	backfilledCol := columns.NewColumn("bool_backfill", typing.Boolean)
+	backfilledCol.SetBackfilled(true)
+	tableData.UpdateInMemoryColumnsFromDestination(backfilledCol)
+	for _, inMemoryCol := range tableData.inMemoryColumns.GetColumns() {
+		if inMemoryCol.Name(nil) == backfilledCol.Name(nil) {
+			assert.True(t, inMemoryCol.Backfilled(), inMemoryCol.Name(nil))
+		} else {
+			assert.False(t, inMemoryCol.Backfilled(), inMemoryCol.Name(nil))
+		}
+	}
 }
