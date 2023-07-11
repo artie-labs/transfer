eb17951e23bd3d7e8f25b4f4448e579ff88cfdf7
Robin Tang <rtang.cs@gmail.com>
2023-07-11 07:33:33 -0700

    Adding a test.

---
+++ lib/sql/escape_test.go
@@ -0,0 +1,91 @@
+package sql
+
+import (
+	"testing"
+
+	"github.com/artie-labs/transfer/lib/config/constants"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEscapeName(t *testing.T) {
+	type _testCase struct {
+		name         string
+		nameToEscape string
+		args         *NameArgs
+		expectedName string
+	}
+
+	testCases := []_testCase{
+		{
+			name:         "args = nil",
+			nameToEscape: "order",
+			expectedName: "order",
+		},
+		{
+			name:         "escape = false",
+			args:         &NameArgs{},
+			nameToEscape: "order",
+			expectedName: "order",
+		},
+		{
+			name: "escape = true, snowflake",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.Snowflake,
+			},
+			nameToEscape: "order",
+			expectedName: `"order"`,
+		},
+		{
+			name: "escape = true, snowflake #2",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.Snowflake,
+			},
+			nameToEscape: "hello",
+			expectedName: `hello`,
+		},
+		{
+			name: "escape = true, redshift",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.Redshift,
+			},
+			nameToEscape: "order",
+			expectedName: `"order"`,
+		},
+		{
+			name: "escape = true, redshift #2",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.Redshift,
+			},
+			nameToEscape: "hello",
+			expectedName: `hello`,
+		},
+		{
+			name: "escape = true, bigquery",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.BigQuery,
+			},
+			nameToEscape: "order",
+			expectedName: "`order`",
+		},
+		{
+			name: "escape = true, bigquery, #2",
+			args: &NameArgs{
+				Escape:   true,
+				DestKind: constants.BigQuery,
+			},
+			nameToEscape: "hello",
+			expectedName: "hello",
+		},
+	}
+
+	for _, testCase := range testCases {
+		actualName := EscapeName(testCase.nameToEscape, testCase.args)
+		assert.Equal(t, testCase.expectedName, actualName, testCase.name)
+	}
+}
