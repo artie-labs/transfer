7e80709c600454740c43775af12f62c0d7930e7c
Robin Tang <rtang.cs@gmail.com>
2023-12-22 09:35:26 -0800

    Clean.

--- lib/telemetry/metrics/null_provider.go
+++ lib/telemetry/metrics/null_provider.go
@@ -5,21 +5,16 @@ import "time"
 type NullMetricsProvider struct{}
 
 func (n NullMetricsProvider) Gauge(name string, value float64, tags map[string]string) {
-	return
 }
 
 func (n NullMetricsProvider) GaugeWithSample(name string, value float64, tags map[string]string, sample float64) {
-	return
 }
 
 func (n NullMetricsProvider) Count(name string, value int64, tags map[string]string) {
-	return
 }
 
 func (n NullMetricsProvider) Timing(name string, value time.Duration, tags map[string]string) {
-	return
 }
 
 func (n NullMetricsProvider) Incr(name string, tags map[string]string) {
-	return
 }
