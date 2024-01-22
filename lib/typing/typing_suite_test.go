59d0e9cd913a55fe2b9270cecf392bbe99b8727f
Nathan Villaescusa <nathan@artie.so>
2024-01-22 15:22:40 -0800

    Simplify

--- lib/typing/typing_suite_test.go
+++ lib/typing/typing_suite_test.go
@@ -1,24 +1,13 @@
 package typing
 
 import (
-	"context"
 	"testing"
 
-	"github.com/artie-labs/transfer/lib/config"
 	"github.com/stretchr/testify/suite"
 )
 
 type TypingTestSuite struct {
 	suite.Suite
-	ctx context.Context
-}
-
-func (t *TypingTestSuite) SetupTest() {
-	t.ctx = context.Background() // Initialize the context
-	t.ctx = config.InjectSettingsIntoContext(t.ctx, &config.Settings{
-		VerboseLogging: false,
-		Config:         &config.Config{},
-	})
 }
 
 func TestTypingTestSuite(t *testing.T) {
