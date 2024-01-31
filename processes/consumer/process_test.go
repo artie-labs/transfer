63d0c6ec8df9544a10100f9c0a6fe39293c8cb72
Nathan Villaescusa <nathan@artie.so>
2024-01-31 11:11:27 -0800

    Simplify

--- processes/consumer/process_test.go
+++ processes/consumer/process_test.go
@@ -23,9 +23,7 @@ func TestProcessMessageFailures(t *testing.T) {
 		BufferRows:           10,
 		FlushSizeKb:          900,
 	}
-	ctx := context.Background()
-
-	ctx = models.LoadMemoryDB(ctx)
+	ctx := models.LoadMemoryDB(context.Background())
 	kafkaMsg := kafka.Message{
 		Topic:         "foo",
 		Partition:     0,
