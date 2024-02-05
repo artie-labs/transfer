530efe83b6750e706c722f158027c06ad44192a6
Robin Tang <rtang.cs@gmail.com>
2024-02-05 13:14:59 -0800

    Checkpoint.

--- clients/snowflake/append.go
+++ clients/snowflake/append.go
@@ -2,14 +2,19 @@ package snowflake
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
+
+	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/destination/ddl"
+	"github.com/artie-labs/transfer/lib/typing/columns"
 
 	"github.com/artie-labs/transfer/lib/optimization"
 )
 
 func (s *Store) Append(ctx context.Context, tableData *optimization.TableData) error {
-	err := s.append(ctx, tableData)
+	// TODO: Remove ctx.
+
+	err := s.append(tableData)
 	if IsAuthExpiredError(err) {
 		slog.Warn("authentication has expired, will reload the Snowflake store and retry appending", slog.Any("err", err))
 		s.reestablishConnection()
@@ -19,6 +24,38 @@ func (s *Store) Append(ctx context.Context, tableData *optimization.TableData) e
 	return err
 }
 
-func (s *Store) append(ctx context.Context, tableData *optimization.TableData) error {
-	return fmt.Errorf("snowflake: %s did not implement this yet", s.Label())
+func (s *Store) append(tableData *optimization.TableData) error {
+	if tableData.ShouldSkipUpdate() {
+		return nil
+	}
+
+	fqName := tableData.ToFqName(s.Label(), true, s.config.SharedDestinationConfig.UppercaseEscapedNames, "")
+	tableConfig, err := s.getTableConfig(fqName, tableData.TopicConfig.DropDeletedColumns)
+	if err != nil {
+		return err
+	}
+
+	// We don't care about srcKeysMissing because we don't drop columns in append
+	_, targetKeysMissing := columns.Diff(tableData.ReadOnlyInMemoryCols(), tableConfig.Columns(),
+		tableData.TopicConfig.SoftDelete, tableData.TopicConfig.IncludeArtieUpdatedAt,
+		tableData.TopicConfig.IncludeDatabaseUpdatedAt, tableData.Mode())
+	createAlterTableArgs := ddl.AlterTableArgs{
+		Dwh:               s,
+		Tc:                tableConfig,
+		FqTableName:       fqName,
+		CreateTable:       tableConfig.CreateTable(),
+		ColumnOp:          constants.Add,
+		CdcTime:           tableData.LatestCDCTs,
+		UppercaseEscNames: &s.config.SharedDestinationConfig.UppercaseEscapedNames,
+	}
+
+	// Keys that exist in CDC stream, but not in Snowflake
+	err = ddl.AlterTable(createAlterTableArgs, targetKeysMissing...)
+	if err != nil {
+		slog.Warn("Failed to apply alter table", slog.Any("err", err))
+		return err
+	}
+
+	tableData.MergeColumnsFromDestination(tableConfig.Columns().GetColumns()...)
+	return s.prepareTempTable(tableData, tableConfig, fqName)
 }
