469275c716f956caf021fcdb62d53d88167d79e6
Robin Tang <rtang.cs@gmail.com>
2024-02-05 14:12:28 -0800

    Renaming Flush and Merge.

--- models/memory.go
+++ models/memory.go
@@ -12,19 +12,19 @@ import (
 // We did this because certain operations require different locking patterns
 type TableData struct {
 	*optimization.TableData
-	lastMergeTime time.Time
+	lastFlushTime time.Time
 	sync.Mutex
 }
 
 func (t *TableData) Wipe() {
 	t.TableData = nil
-	t.lastMergeTime = time.Now()
+	t.lastFlushTime = time.Now()
 }
 
-// ShouldSkipMerge - this function is only used when the flush reason was time-based.
+// ShouldSkipFlush - this function is only used when the flush reason was time-based.
 // We want to add this in so that it can strike a balance between the Flush and Consumer go-routines on when to merge.
 // Say our flush interval is 5 mins and it flushed 4 mins ago based on size or rows - we don't want to flush right after since the buffer would be mostly empty.
-func (t *TableData) ShouldSkipMerge(cooldown time.Duration) bool {
+func (t *TableData) ShouldSkipFlush(cooldown time.Duration) bool {
 	if cooldown > 1*time.Minute {
 		confidenceInterval := 0.25
 		confidenceDuration := time.Duration(confidenceInterval * float64(cooldown))
@@ -33,7 +33,7 @@ func (t *TableData) ShouldSkipMerge(cooldown time.Duration) bool {
 		cooldown = cooldown - confidenceDuration
 	}
 
-	return time.Since(t.lastMergeTime) < cooldown
+	return time.Since(t.lastFlushTime) < cooldown
 }
 
 func (t *TableData) Empty() bool {
--- processes/consumer/flush.go
+++ processes/consumer/flush.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"sync"
 	"time"
@@ -14,7 +15,7 @@ import (
 )
 
 type Args struct {
-	// If cooldown is passed in, we'll skip the merge if the table has been recently merged
+	// If cooldown is passed in, we'll skip the flush if the table has been recently flushed
 	CoolDown *time.Duration
 	// If specificTable is not passed in, we'll just flush everything.
 	SpecificTable string
@@ -23,7 +24,7 @@ type Args struct {
 	Reason string
 }
 
-// Flush will merge and commit the offset on the specified topics within `args.SpecificTable`.
+// Flush will merge/append and commit the offset on the specified topics within `args.SpecificTable`.
 // If the table list is empty, it'll flush everything. This is the default behavior for the time duration based flush.
 // Table specific flushes will be triggered based on the size of the pool (length and size wise).
 func Flush(ctx context.Context, inMemDB *models.DatabaseData, dest destination.Baseline, args Args) error {
@@ -52,19 +53,19 @@ func Flush(ctx context.Context, inMemDB *models.DatabaseData, dest destination.B
 				slog.String("tableName", _tableName),
 			}
 
-			if args.CoolDown != nil && _tableData.ShouldSkipMerge(*args.CoolDown) {
-				slog.With(logFields...).Info("Skipping merge because we are currently in a merge cooldown")
+			if args.CoolDown != nil && _tableData.ShouldSkipFlush(*args.CoolDown) {
+				slog.With(logFields...).Info("Skipping flush because we are currently in a flush cooldown")
 				return
 			}
 
-			// Lock the tables when executing merge.
+			// Lock the tables when executing merge / append.
 			_tableData.Lock()
 			defer _tableData.Unlock()
 			if _tableData.Empty() {
 				return
 			}
 
-			// This is added so that we have a new temporary table suffix for each merge.
+			// This is added so that we have a new temporary table suffix for each merge / append.
 			_tableData.ResetTempTableSuffix()
 
 			start := time.Now()
@@ -77,18 +78,20 @@ func Flush(ctx context.Context, inMemDB *models.DatabaseData, dest destination.B
 			}
 
 			var err error
+			action := "merge"
 			// Merge or Append depending on the mode.
 			if _tableData.Mode() == config.History {
 				err = dest.Append(ctx, _tableData.TableData)
+				action = "append"
 			} else {
 				err = dest.Merge(ctx, _tableData.TableData)
 			}
 
 			if err != nil {
 				tags["what"] = "merge_fail"
-				slog.With(logFields...).Warn("Failed to execute merge...not going to flush memory", slog.Any("err", err))
+				slog.With(logFields...).Warn(fmt.Sprintf("Failed to execute %s...not going to flush memory", action), slog.Any("err", err))
 			} else {
-				slog.With(logFields...).Info("Merge success, clearing memory...")
+				slog.With(logFields...).Info(fmt.Sprintf("%s success, clearing memory...", action))
 				commitErr := commitOffset(ctx, _tableData.TopicConfig.Topic, _tableData.PartitionsToLastMessage)
 				if commitErr == nil {
 					inMemDB.ClearTableConfig(_tableName)
