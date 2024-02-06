75d8040c5b627d0e6dbec9c1bfc8339317d6d8ac
Robin Tang <rtang.cs@gmail.com>
2024-02-06 09:18:02 -0800

    Add tests

--- lib/config/config.go
+++ lib/config/config.go
@@ -219,6 +219,10 @@ func readFileToConfig(pathToConfig string) (*Config, error) {
 		config.Mode = Replication
 	}
 
+	if config.Mode == "" {
+		config.Mode = Replication
+	}
+
 	return &config, nil
 }
 
