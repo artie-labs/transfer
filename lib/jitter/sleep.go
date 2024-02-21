4a6aabf53e26b51545c3bb603b8dbaabcce238e6
Nathan Villaescusa <nathan@artie.so>
2024-02-21 00:27:36 -0800

    Add `WithRetries` function

--- lib/jitter/sleep.go
+++ lib/jitter/sleep.go
@@ -1,6 +1,7 @@
 package jitter
 
 import (
+	"log/slog"
 	"math/rand"
 	"time"
 )
@@ -13,4 +14,70 @@ func Jitter(baseMs, maxMs, attempts int) time.Duration {
 	// 2 ** x == 1 << x
 	ms := rand.Intn(min(maxMs, baseMs*(1<<attempts)))
 	return time.Duration(ms) * time.Millisecond
+}
+
+type RetryConfig struct {
+	jitterBaseMs   int
+	jitterMaxMs    int
+	maxAttempts    int
+	isRetryableErr func(err error) bool
+}
+
+type NewRetryConfigArgs struct {
+	JitterBaseMs   int
+	JitterMaxMs    int
+	MaxAttempts    int
+	IsRetryableErr func(err error) bool
+}
+
+func NewRetryConfig(args NewRetryConfigArgs) RetryConfig {
+	isRetryableErr := args.IsRetryableErr
+	if isRetryableErr == nil {
+		isRetryableErr = func(_ error) bool { return true }
+	}
+
+	return RetryConfig{
+		jitterBaseMs:   min(args.JitterBaseMs, 0),
+		jitterMaxMs:    min(args.JitterMaxMs, 0),
+		maxAttempts:    max(args.MaxAttempts, 1),
+		isRetryableErr: isRetryableErr,
+	}
+}
+
+func (r RetryConfig) sleepDuration(attempt int) time.Duration {
+	if r.jitterMaxMs == 0 {
+		return time.Duration(0)
+	}
+	return Jitter(r.jitterBaseMs, r.jitterMaxMs, attempt)
+}
+
+func WithRetries[T any](retrier RetryConfig, f func(attempt int) (T, error)) (T, error) {
+	maxAttempts := max(retrier.maxAttempts, 1)
+	var result T
+	var err error
+	for attempt := 0; attempt < maxAttempts; attempt++ {
+		if attempt > 0 {
+			sleepDuration := retrier.sleepDuration(attempt)
+			if sleepDuration > 0 {
+				slog.Info("An error occurred, retrying after delay...",
+					slog.Duration("sleep", sleepDuration),
+					slog.Any("attemptsLeft", maxAttempts-attempt),
+					slog.Any("err", err),
+				)
+				time.Sleep(sleepDuration)
+			} else {
+				slog.Info("An error occurred, retrying...",
+					slog.Any("attemptsLeft", maxAttempts-attempt),
+					slog.Any("err", err),
+				)
+			}
+		}
+		result, err = f(attempt)
+		if err == nil {
+			return result, nil
+		} else if !retrier.isRetryableErr(err) {
+			break
+		}
+	}
+	return result, err
 }
---
+++ lib/jitter/sleep_test.go
@@ -0,0 +1,82 @@
+package jitter
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithJitteredRetries(t *testing.T) {
+	{
+		// 0 max attempts - still runs
+		retryCfg := NewRetryConfig(NewRetryConfigArgs{})
+		calls := 0
+		value, err := WithRetries(retryCfg, func(attempt int) (int, error) {
+			calls++
+			return 100, nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, value, 100)
+		assert.Equal(t, calls, 1)
+	}
+	{
+		// 1 max attempts - succeeds
+		calls := 0
+		retryCfg := NewRetryConfig(NewRetryConfigArgs{MaxAttempts: 1})
+		value, err := WithRetries(retryCfg, func(attempt int) (int, error) {
+			calls++
+			return 100, nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, value, 100)
+		assert.Equal(t, calls, 1)
+	}
+	{
+		// 1 max attempts - fails
+		calls := 0
+		retryCfg := NewRetryConfig(NewRetryConfigArgs{MaxAttempts: 1})
+		_, err := WithRetries(retryCfg, func(attempt int) (int, error) {
+			calls++
+			return 0, fmt.Errorf("oops I failed again")
+		})
+		assert.ErrorContains(t, err, "oops I failed again")
+		assert.Equal(t, calls, 1)
+	}
+	{
+		// 2 max attempts - first fails and second succeeds
+		calls := 0
+		retryCfg := NewRetryConfig(NewRetryConfigArgs{MaxAttempts: 2})
+		value, err := WithRetries(retryCfg, func(attempt int) (int, error) {
+			calls++
+			if attempt == 0 {
+				return 0, fmt.Errorf("oops I failed again")
+			}
+			return 100, nil
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, value, 100)
+		assert.Equal(t, calls, 2)
+	}
+	{
+		// 3 max attempts - first fails with a retryable error, second fails with a non-retryable error
+		calls := 0
+		retryCfg := NewRetryConfig(NewRetryConfigArgs{
+			MaxAttempts:    3,
+			IsRetryableErr: func(err error) bool { return strings.Contains(err.Error(), "retry") },
+		})
+		_, err := WithRetries(retryCfg, func(attempt int) (int, error) {
+			calls++
+			if attempt == 0 {
+				return 0, fmt.Errorf("retry this one")
+			} else if attempt == 1 {
+				return 0, fmt.Errorf("oops I failed again")
+			}
+			assert.Fail(t, "Should not  happen")
+			return 0, nil
+		})
+		assert.ErrorContains(t, err, "oops I failed again")
+		assert.Equal(t, calls, 2)
+	}
+}
