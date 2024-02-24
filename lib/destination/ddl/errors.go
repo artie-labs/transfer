23c9b8210a8d2150bb0a1c27619e7c7b6f6f8506
Robin Tang <rtang.cs@gmail.com>
2024-02-24 12:28:28 -0800

    Adding idempotency to MSSQL.

--- lib/destination/ddl/errors.go
+++ lib/destination/ddl/errors.go
@@ -20,6 +20,19 @@ func ColumnAlreadyExistErr(err error, kind constants.DestinationKind) bool {
 		// Snowflake doesn't have column mutations (IF NOT EXISTS)
 		// Redshift's error: ERROR: column "foo" of relation "statement" already exists
 		return strings.Contains(err.Error(), "already exists")
+	case constants.MSSQL:
+		alreadyExistErrs := []string{
+			// Column names in each table must be unique. Column name 'first_name' in table 'users' is specified more than once.
+			"Column names in each table must be unique",
+			// There is already an object named 'customers' in the database.
+			"There is already an object named",
+		}
+
+		for _, alreadyExistErr := range alreadyExistErrs {
+			if alreadyExist := strings.Contains(err.Error(), alreadyExistErr); alreadyExist {
+				return alreadyExist
+			}
+		}
 	}
 
 	return false
