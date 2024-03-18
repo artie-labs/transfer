14ffc5aac424a80bafadcec9d84f4607cd15e3f5
Robin Tang <rtang.cs@gmail.com>
2024-03-18 12:15:55 -0700

    Clean up.

--- lib/destination/ddl/ddl.go
+++ lib/destination/ddl/ddl.go
@@ -125,7 +125,12 @@ func AlterTable(args AlterTableArgs, cols ...columns.Column) error {
 	}
 
 	if len(pkCols) > 0 {
-		colSQLParts = append(colSQLParts, fmt.Sprintf("PRIMARY KEY (%s)", strings.Join(pkCols, ", ")))
+		pkStatement := fmt.Sprintf("PRIMARY KEY (%s)", strings.Join(pkCols, ", "))
+		if args.Dwh.Label() == constants.BigQuery {
+			pkStatement += " NOT ENFORCED"
+		}
+
+		colSQLParts = append(colSQLParts, pkStatement)
 	}
 
 	var err error
