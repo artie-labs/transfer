3d871429dc39672318f6fc8738eaf0dc40d797dc
Robin Tang <rtang.cs@gmail.com>
2024-03-25 09:04:49 -0700

    Adding more tests.

--- lib/cdc/util/optional_schema_test.go
+++ lib/cdc/util/optional_schema_test.go
@@ -2,7 +2,12 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
+
+	"github.com/artie-labs/transfer/lib/ptr"
+
+	"github.com/artie-labs/transfer/lib/typing/decimal"
 
 	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/stretchr/testify/assert"
@@ -42,6 +47,59 @@ func TestGetOptionalSchema(t *testing.T) {
 				"email":      typing.String,
 			},
 		},
+		{
+			body: PostgresDelete,
+			expected: map[string]typing.KindDetails{
+				"id":         typing.Integer,
+				"first_name": typing.String,
+				"last_name":  typing.String,
+				"email":      typing.String,
+			},
+		},
+		{
+			body: PostgresUpdate,
+			expected: map[string]typing.KindDetails{
+				"id":           typing.Integer,
+				"first_name":   typing.String,
+				"last_name":    typing.String,
+				"email":        typing.String,
+				"boolean_test": typing.Boolean,
+				"bool_test":    typing.Boolean,
+				"bit_test":     typing.Boolean,
+				"numeric_test": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(decimal.DefaultScale, ptr.ToInt(decimal.PrecisionNotSpecified), nil),
+				},
+				"numeric_5": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(0, ptr.ToInt(5), nil),
+				},
+				"numeric_5_2": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(2, ptr.ToInt(5), nil),
+				},
+				"numeric_5_6": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(6, ptr.ToInt(5), nil),
+				},
+				"numeric_5_0": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(0, ptr.ToInt(5), nil),
+				},
+				"numeric_39_0": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(0, ptr.ToInt(39), nil),
+				},
+				"numeric_39_2": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(2, ptr.ToInt(39), nil),
+				},
+				"numeric_39_6": {
+					Kind:                   typing.EDecimal.Kind,
+					ExtendedDecimalDetails: decimal.NewDecimal(6, ptr.ToInt(39), nil),
+				},
+			},
+		},
 	}
 
 	for idx, tc := range tcs {
@@ -50,6 +108,16 @@ func TestGetOptionalSchema(t *testing.T) {
 		assert.NoError(t, err, idx)
 
 		actualData := schemaEventPayload.GetOptionalSchema()
-		assert.Equal(t, tc.expected, actualData, idx)
+		for actualKey, actualVal := range actualData {
+			testMsg := fmt.Sprintf("key: %s, actualKind: %s, index: %d", actualKey, actualVal.Kind, idx)
+
+			expectedValue, isOk := tc.expected[actualKey]
+			assert.True(t, isOk, testMsg)
+			assert.Equal(t, expectedValue.Kind, actualVal.Kind, testMsg)
+			if expectedValue.ExtendedDecimalDetails != nil || actualVal.ExtendedDecimalDetails != nil {
+				assert.Equal(t, expectedValue.ExtendedDecimalDetails.Scale(), actualVal.ExtendedDecimalDetails.Scale(), testMsg)
+				assert.Equal(t, *expectedValue.ExtendedDecimalDetails.Precision(), *actualVal.ExtendedDecimalDetails.Precision(), testMsg)
+			}
+		}
 	}
 }
