a6f1e8cee213204153055e35588748de1c0a1be0
Robin Tang <rtang.cs@gmail.com>
2024-03-30 15:01:06 -0700

    Rewrite tests.

--- lib/typing/values/string_test.go
+++ lib/typing/values/string_test.go
@@ -5,18 +5,14 @@ import (
 	"testing"
 	"time"
 
-	"github.com/artie-labs/transfer/lib/ptr"
-	"github.com/artie-labs/transfer/lib/typing/decimal"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/artie-labs/transfer/lib/config/constants"
-
-	"github.com/artie-labs/transfer/lib/typing/ext"
-
+	"github.com/artie-labs/transfer/lib/ptr"
 	"github.com/artie-labs/transfer/lib/typing"
-
 	"github.com/artie-labs/transfer/lib/typing/columns"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/artie-labs/transfer/lib/typing/decimal"
+	"github.com/artie-labs/transfer/lib/typing/ext"
 )
 
 func TestBooleanToBit(t *testing.T) {
