5b52eeebf3cfa0aee77822b74f5ef712faa08fad
Robin Tang <rtang.cs@gmail.com>
2024-03-31 21:49:55 -0700

    Add tests.

--- lib/jitter/sleep_test.go
+++ lib/jitter/sleep_test.go
@@ -8,6 +8,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPowerOfTwo(t *testing.T) {
+	assert.Equal(t, 1, powerOfTwo(0))
+	assert.Equal(t, 2, powerOfTwo(1))
+	assert.Equal(t, 4, powerOfTwo(2))
+	assert.Equal(t, 8, powerOfTwo(3))
+	assert.Equal(t, 16, powerOfTwo(4))
+	assert.Equal(t, 32, powerOfTwo(5))
+	assert.Equal(t, 64, powerOfTwo(6))
+	assert.Equal(t, 128, powerOfTwo(7))
+	assert.Equal(t, 256, powerOfTwo(8))
+	assert.Equal(t, 512, powerOfTwo(9))
+	assert.Equal(t, 1024, powerOfTwo(10))
+}
+
 func TestJitter(t *testing.T) {
 	// maxMs <= 0 returns time.Duration(0)
 	assert.Equal(t, time.Duration(0), Jitter(10, 0, 0))
