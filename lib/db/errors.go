c7268164a7e8c2c459f0a48f6da6fcf0777cc7fd
Robin Tang <rtang.cs@gmail.com>
2024-04-01 18:27:39 -0700

    Minor TODO. (#398)

--- lib/db/errors.go
+++ lib/db/errors.go
@@ -2,25 +2,12 @@ package db
 
 import (
 	"errors"
-	"log/slog"
-	"strings"
 	"syscall"
 )
 
 func retryableError(err error) bool {
-	if err != nil {
-		if errors.Is(err, syscall.ECONNRESET) || errors.Is(err, syscall.ECONNREFUSED) {
-			return true
-		}
-		if strings.Contains(err.Error(), "read: connection reset by peer") {
-			// TODO: Remove this conditional if we don't see this warning in logs
-			slog.Warn("matched 'read: connection reset by peer' by string")
-			return true
-		} else if strings.Contains(err.Error(), "connect: connection refused") {
-			// TODO: Remove this conditional if we don't see this warning in logs
-			slog.Warn("matched 'connect: connection refused' by string")
-			return true
-		}
+	if errors.Is(err, syscall.ECONNRESET) || errors.Is(err, syscall.ECONNREFUSED) {
+		return true
 	}
 
 	return false
