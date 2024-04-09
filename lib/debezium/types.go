1de782ed2719ef1c083733ed732568ab0e08905c
Nathan Villaescusa <nathan@artie.so>
2024-04-08 21:52:07 -0700

    More words

--- lib/debezium/types.go
+++ lib/debezium/types.go
@@ -190,7 +190,7 @@ func (f Field) ParseValue(value any) (any, error) {
 	if bytes, ok := value.([]byte); ok {
 		// Preserve existing behavior by base64 encoding []byte values to a string.
 		// TODO: Look into inverting this logic so that in the case the field type is "bytes" but the value is a string we
-		// base64 decode it here. Then things downstream from here can just deal with []byte values.
+		// base64 decode it in a preprocessing step above. Then things downstream from this method can just deal with []byte values.
 		return base64.StdEncoding.EncodeToString(bytes), nil
 	}
 
