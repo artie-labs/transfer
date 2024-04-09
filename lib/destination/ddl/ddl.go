6e33526b371a63b63f2abe66e2f7c6332d58d17a
Robin Tang <rtang.cs@gmail.com>
2024-04-08 19:10:14 -0700

    One liner.

--- lib/destination/ddl/ddl.go
+++ lib/destination/ddl/ddl.go
@@ -25,8 +25,7 @@ func DropTemporaryTable(dwh destination.DataWarehouse, fqTableName string, shoul
 	if strings.Contains(strings.ToLower(fqTableName), constants.ArtiePrefix) {
 		sqlCommand := fmt.Sprintf("DROP TABLE IF EXISTS %s", fqTableName)
 		slog.Debug("Dropping temporary table", slog.String("sql", sqlCommand))
-		_, err := dwh.Exec(sqlCommand)
-		if err != nil {
+		if _, err := dwh.Exec(sqlCommand); err != nil {
 			slog.Warn("Failed to drop temporary table, it will get garbage collected by the TTL...", slog.Any("err", err))
 			if shouldReturnError {
 				return fmt.Errorf("failed to drop temp table: %w", err)
