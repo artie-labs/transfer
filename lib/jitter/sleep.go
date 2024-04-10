7f9ab89ec25266acdbd9af5cb8c4eb97a4ae5086
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-04-10 15:48:07 -0700

    Use `int64` for `computeJitterUpperBoundMs` (#426)

--- lib/jitter/sleep.go
+++ lib/jitter/sleep.go
@@ -7,15 +7,14 @@ import (
 
 const DefaultMaxMs = 3500
 
-func computeJitterUpperBoundMs(baseMs, maxMs, attempts int) int {
-	// https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-	// sleep = random_between(0, min(cap, base * 2 ** attempt))
-	// 2 ** x == 1 << x
+// computeJitterUpperBoundMs calculates min(maxMs, baseMs * 2 ** attempt).
+func computeJitterUpperBoundMs(baseMs, maxMs, attempts int64) int64 {
 	if maxMs <= 0 {
 		return 0
 	}
 
 	// Check for overflows when computing base * 2 ** attempts.
+	// 2 ** x == 1 << x
 	if attemptsMaxMs := baseMs * (1 << attempts); attemptsMaxMs > 0 {
 		maxMs = min(maxMs, attemptsMaxMs)
 	}
@@ -23,10 +22,13 @@ func computeJitterUpperBoundMs(baseMs, maxMs, attempts int) int {
 	return maxMs
 }
 
+// Jitter implements exponential backoff + jitter.
+// See: https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+// Algorithm: sleep = random_between(0, min(cap, base * 2 ** attempt))
 func Jitter(baseMs, maxMs, attempts int) time.Duration {
-	upperBoundMs := computeJitterUpperBoundMs(baseMs, maxMs, attempts)
+	upperBoundMs := computeJitterUpperBoundMs(int64(baseMs), int64(maxMs), int64(attempts))
 	if upperBoundMs <= 0 {
 		return time.Duration(0)
 	}
-	return time.Duration(rand.Intn(upperBoundMs)) * time.Millisecond
+	return time.Duration(rand.Int63n(upperBoundMs)) * time.Millisecond
 }
