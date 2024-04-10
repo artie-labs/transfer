ecd50cc87060975dc9a1008a50418e68b9eff149
Nathan Villaescusa <nathan@artie.so>
2024-04-10 15:43:26 -0700

    Use int64 for `computeJitterUpperBoundMs`

--- lib/jitter/sleep_test.go
+++ lib/jitter/sleep_test.go
@@ -10,23 +10,23 @@ import (
 
 func TestComputeJitterUpperBoundMs(t *testing.T) {
 	// A maxMs that is <= 0 returns 0.
-	assert.Equal(t, 0, computeJitterUpperBoundMs(0, 0, 0))
-	assert.Equal(t, 0, computeJitterUpperBoundMs(10, 0, 0))
-	assert.Equal(t, 0, computeJitterUpperBoundMs(10, 0, 100))
-	assert.Equal(t, 0, computeJitterUpperBoundMs(10, -1, 0))
-	assert.Equal(t, 0, computeJitterUpperBoundMs(10, -1, 100))
+	assert.Equal(t, int64(0), computeJitterUpperBoundMs(0, 0, 0))
+	assert.Equal(t, int64(0), computeJitterUpperBoundMs(10, 0, 0))
+	assert.Equal(t, int64(0), computeJitterUpperBoundMs(10, 0, 100))
+	assert.Equal(t, int64(0), computeJitterUpperBoundMs(10, -1, 0))
+	assert.Equal(t, int64(0), computeJitterUpperBoundMs(10, -1, 100))
 
 	// Increasing attempts with a baseMs of 10 and essentially no maxMs.
-	assert.Equal(t, 10, computeJitterUpperBoundMs(10, math.MaxInt, 0))
-	assert.Equal(t, 20, computeJitterUpperBoundMs(10, math.MaxInt, 1))
-	assert.Equal(t, 40, computeJitterUpperBoundMs(10, math.MaxInt, 2))
-	assert.Equal(t, 80, computeJitterUpperBoundMs(10, math.MaxInt, 3))
-	assert.Equal(t, 160, computeJitterUpperBoundMs(10, math.MaxInt, 4))
+	assert.Equal(t, int64(10), computeJitterUpperBoundMs(10, math.MaxInt64, 0))
+	assert.Equal(t, int64(20), computeJitterUpperBoundMs(10, math.MaxInt64, 1))
+	assert.Equal(t, int64(40), computeJitterUpperBoundMs(10, math.MaxInt64, 2))
+	assert.Equal(t, int64(80), computeJitterUpperBoundMs(10, math.MaxInt64, 3))
+	assert.Equal(t, int64(160), computeJitterUpperBoundMs(10, math.MaxInt64, 4))
 
 	// Large inputs do not panic.
-	assert.Equal(t, 100, computeJitterUpperBoundMs(10, 100, 200))
-	assert.Equal(t, 100, computeJitterUpperBoundMs(10, 100, math.MaxInt))
-	assert.Equal(t, math.MaxInt, computeJitterUpperBoundMs(math.MaxInt, math.MaxInt, math.MaxInt))
+	assert.Equal(t, int64(100), computeJitterUpperBoundMs(10, 100, 200))
+	assert.Equal(t, int64(100), computeJitterUpperBoundMs(10, 100, math.MaxInt64))
+	assert.Equal(t, int64(math.MaxInt64), computeJitterUpperBoundMs(math.MaxInt64, math.MaxInt64, math.MaxInt64))
 }
 
 func TestJitter(t *testing.T) {
