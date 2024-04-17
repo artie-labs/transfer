82f0c2e0606faf75bcbacf6f3d21b04f3718690e
Nathan Villaescusa <nathan@artie.so>
2024-04-16 19:51:42 -0700

    Add test

---
+++ clients/shared/temp_table_test.go
@@ -0,0 +1,69 @@
+package shared
+
+import (
+	"database/sql"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/artie-labs/transfer/lib/config"
+	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/destination/types"
+	"github.com/artie-labs/transfer/lib/kafkalib"
+	"github.com/artie-labs/transfer/lib/optimization"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockDWH struct{}
+
+func (MockDWH) Label() constants.DestinationKind                   { panic("not implemented") }
+func (MockDWH) Merge(tableData *optimization.TableData) error      { panic("not implemented") }
+func (MockDWH) Append(tableData *optimization.TableData) error     { panic("not implemented") }
+func (MockDWH) Dedupe(tableID optimization.TableIdentifier) error  { panic("not implemented") }
+func (MockDWH) Exec(query string, args ...any) (sql.Result, error) { panic("not implemented") }
+func (MockDWH) Query(query string, args ...any) (*sql.Rows, error) { panic("not implemented") }
+func (MockDWH) Begin() (*sql.Tx, error)                            { panic("not implemented") }
+func (MockDWH) IsRetryableError(err error) bool                    { panic("not implemented") }
+func (MockDWH) GetTableConfig(tableData *optimization.TableData) (*types.DwhTableConfig, error) {
+	panic("not implemented")
+}
+func (MockDWH) PrepareTemporaryTable(tableData *optimization.TableData, tableConfig *types.DwhTableConfig, tempTableName string, additionalSettings types.AdditionalSettings, createTempTable bool) error {
+	panic("not implemented")
+}
+
+func (m MockDWH) ToFullyQualifiedName(tableID optimization.TableIdentifier, escape bool) string {
+	return tableID.FqName(constants.Redshift, false, true, optimization.FqNameOpts{})
+}
+
+func TestTempTableName(t *testing.T) {
+	dwh := MockDWH{}
+	tableData := optimization.NewTableData(
+		nil,
+		config.Replication,
+		[]string{},
+		kafkalib.TopicConfig{
+			Schema:   "schema",
+			Database: "db",
+		},
+		"table",
+	)
+	tableData.ResetTempTableSuffix()
+
+	tempTableName := TempTableName(dwh, tableData)
+
+	expectedPrefix := "schema.table___artie_"
+	assert.True(t, strings.HasPrefix(tempTableName, expectedPrefix))
+	assert.Len(t, tempTableName, len(expectedPrefix)+16)
+
+	parts := strings.Split(tempTableName, "_")
+
+	// Check the penultimate part:
+	penultimatePart := parts[len(parts)-2]
+	assert.Len(t, penultimatePart, 5)
+
+	// Check the last part:
+	lastPart := parts[len(parts)-1]
+	assert.Len(t, lastPart, 10)
+	_, err := strconv.ParseInt(lastPart, 10, 64)
+	assert.NoError(t, err)
+}
