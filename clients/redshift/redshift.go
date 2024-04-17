34c7f2606245f8ceac331d695735940944d4c839
Nathan Villaescusa <nathan@artie.so>
2024-04-16 18:21:49 -0700

    Use the same temporary table name logic

--- clients/redshift/redshift.go
+++ clients/redshift/redshift.go
@@ -3,7 +3,7 @@ package redshift
 import (
 	"fmt"
 	"log/slog"
-	"math/rand"
+	"strings"
 
 	_ "github.com/lib/pq"
 
@@ -15,6 +15,7 @@ import (
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/optimization"
 	"github.com/artie-labs/transfer/lib/ptr"
+	"github.com/artie-labs/transfer/lib/stringutil"
 )
 
 type Store struct {
@@ -94,7 +95,9 @@ WHERE
 
 func (s *Store) Dedupe(tableID optimization.TableIdentifier) error {
 	fqTableName := s.ToFullyQualifiedName(tableID, true)
-	stagingTableName := fmt.Sprintf("%s_dedupe_staging_%.5d", constants.ArtiePrefix, rand.Intn(100_000))
+
+	temporaryTableSuffix := strings.ToLower(fmt.Sprintf("%s_%s", constants.ArtiePrefix, stringutil.Random(5)))
+	stagingTableName := fmt.Sprintf("%s_%s", s.ToFullyQualifiedName(tableID, false), temporaryTableSuffix)
 
 	query := fmt.Sprintf(`
 CREATE TABLE %s AS SELECT DISTINCT * FROM %s;
