a6d651371110dcce96e3cd07b90649e941f9dd35
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-04-17 23:02:37 -0700

    [tests] Split out `TempTableName` tests (#452)

--- clients/mssql/store_test.go
+++ clients/mssql/store_test.go
@@ -1,8 +1,12 @@
 package mssql
 
 import (
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/artie-labs/transfer/clients/shared"
 	"github.com/artie-labs/transfer/lib/config"
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/optimization"
@@ -35,5 +39,29 @@ func TestFullyQualifiedName(t *testing.T) {
 		}
 		assert.Equal(t, `schema."table"`, store.ToFullyQualifiedName(tableData, true), "escaped")
 		assert.Equal(t, "schema.table", store.ToFullyQualifiedName(tableData, false), "unescaped")
+	}
+}
+
+func TestTempTableName(t *testing.T) {
+	trimTTL := func(tableName string) string {
+		lastUnderscore := strings.LastIndex(tableName, "_")
+		assert.GreaterOrEqual(t, lastUnderscore, 0)
+		epoch, err := strconv.ParseInt(tableName[lastUnderscore+1:], 10, 64)
+		assert.NoError(t, err)
+		assert.Greater(t, time.Unix(epoch, 0), time.Now().Add(5*time.Hour)) // default TTL is 6 hours from now
+		return tableName[:lastUnderscore]
+	}
+
+	{
+		// Schema is "schema":
+		tableData := optimization.NewTableData(nil, config.Replication, nil, kafkalib.TopicConfig{Database: "db", Schema: "schema"}, "table")
+		tempTableName := shared.TempTableName(&Store{}, tableData, "sUfFiX")
+		assert.Equal(t, "schema.table___artie_sUfFiX", trimTTL(tempTableName))
+	}
+	{
+		// Schema is "public" -> "dbo":
+		tableData := optimization.NewTableData(nil, config.Replication, nil, kafkalib.TopicConfig{Database: "db", Schema: "public"}, "table")
+		tempTableName := shared.TempTableName(&Store{}, tableData, "sUfFiX")
+		assert.Equal(t, "dbo.table___artie_sUfFiX", trimTTL(tempTableName))
 	}
 }
