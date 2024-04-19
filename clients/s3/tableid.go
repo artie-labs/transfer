7fe4b7bca85562fd9ce7f742e521493582001b07
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-04-18 21:54:16 -0700

    Return immediately from `EscapeName` when `!args.Escape` (#467)

--- clients/s3/tableid.go
+++ clients/s3/tableid.go
@@ -2,9 +2,6 @@ package s3
 
 import (
 	"fmt"
-
-	"github.com/artie-labs/transfer/lib/config/constants"
-	"github.com/artie-labs/transfer/lib/sql"
 )
 
 type TableIdentifier struct {
@@ -29,12 +26,7 @@ func (ti TableIdentifier) Table() string {
 	return ti.table
 }
 
-func (ti TableIdentifier) FullyQualifiedName(_, uppercaseEscNames bool) string {
+func (ti TableIdentifier) FullyQualifiedName(_, _ bool) string {
 	// S3 should be db.schema.tableName, but we don't need to escape, since it's not a SQL db.
-	return fmt.Sprintf(
-		"%s.%s.%s",
-		ti.database,
-		ti.schema,
-		sql.EscapeName(ti.table, uppercaseEscNames, &sql.NameArgs{Escape: false, DestKind: constants.S3}),
-	)
+	return fmt.Sprintf("%s.%s.%s", ti.database, ti.schema, ti.table)
 }
