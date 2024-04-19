c5e34170652fc507e92b34e801939386f949efbe
Nathan Villaescusa <nathan@artie.so>
2024-04-19 12:14:58 -0700

    Add tag

--- processes/consumer/process.go
+++ processes/consumer/process.go
@@ -62,6 +62,7 @@ func (p processArgs) process(ctx context.Context, cfg config.Config, inMemDB *mo
 	tags["op"] = _event.Operation()
 	evt, err := event.ToMemoryEvent(_event, pkMap, topicConfig.tc, cfg.Mode)
 	if err != nil {
+		tags["what"] = "to_mem_event_err"
 		return "", err
 	}
 	// Table name is only available after event has been cast
