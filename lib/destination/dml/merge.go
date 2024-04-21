d5bb28d1349b4a2b70c6913b6442058402312be7
Nathan Villaescusa <nathan@artie.so>
2024-04-21 16:14:23 -0700

    Imports

--- lib/destination/dml/merge.go
+++ lib/destination/dml/merge.go
@@ -5,15 +5,12 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/artie-labs/transfer/lib/destination/types"
-	"github.com/artie-labs/transfer/lib/sql"
-
-	"github.com/artie-labs/transfer/lib/typing/columns"
-
-	"github.com/artie-labs/transfer/lib/typing"
-
 	"github.com/artie-labs/transfer/lib/array"
 	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/destination/types"
+	"github.com/artie-labs/transfer/lib/sql"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/columns"
 )
 
 type MergeArgument struct {
