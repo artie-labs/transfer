0e9bf116e98d7b99f956dea523569c0ddf0d98ab
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-04-23 13:48:36 -0700

    [redshift] Don't uppercase escaped table names (#493)

--- clients/redshift/tableid_test.go
+++ clients/redshift/tableid_test.go
@@ -7,21 +7,18 @@ import (
 )
 
 func TestTableIdentifier_WithTable(t *testing.T) {
-	tableID := NewTableIdentifier("schema", "foo", true)
+	tableID := NewTableIdentifier("schema", "foo")
 	tableID2 := tableID.WithTable("bar")
 	typedTableID2, ok := tableID2.(TableIdentifier)
 	assert.True(t, ok)
 	assert.Equal(t, "schema", typedTableID2.Schema())
 	assert.Equal(t, "bar", tableID2.Table())
-	assert.True(t, typedTableID2.uppercaseEscapedNames)
 }
 
 func TestTableIdentifier_FullyQualifiedName(t *testing.T) {
-	// Table name that does not need escaping:
-	assert.Equal(t, "schema.foo", NewTableIdentifier("schema", "foo", false).FullyQualifiedName())
-	assert.Equal(t, "schema.foo", NewTableIdentifier("schema", "foo", true).FullyQualifiedName())
+	// Table name that is not a reserved word:
+	assert.Equal(t, `schema.foo`, NewTableIdentifier("schema", "foo").FullyQualifiedName())
 
-	// Table name that needs escaping:
-	assert.Equal(t, `schema."table"`, NewTableIdentifier("schema", "table", false).FullyQualifiedName())
-	assert.Equal(t, `schema."TABLE"`, NewTableIdentifier("schema", "table", true).FullyQualifiedName())
+	// Table name that is a reserved word:
+	assert.Equal(t, `schema."table"`, NewTableIdentifier("schema", "table").FullyQualifiedName())
 }
