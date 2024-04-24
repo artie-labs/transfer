19221f956cc7be1c6740a1bc198bfadfaee95672
Nathan Villaescusa <nathan@artie.so>
2024-04-24 13:06:45 -0700

    [mssql] Escape all columns that do not start with "__artie"

--- lib/sql/escape.go
+++ lib/sql/escape.go
@@ -25,7 +25,7 @@ func NeedsEscaping(name string, destKind constants.DestinationKind) bool {
 	if destKind == constants.Redshift {
 		reservedKeywords = constants.RedshiftReservedKeywords
 	} else if destKind == constants.MSSQL {
-		reservedKeywords = constants.MSSQLReservedKeywords
+		return !strings.Contains(name, constants.ArtiePrefix)
 	} else {
 		reservedKeywords = constants.ReservedKeywords
 	}
