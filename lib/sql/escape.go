06ccd1d4bdb84573d769c3a0fa9446159666ea18
Nathan Villaescusa <nathan@artie.so>
2024-05-01 13:23:59 -0700

    [sql] Move `NeedsEscaping` to `Dialect`

--- lib/sql/escape.go
+++ lib/sql/escape.go
@@ -1,11 +1,6 @@
 package sql
 
 import (
-	"log/slog"
-	"slices"
-	"strconv"
-	"strings"
-
 	"github.com/artie-labs/transfer/lib/config/constants"
 )
 
@@ -13,45 +8,12 @@ import (
 var symbolsToEscape = []string{":"}
 
 func EscapeNameIfNecessary(name string, uppercaseEscNames bool, destKind constants.DestinationKind) string {
-	if NeedsEscaping(name, uppercaseEscNames, destKind) {
-		return EscapeName(name, uppercaseEscNames, destKind)
+	var dialect = dialectFor(destKind, uppercaseEscNames)
+
+	if destKind != constants.S3 && dialect.NeedsEscaping(name) {
+		return dialect.QuoteIdentifier(name)
 	}
 	return name
-}
-
-func NeedsEscaping(name string, uppercaseEscNames bool, destKind constants.DestinationKind) bool {
-	switch destKind {
-	case constants.BigQuery, constants.MSSQL, constants.Redshift:
-		return true
-	case constants.S3:
-		return false
-	case constants.Snowflake:
-		if uppercaseEscNames {
-			// If uppercaseEscNames is true then we will escape all identifiers that do not start with the Artie priefix.
-			// Since they will be uppercased afer they are escaped then they will result in the same value as if we
-			// we were to use them in a query without any escaping at all.
-			return true
-		} else {
-			if slices.Contains(constants.ReservedKeywords, name) {
-				return true
-			}
-			// If it does not contain any reserved words, does it contain any symbols that need to be escaped?
-			for _, symbol := range symbolsToEscape {
-				if strings.Contains(name, symbol) {
-					return true
-				}
-			}
-			// If it still doesn't need to be escaped, we should check if it's a number.
-			if _, err := strconv.Atoi(name); err == nil {
-				return true
-			}
-		}
-	default:
-		slog.Error("Unsupported destination kind", slog.String("destKind", string(destKind)))
-		return true
-	}
-
-	return false
 }
 
 func dialectFor(destKind constants.DestinationKind, uppercaseEscNames bool) Dialect {
@@ -68,5 +30,6 @@ func dialectFor(destKind constants.DestinationKind, uppercaseEscNames bool) Dial
 }
 
 func EscapeName(name string, uppercaseEscNames bool, destKind constants.DestinationKind) string {
+	// TODO: This is only used in one place, remove once [Dialect] has beem added to [Store].
 	return dialectFor(destKind, uppercaseEscNames).QuoteIdentifier(name)
 }
--- lib/sql/escape_test.go
+++ lib/sql/escape_test.go
@@ -7,39 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNeedsEscaping(t *testing.T) {
-	// BigQuery:
-	assert.True(t, NeedsEscaping("select", false, constants.BigQuery))       // name that is reserved
-	assert.True(t, NeedsEscaping("foo", false, constants.BigQuery))          // name that is not reserved
-	assert.True(t, NeedsEscaping("__artie_foo", false, constants.BigQuery))  // Artie prefix
-	assert.True(t, NeedsEscaping("__artie_foo:bar", false, constants.MSSQL)) // Artie prefix + symbol
-
-	// MS SQL:
-	assert.True(t, NeedsEscaping("select", false, constants.MSSQL))          // name that is reserved
-	assert.True(t, NeedsEscaping("foo", false, constants.MSSQL))             // name that is not reserved
-	assert.True(t, NeedsEscaping("__artie_foo", false, constants.MSSQL))     // Artie prefix
-	assert.True(t, NeedsEscaping("__artie_foo:bar", false, constants.MSSQL)) // Artie prefix + symbol
-
-	// Redshift:
-	assert.True(t, NeedsEscaping("select", false, constants.Redshift))          // name that is reserved
-	assert.True(t, NeedsEscaping("truncatecolumns", false, constants.Redshift)) // name that is reserved for Redshift
-	assert.True(t, NeedsEscaping("foo", false, constants.Redshift))             // name that is not reserved
-	assert.True(t, NeedsEscaping("__artie_foo", false, constants.Redshift))     // Artie prefix
-	assert.True(t, NeedsEscaping("__artie_foo:bar", false, constants.Redshift)) // Artie prefix + symbol
-
-	// Snowflake (uppercaseEscNames = false):
-	assert.True(t, NeedsEscaping("select", false, constants.Snowflake))          // name that is reserved
-	assert.False(t, NeedsEscaping("foo", false, constants.Snowflake))            // name that is not reserved
-	assert.False(t, NeedsEscaping("__artie_foo", false, constants.Snowflake))    // Artie prefix
-	assert.True(t, NeedsEscaping("__artie_foo:bar", false, constants.Snowflake)) // Artie prefix + symbol
-
-	// Snowflake (uppercaseEscNames = true):
-	assert.True(t, NeedsEscaping("select", true, constants.Snowflake))          // name that is reserved
-	assert.True(t, NeedsEscaping("foo", true, constants.Snowflake))             // name that is not reserved
-	assert.True(t, NeedsEscaping("__artie_foo", true, constants.Snowflake))     // Artie prefix
-	assert.True(t, NeedsEscaping("__artie_foo:bar", true, constants.Snowflake)) // Artie prefix + symbol
-}
-
 func TestEscapeNameIfNecessary(t *testing.T) {
 	type _testCase struct {
 		name                     string
