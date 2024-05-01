98135da1914fdd9aa2888bb8a7cb1ffc0c39a832
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-05-01 13:28:58 -0700

    [sql] Move `NeedsEscaping` to `Dialect` (#523)

--- lib/sql/dialect.go
+++ lib/sql/dialect.go
@@ -3,20 +3,29 @@ package sql
 import (
 	"fmt"
 	"log/slog"
+	"slices"
+	"strconv"
 	"strings"
+
+	"github.com/artie-labs/transfer/lib/config/constants"
 )
 
 type Dialect interface {
+	NeedsEscaping(identifier string) bool // TODO: Remove this when we escape everything
 	QuoteIdentifier(identifier string) string
 }
 
 type DefaultDialect struct{}
+
+func (DefaultDialect) NeedsEscaping(_ string) bool { return true }
 
 func (DefaultDialect) QuoteIdentifier(identifier string) string {
 	return fmt.Sprintf(`"%s"`, identifier)
 }
 
 type BigQueryDialect struct{}
+
+func (BigQueryDialect) NeedsEscaping(_ string) bool { return true }
 
 func (BigQueryDialect) QuoteIdentifier(identifier string) string {
 	// BigQuery needs backticks to quote.
@@ -25,6 +34,8 @@ func (BigQueryDialect) QuoteIdentifier(identifier string) string {
 
 type RedshiftDialect struct{}
 
+func (RedshiftDialect) NeedsEscaping(_ string) bool { return true }
+
 func (rd RedshiftDialect) QuoteIdentifier(identifier string) string {
 	// Preserve the existing behavior of Redshift identifiers being lowercased due to not being quoted.
 	return fmt.Sprintf(`"%s"`, strings.ToLower(identifier))
@@ -32,6 +43,33 @@ func (rd RedshiftDialect) QuoteIdentifier(identifier string) string {
 
 type SnowflakeDialect struct {
 	UppercaseEscNames bool
+}
+
+// symbolsToEscape are additional keywords that we need to escape
+var symbolsToEscape = []string{":"}
+
+func (sd SnowflakeDialect) NeedsEscaping(name string) bool {
+	if sd.UppercaseEscNames {
+		// If uppercaseEscNames is true then we will escape all identifiers that do not start with the Artie priefix.
+		// Since they will be uppercased afer they are escaped then they will result in the same value as if we
+		// we were to use them in a query without any escaping at all.
+		return true
+	} else {
+		if slices.Contains(constants.ReservedKeywords, name) {
+			return true
+		}
+		// If it does not contain any reserved words, does it contain any symbols that need to be escaped?
+		for _, symbol := range symbolsToEscape {
+			if strings.Contains(name, symbol) {
+				return true
+			}
+		}
+		// If it still doesn't need to be escaped, we should check if it's a number.
+		if _, err := strconv.Atoi(name); err == nil {
+			return true
+		}
+		return false
+	}
 }
 
 func (sd SnowflakeDialect) QuoteIdentifier(identifier string) string {
--- lib/sql/dialect_test.go
+++ lib/sql/dialect_test.go
@@ -24,6 +24,28 @@ func TestRedshiftDialect_QuoteIdentifier(t *testing.T) {
 	assert.Equal(t, `"foo"`, dialect.QuoteIdentifier("FOO"))
 }
 
+func TestSnowflakeDialect_NeedsEscaping(t *testing.T) {
+	{
+		// UppercaseEscNames enabled:
+		dialect := SnowflakeDialect{UppercaseEscNames: true}
+
+		assert.True(t, dialect.NeedsEscaping("select"))          // name that is reserved
+		assert.True(t, dialect.NeedsEscaping("foo"))             // name that is not reserved
+		assert.True(t, dialect.NeedsEscaping("__artie_foo"))     // Artie prefix
+		assert.True(t, dialect.NeedsEscaping("__artie_foo:bar")) // Artie prefix + symbol
+	}
+
+	{
+		// UppercaseEscNames disabled:
+		dialect := SnowflakeDialect{UppercaseEscNames: false}
+
+		assert.True(t, dialect.NeedsEscaping("select"))          // name that is reserved
+		assert.False(t, dialect.NeedsEscaping("foo"))            // name that is not reserved
+		assert.False(t, dialect.NeedsEscaping("__artie_foo"))    // Artie prefix
+		assert.True(t, dialect.NeedsEscaping("__artie_foo:bar")) // Artie prefix + symbol
+	}
+}
+
 func TestSnowflakeDialect_QuoteIdentifier(t *testing.T) {
 	{
 		// UppercaseEscNames enabled:
