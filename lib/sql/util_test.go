be66693fb7332b7f18ec2545c78d56353b4b463b
Nathan Villaescusa <nathan@artie.so>
2024-05-01 18:23:18 -0700

    Cleanup

--- lib/sql/util_test.go
+++ lib/sql/util_test.go
@@ -7,36 +7,34 @@ import (
 )
 
 func TestQuoteLiteral(t *testing.T) {
-	type _testCase struct {
-		name           string
-		colVal         string
-		expectedString string
-	}
-
-	testCases := []_testCase{
+	testCases := []struct {
+		name     string
+		colVal   string
+		expected string
+	}{
 		{
-			name:           "string",
-			colVal:         "hello",
-			expectedString: "'hello'",
+			name:     "string",
+			colVal:   "hello",
+			expected: "'hello'",
 		},
 		{
-			name:           "string that requires escaping",
-			colVal:         "bobby o'reilly",
-			expectedString: `'bobby o\'reilly'`,
+			name:     "string that requires escaping",
+			colVal:   "bobby o'reilly",
+			expected: `'bobby o\'reilly'`,
 		},
 		{
-			name:           "string with line breaks",
-			colVal:         "line1 \n line 2",
-			expectedString: "'line1 \n line 2'",
+			name:     "string with line breaks",
+			colVal:   "line1 \n line 2",
+			expected: "'line1 \n line 2'",
 		},
 		{
-			name:           "string with existing backslash",
-			colVal:         `hello \ there \ hh`,
-			expectedString: `'hello \\ there \\ hh'`,
+			name:     "string with existing backslash",
+			colVal:   `hello \ there \ hh`,
+			expected: `'hello \\ there \\ hh'`,
 		},
 	}
 
 	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expectedString, QuoteLiteral(testCase.colVal), testCase.name)
+		assert.Equal(t, testCase.expected, QuoteLiteral(testCase.colVal), testCase.name)
 	}
 }
