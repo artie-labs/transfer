d1d009ae4094251ea14aa60595b4c04bd482f330
Nathan Villaescusa <nathan@artie.so>
2024-05-08 12:16:39 -0700

    Add `ExecStatements` function

--- lib/destination/dwh.go
+++ lib/destination/dwh.go
@@ -2,6 +2,8 @@ package destination
 
 import (
 	"database/sql"
+	"fmt"
+	"log/slog"
 
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/destination/types"
@@ -34,4 +36,36 @@ type Baseline interface {
 	Append(tableData *optimization.TableData) error
 	IsRetryableError(err error) bool
 	IdentifierFor(topicConfig kafkalib.TopicConfig, table string) types.TableIdentifier
+}
+
+func ExecStatements(dwh DataWarehouse, statements []string) error {
+	switch len(statements) {
+	case 0:
+		return fmt.Errorf("statements is empty")
+	case 1:
+		slog.Debug("Executing...", slog.String("query", statements[0]))
+		if _, err := dwh.Exec(statements[0]); err != nil {
+			return fmt.Errorf("failed to execute statement: %w", err)
+		}
+
+		return nil
+	default:
+		tx, err := dwh.Begin()
+		if err != nil {
+			return fmt.Errorf("failed to start tx: %w", err)
+		}
+
+		for _, statement := range statements {
+			slog.Debug("Executing...", slog.String("query", statement))
+			if _, err = tx.Exec(statement); err != nil {
+				return fmt.Errorf("failed execute statement: %q, err: %w", statement, err)
+			}
+		}
+
+		if err = tx.Commit(); err != nil {
+			return fmt.Errorf("failed to commit statements: %v, err: %w", statements, err)
+		}
+
+		return nil
+	}
 }
