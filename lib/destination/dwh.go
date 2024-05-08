33b50986f3651c502e1ef26f3c6078b35d595851
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-05-08 14:01:08 -0700

    Add `ExecStatements` function (#576)

--- lib/destination/dwh.go
+++ lib/destination/dwh.go
@@ -2,6 +2,8 @@ package destination
 
 import (
 	"database/sql"
+	"fmt"
+	"log/slog"
 
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/destination/types"
@@ -34,4 +36,47 @@ type Baseline interface {
 	Append(tableData *optimization.TableData) error
 	IsRetryableError(err error) bool
 	IdentifierFor(topicConfig kafkalib.TopicConfig, table string) types.TableIdentifier
+}
+
+// ExecStatements executes one or more statements against a [DataWarehouse].
+// If there is more than one statement, the statements will be executed inside of a transaction.
+func ExecStatements(dwh DataWarehouse, statements []string) error {
+	switch len(statements) {
+	case 0:
+		return fmt.Errorf("statements is empty")
+	case 1:
+		slog.Debug("Executing...", slog.String("query", statements[0]))
+		if _, err := dwh.Exec(statements[0]); err != nil {
+			return fmt.Errorf("failed to execute statement: %w", err)
+		}
+
+		return nil
+	default:
+		tx, err := dwh.Begin()
+		if err != nil {
+			return fmt.Errorf("failed to start tx: %w", err)
+		}
+		var committed bool
+		defer func() {
+			if !committed {
+				if rollbackErr := tx.Rollback(); rollbackErr != nil {
+					slog.Warn("Unable to rollback", slog.Any("err", rollbackErr))
+				}
+			}
+		}()
+
+		for _, statement := range statements {
+			slog.Debug("Executing...", slog.String("query", statement))
+			if _, err = tx.Exec(statement); err != nil {
+				return fmt.Errorf("failed to execute statement: %q, err: %w", statement, err)
+			}
+		}
+
+		if err = tx.Commit(); err != nil {
+			return fmt.Errorf("failed to commit statements: %v, err: %w", statements, err)
+		}
+		committed = true
+
+		return nil
+	}
 }
