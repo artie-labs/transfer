10793dd3b3eebdf2f4c3aa8cab93e50a9ac49fea
Robin Tang <rtang.cs@gmail.com>
2024-05-08 18:16:22 -0700

    Checkpoint.

--- clients/shared/table_config_test.go
+++ clients/shared/table_config_test.go
@@ -1,18 +1,15 @@
 package shared
 
 import (
-	"database/sql"
 	"fmt"
 	"testing"
 
+	"github.com/artie-labs/transfer/lib/mocks"
+
 	"github.com/stretchr/testify/assert"
 
-	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/destination/types"
-	"github.com/artie-labs/transfer/lib/kafkalib"
-	"github.com/artie-labs/transfer/lib/optimization"
 	"github.com/artie-labs/transfer/lib/ptr"
-	sqllib "github.com/artie-labs/transfer/lib/sql"
 	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/artie-labs/transfer/lib/typing/columns"
 )
@@ -53,30 +50,6 @@ func TestGetTableCfgArgs_ShouldParseComment(t *testing.T) {
 	}
 }
 
-type MockDWH struct{}
-
-func (MockDWH) Label() constants.DestinationKind               { panic("not implemented") }
-func (MockDWH) Dialect() sqllib.Dialect                        { panic("not implemented") }
-func (MockDWH) AdditionalDateFormats() []string                { panic("not implemented") }
-func (MockDWH) Merge(tableData *optimization.TableData) error  { panic("not implemented") }
-func (MockDWH) Append(tableData *optimization.TableData) error { panic("not implemented") }
-func (MockDWH) Dedupe(tableID types.TableIdentifier, primaryKeys []string, topicConfig kafkalib.TopicConfig) error {
-	panic("not implemented")
-}
-func (MockDWH) Exec(query string, args ...any) (sql.Result, error) { panic("not implemented") }
-func (MockDWH) Query(query string, args ...any) (*sql.Rows, error) { panic("not implemented") }
-func (MockDWH) Begin() (*sql.Tx, error)                            { panic("not implemented") }
-func (MockDWH) IsRetryableError(err error) bool                    { panic("not implemented") }
-func (MockDWH) GetTableConfig(tableData *optimization.TableData) (*types.DwhTableConfig, error) {
-	panic("not implemented")
-}
-func (MockDWH) PrepareTemporaryTable(tableData *optimization.TableData, tableConfig *types.DwhTableConfig, tempTableID types.TableIdentifier, additionalSettings types.AdditionalSettings, createTempTable bool) error {
-	panic("not implemented")
-}
-func (MockDWH) IdentifierFor(topicConfig kafkalib.TopicConfig, name string) types.TableIdentifier {
-	panic("not implemented")
-}
-
 type MockTableIdentifier struct{ fqName string }
 
 func (MockTableIdentifier) Table() string                                { panic("not implemented") }
@@ -97,7 +70,7 @@ func TestGetTableConfig(t *testing.T) {
 	cm.AddTableToConfig(tableID, dwhTableCfg)
 
 	actualTableCfg, err := GetTableCfgArgs{
-		Dwh:       MockDWH{},
+		Dwh:       &mocks.FakeDataWarehouse{},
 		TableID:   tableID,
 		ConfigMap: cm,
 	}.GetTableConfig()
