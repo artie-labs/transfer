a30f56763cedb7535365270dec965688fd24bd47
Nathan Villaescusa <nathan@artie.so>
2024-05-09 16:57:20 -0700

    Inline

--- lib/sql/mssql.go
+++ lib/sql/mssql.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"strings"
 
+	"strconv"
+
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/ext"
 )
 
 type MSSQLDialect struct{}
@@ -18,12 +21,79 @@ func (MSSQLDialect) EscapeStruct(value string) string {
 	panic("not implemented") // We don't currently support backfills for MS SQL.
 }
 
-func (MSSQLDialect) DataTypeForKind(kd typing.KindDetails, isPk bool) string {
-	return typing.KindToMSSQL(kd, isPk)
+func (MSSQLDialect) DataTypeForKind(kindDetails typing.KindDetails, isPk bool) string {
+	const maxVarCharLengthForPrimaryKey = 900 // Primary keys cannot exceed 900 chars in length.
+	// https://learn.microsoft.com/en-us/sql/relational-databases/tables/primary-and-foreign-key-constraints?view=sql-server-ver16#PKeys
+
+	switch kindDetails.Kind {
+	case typing.Float.Kind:
+		return "float"
+	case typing.Integer.Kind:
+		return "bigint"
+	case typing.Struct.Kind, typing.Array.Kind:
+		return "NVARCHAR(MAX)"
+	case typing.String.Kind:
+		if kindDetails.OptionalStringPrecision != nil {
+			precision := *kindDetails.OptionalStringPrecision
+			if isPk {
+				precision = min(maxVarCharLengthForPrimaryKey, precision)
+			}
+			return fmt.Sprintf("VARCHAR(%d)", precision)
+		}
+		if isPk {
+			return fmt.Sprintf("VARCHAR(%d)", maxVarCharLengthForPrimaryKey)
+		}
+		return "VARCHAR(MAX)"
+	case typing.Boolean.Kind:
+		return "BIT"
+	case typing.ETime.Kind:
+		switch kindDetails.ExtendedTimeDetails.Type {
+		case ext.DateTimeKindType:
+			return "datetime2"
+		case ext.DateKindType:
+			return "date"
+		case ext.TimeKindType:
+			return "time"
+		}
+	case typing.EDecimal.Kind:
+		return kindDetails.ExtendedDecimalDetails.MsSQLKind()
+	}
+	return kindDetails.Kind
 }
 
-func (MSSQLDialect) KindForDataType(_type string, stringPrecision string) (typing.KindDetails, error) {
-	return typing.MSSQLTypeToKind(_type, stringPrecision), nil
+func (MSSQLDialect) KindForDataType(rawType string, stringPrecision string) (typing.KindDetails, error) {
+
+	rawType = strings.ToLower(rawType)
+	if strings.HasPrefix(rawType, "numeric") {
+		return typing.ParseNumeric(typing.DefaultPrefix, rawType), nil
+	}
+	switch rawType {
+	case "char", "varchar", "nchar", "nvarchar", "ntext":
+		var strPrecision *int
+		precision, err := strconv.Atoi(stringPrecision)
+		if err == nil {
+			strPrecision = &precision
+		}
+		if precision == -1 {
+			strPrecision = nil
+		}
+		return typing.KindDetails{Kind: typing.String.Kind, OptionalStringPrecision: strPrecision}, nil
+	case "smallint", "tinyint", "bigint", "int":
+		return typing.Integer, nil
+	case "float", "real":
+		return typing.Float, nil
+	case "datetime", "datetime2":
+		return typing.NewKindDetailsFromTemplate(typing.ETime, ext.DateTimeKindType), nil
+	case "time":
+		return typing.NewKindDetailsFromTemplate(typing.ETime, ext.TimeKindType), nil
+	case "date":
+		return typing.NewKindDetailsFromTemplate(typing.ETime, ext.DateKindType), nil
+	case "bit":
+		return typing.Boolean, nil
+	case "text":
+		return typing.String, nil
+	}
+	return typing.Invalid, nil
 }
 
 func (MSSQLDialect) IsColumnAlreadyExistsErr(err error) bool {
