0b02d2a88fa9ec7fb28dbf47fb801c6488e19754
Robin Tang <rtang.cs@gmail.com>
2024-05-09 09:26:57 -0700

    Remove tests.

--- lib/destination/utils/load.go
+++ lib/destination/utils/load.go
@@ -11,9 +11,7 @@ import (
 	"github.com/artie-labs/transfer/lib/config"
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/db"
-	"github.com/artie-labs/transfer/lib/db/mock"
 	"github.com/artie-labs/transfer/lib/destination"
-	"github.com/artie-labs/transfer/lib/mocks"
 )
 
 func IsOutputBaseline(cfg config.Config) bool {
@@ -36,16 +34,6 @@ func LoadBaseline(cfg config.Config) (destination.Baseline, error) {
 
 func LoadDataWarehouse(cfg config.Config, store *db.Store) (destination.DataWarehouse, error) {
 	switch cfg.Output {
-	case "test":
-		// TODO - In the future, we can create a fake store that follows the MERGE syntax for SQL standard.
-		// Also, the fake library not only needs to support MERGE, but needs to be able to make it easy for us to return
-		// The results via results.Sql (from the database/sql library)
-		// Problem though, is that each DWH seems to implement MERGE differently.
-		// So for now, the fake store will just output the merge command by following Snowflake's syntax.
-		store := db.Store(&mock.DB{
-			Fake: mocks.FakeStore{},
-		})
-		return snowflake.LoadSnowflake(cfg, &store)
 	case constants.Snowflake:
 		s, err := snowflake.LoadSnowflake(cfg, store)
 		if err != nil {
