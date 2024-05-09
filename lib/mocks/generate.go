c722ef6d024918f6c942b86d883f4c5e41db1530
Robin Tang <rtang.cs@gmail.com>
2024-05-08 18:54:37 -0700

    Removing more mocks (#583)

--- lib/mocks/generate.go
+++ lib/mocks/generate.go
@@ -3,3 +3,6 @@ package mocks
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 //counterfeiter:generate -o=db.store.mock.go ../db Store
 //counterfeiter:generate -o=kafkalib.consumer.mock.go ../kafkalib Consumer
+
+//counterfeiter:generate -o=dwh.mock.go ../destination DataWarehouse
+//counterfeiter:generate -o=baseline.mock.go ../destination Baseline
