747100674e667254254d6a4ce56aa0f5bb2be992
Robin Tang <rtang.cs@gmail.com>
2024-05-13 15:21:18 -0700

    Revert "Reload the client upon a rebalance (#611)"
    This reverts commit 2fdd4f884fe3f0cd6176b9ab51f9d59702a0e4c0.

--- lib/kafkalib/consumer.go
+++ lib/kafkalib/consumer.go
@@ -2,8 +2,6 @@ package kafkalib
 
 import (
 	"context"
-	"errors"
-
 	"github.com/segmentio/kafka-go"
 )
 
@@ -11,32 +9,4 @@ type Consumer interface {
 	Close() (err error)
 	ReadMessage(ctx context.Context) (kafka.Message, error)
 	CommitMessages(ctx context.Context, msgs ...kafka.Message) error
-}
-
-type Reader struct {
-	*kafka.Reader
-	config kafka.ReaderConfig
-}
-
-func NewReader(config kafka.ReaderConfig) *Reader {
-	return &Reader{
-		Reader: kafka.NewReader(config),
-		config: config,
-	}
-}
-
-func ShouldReload(err error) bool {
-	if err == nil {
-		return false
-	}
-
-	// Kafka Segment Go doesn't handle reloading the client: https://github.com/segmentio/kafka-go/issues/833
-	// [27] Rebalance In Progress: the coordinator has begun rebalancing the group, the client should rejoin the group
-	return errors.Is(err, kafka.RebalanceInProgress)
-}
-
-func (r *Reader) Reload() {
-	// Close, then reload.
-	_ = r.Close()
-	r.Reader = kafka.NewReader(r.config)
 }
