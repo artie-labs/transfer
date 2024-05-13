5fdca452d520313d054265ff7b82ec5356d171ed
Robin Tang <rtang.cs@gmail.com>
2024-05-13 15:25:20 -0700

    Kafka Revert reload (#634)

--- processes/consumer/kafka.go
+++ processes/consumer/kafka.go
@@ -104,18 +104,12 @@ func StartConsumer(ctx context.Context, cfg config.Config, inMemDB *models.Datab
 				Brokers: cfg.Kafka.BootstrapServers(),
 			}
 
-			kafkaConsumer := kafkalib.NewReader(kafkaCfg)
+			kafkaConsumer := kafka.NewReader(kafkaCfg)
 			topicToConsumer.Add(topic, kafkaConsumer)
 			for {
 				kafkaMsg, err := kafkaConsumer.FetchMessage(ctx)
 				if err != nil {
-					if kafkalib.ShouldReload(err) {
-						slog.Warn("Kafka reader needs to be reloaded", slog.Any("err", err))
-						kafkaConsumer.Reload()
-					} else {
-						slog.With(artie.KafkaMsgLogFields(kafkaMsg)...).Warn("Failed to read kafka message", slog.Any("err", err))
-					}
-
+					slog.With(artie.KafkaMsgLogFields(kafkaMsg)...).Warn("Failed to read kafka message", slog.Any("err", err))
 					time.Sleep(500 * time.Millisecond)
 					continue
 				}
