1b7800ec9cb12726ef92afd8c8a242f8378a2474
Nathan Villaescusa <nathan@artie.so>
2024-05-13 19:06:49 -0700

    Combine

--- lib/sql/dialect.go
+++ lib/sql/dialect.go
@@ -4,6 +4,7 @@ import (
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/columns"
 )
 
 type TableIdentifier interface {
@@ -25,4 +26,14 @@ type Dialect interface {
 	BuildProcessToastColExpression(colName string) string
 	BuildProcessToastStructColExpression(colName string) string
 	BuildDedupeQueries(tableID, stagingTableID TableIdentifier, primaryKeys []string, topicConfig kafkalib.TopicConfig) []string
+	BuildMergeQueries(
+		tableID TableIdentifier,
+		subQuery string,
+		idempotentKey string,
+		primaryKeys []columns.Column,
+		additionalEqualityStrings []string,
+		cols []columns.Column,
+		softDelete bool,
+		containsHardDeletes *bool,
+	) ([]string, error)
 }
