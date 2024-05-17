7066de70e95b7f97bc0925eafcaaeb1595de4781
Robin Tang <rtang.cs@gmail.com>
2024-05-17 10:59:10 -0700

    [BigQuery] Missed one spot for aliasing (#690)

--- clients/bigquery/merge.go
+++ clients/bigquery/merge.go
@@ -2,13 +2,18 @@ package bigquery
 
 import (
 	"fmt"
-	"log/slog"
 
 	"cloud.google.com/go/bigquery"
 
 	"github.com/artie-labs/transfer/clients/shared"
+	"github.com/artie-labs/transfer/lib/array"
+	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/destination/types"
+	"github.com/artie-labs/transfer/lib/kafkalib/partition"
 	"github.com/artie-labs/transfer/lib/optimization"
+	"github.com/artie-labs/transfer/lib/sql"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/columns"
 )
 
 type Row struct {
@@ -34,10 +39,9 @@ func (s *Store) Merge(tableData *optimization.TableData) error {
 			return fmt.Errorf("failed to generate distinct dates: %w", err)
 		}
 
-		mergeString, err := tableData.TopicConfig().BigQueryPartitionSettings.GenerateMergeString(distinctDates)
+		mergeString, err := generateMergeString(tableData.TopicConfig().BigQueryPartitionSettings, s.Dialect(), distinctDates)
 		if err != nil {
-			slog.Warn("Failed to generate merge string", slog.Any("err", err))
-			return err
+			return fmt.Errorf("failed to generate merge string: %w", err)
 		}
 
 		additionalEqualityStrings = []string{mergeString}
@@ -48,4 +52,30 @@ func (s *Store) Merge(tableData *optimization.TableData) error {
 		// BigQuery has DDL quotas.
 		RetryColBackfill: true,
 	})
+}
+
+func generateMergeString(bqSettings *partition.BigQuerySettings, dialect sql.Dialect, values []string) (string, error) {
+	if err := bqSettings.Valid(); err != nil {
+		return "", fmt.Errorf("failed to validate bigQuerySettings: %w", err)
+	}
+
+	if len(values) == 0 {
+		return "", fmt.Errorf("values cannot be empty")
+	}
+
+	switch bqSettings.PartitionType {
+	case "time":
+		switch bqSettings.PartitionBy {
+		case "daily":
+			return fmt.Sprintf(`DATE(%s) IN (%s)`,
+				sql.QuoteTableAliasColumn(
+					constants.TargetAlias,
+					columns.NewColumn(bqSettings.PartitionField, typing.Invalid),
+					dialect,
+				),
+				array.StringsJoinAddSingleQuotes(values)), nil
+		}
+	}
+
+	return "", fmt.Errorf("unexpected partitionType: %s and/or partitionBy: %s", bqSettings.PartitionType, bqSettings.PartitionBy)
 }
--- lib/kafkalib/partition/settings.go
+++ lib/kafkalib/partition/settings.go
@@ -3,8 +3,6 @@ package partition
 import (
 	"fmt"
 	"slices"
-
-	"github.com/artie-labs/transfer/lib/array"
 )
 
 var ValidPartitionTypes = []string{
@@ -27,27 +25,6 @@ type BigQuerySettings struct {
 	PartitionType  string `yaml:"partitionType" json:"partitionType"`
 	PartitionField string `yaml:"partitionField" json:"partitionField"`
 	PartitionBy    string `yaml:"partitionBy" json:"partitionBy"`
-}
-
-// GenerateMergeString this is used as an equality string for the MERGE statement.
-func (b *BigQuerySettings) GenerateMergeString(values []string) (string, error) {
-	if err := b.Valid(); err != nil {
-		return "", fmt.Errorf("failed to validate bigQuerySettings: %w", err)
-	}
-
-	if len(values) == 0 {
-		return "", fmt.Errorf("values cannot be empty")
-	}
-
-	switch b.PartitionType {
-	case "time":
-		switch b.PartitionBy {
-		case "daily":
-			return fmt.Sprintf(`DATE(c.%s) IN (%s)`, b.PartitionField, array.StringsJoinAddSingleQuotes(values)), nil
-		}
-	}
-
-	return "", fmt.Errorf("unexpected partitionType: %s and/or partitionBy: %s", b.PartitionType, b.PartitionBy)
 }
 
 func (b *BigQuerySettings) Valid() error {
