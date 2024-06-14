f69f8584a52e2069875f1d0eda78ce7eacce8300
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-06-13 17:20:35 -0700

    Fix tests

--- clients/bigquery/storagewrite_test.go
+++ clients/bigquery/storagewrite_test.go
@@ -70,6 +70,8 @@ func TestRowToMessage(t *testing.T) {
 		"cInt32":   "1234",
 		"cInt64":   "1234",
 		"cString":  "foo bar",
+		"cNumeric": "3.14159",
 		"cArray":   []any{"foo", "bar"},
+		"cStruct":  `{"baz":["foo","bar"]}`,
 	}, result)
 }
