cf2c562d2670ea75d2d61e46717362aff0dd7357
Robin Tang <rtang.cs@gmail.com>
2024-06-17 09:29:27 -0700

    Adding a comment.

--- clients/bigquery/merge.go
+++ clients/bigquery/merge.go
@@ -51,7 +51,8 @@ func (s *Store) Merge(tableData *optimization.TableData) error {
 		AdditionalEqualityStrings: additionalEqualityStrings,
 		// BigQuery has DDL quotas.
 		RetryColBackfill: true,
-		SubQueryDedupe:   true,
+		// We are using BigQuery's streaming API which doesn't guarantee exactly once semantics
+		SubQueryDedupe: true,
 	})
 }
 
