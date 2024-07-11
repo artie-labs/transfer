4036e766d568760db1465e73e4aa3d806727d50b
Robin Tang <rtang.cs@gmail.com>
2024-07-11 11:30:38 -0700

    [Kafka] Refactor the way we handle Kafka connections (#789)

---
+++ lib/kafkalib/connection_test.go
@@ -0,0 +1,69 @@
+package kafkalib
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnection_Mechanism(t *testing.T) {
+	{
+		c := NewConnection(false, false, "", "")
+		assert.Equal(t, Plain, c.Mechanism())
+	}
+	{
+		c := NewConnection(false, false, "username", "password")
+		assert.Equal(t, ScramSha512, c.Mechanism())
+
+		// AWS MSK IAM is enabled, but SCRAM is preferred
+		c = NewConnection(true, false, "username", "password")
+		assert.Equal(t, ScramSha512, c.Mechanism())
+	}
+	{
+		c := NewConnection(true, false, "", "")
+		assert.Equal(t, AwsMskIam, c.Mechanism())
+	}
+}
+
+func TestConnection_Dialer(t *testing.T) {
+	ctx := context.Background()
+	{
+		// Plain
+		c := NewConnection(false, false, "", "")
+		dialer, err := c.Dialer(ctx)
+		assert.NoError(t, err)
+		assert.Nil(t, dialer.TLS)
+		assert.Nil(t, dialer.SASLMechanism)
+	}
+	{
+		// SCRAM enabled with TLS
+		c := NewConnection(false, false, "username", "password")
+		dialer, err := c.Dialer(ctx)
+		assert.NoError(t, err)
+		assert.NotNil(t, dialer.TLS)
+		assert.NotNil(t, dialer.SASLMechanism)
+
+		// w/o TLS
+		c = NewConnection(false, true, "username", "password")
+		dialer, err = c.Dialer(ctx)
+		assert.NoError(t, err)
+		assert.Nil(t, dialer.TLS)
+		assert.NotNil(t, dialer.SASLMechanism)
+	}
+	{
+		// AWS IAM w/ TLS
+		c := NewConnection(true, false, "", "")
+		dialer, err := c.Dialer(ctx)
+		assert.NoError(t, err)
+		assert.NotNil(t, dialer.TLS)
+		assert.NotNil(t, dialer.SASLMechanism)
+
+		// w/o TLS (still enabled because AWS doesn't support not having TLS)
+		c = NewConnection(true, true, "", "")
+		dialer, err = c.Dialer(ctx)
+		assert.NoError(t, err)
+		assert.NotNil(t, dialer.TLS)
+		assert.NotNil(t, dialer.SASLMechanism)
+	}
+}
