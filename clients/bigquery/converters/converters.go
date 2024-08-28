e8ebb3458332db1d0294869a9fb2f1bfddf6d37a
Robin Tang <rtang.cs@gmail.com>
2024-08-28 11:53:08 -0700

    [BigQuery] Float converters (#858)

--- clients/bigquery/converters/converters.go
+++ clients/bigquery/converters/converters.go
@@ -57,3 +57,34 @@ func (BooleanConverter) Convert(value any) (any, error) {
 		return nil, fmt.Errorf("expected bool received %T with value %v", value, value)
 	}
 }
+
+type Float64Converter struct{}
+
+func (Float64Converter) Convert(value any) (any, error) {
+	switch castedVal := value.(type) {
+	case float32:
+		return float64(castedVal), nil
+	case float64:
+		return castedVal, nil
+	case int32:
+		return float64(castedVal), nil
+	case int64:
+		return float64(castedVal), nil
+	case *decimal.Decimal:
+		floatValue, err := castedVal.Value().Float64()
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert decimal to float64: %w", err)
+		}
+
+		return floatValue, nil
+	case string:
+		floatValue, err := strconv.ParseFloat(castedVal, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse string %q to float64: %w", castedVal, err)
+		}
+
+		return floatValue, nil
+	default:
+		return nil, fmt.Errorf("failed to run float64 converter, unexpected type %T with value %v", value, value)
+	}
+}
