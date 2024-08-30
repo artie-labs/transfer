75f0bd64909a6edc4f19156b8542869326404347
Robin Tang <rtang.cs@gmail.com>
2024-08-30 13:53:16 -0700

    Simplify.

--- lib/cdc/util/optional_schema.go
+++ lib/cdc/util/optional_schema.go
@@ -1,29 +1,21 @@
 package util
 
 import (
-	"log/slog"
-
 	"github.com/artie-labs/transfer/lib/debezium"
 	"github.com/artie-labs/transfer/lib/typing"
 )
 
 func (s *SchemaEventPayload) GetOptionalSchema() map[string]typing.KindDetails {
-	fieldsObject := s.Schema.GetSchemaFromLabel(debezium.After)
-	if fieldsObject == nil {
-		// AFTER schema does not exist.
-		return nil
-	}
-
-	schema := make(map[string]typing.KindDetails)
-	for _, field := range fieldsObject.Fields {
-		kd := field.ToKindDetails()
-		if kd == typing.Invalid {
-			slog.Warn("Skipping field from optional schema b/c we cannot determine the data type", slog.String("field", field.FieldName))
-			continue
+	if fieldsObject := s.Schema.GetSchemaFromLabel(debezium.After); fieldsObject != nil {
+		schema := make(map[string]typing.KindDetails)
+		for _, field := range fieldsObject.Fields {
+			if kd := field.ToKindDetails(); kd != typing.Invalid {
+				schema[field.FieldName] = kd
+			}
 		}
 
-		schema[field.FieldName] = kd
+		return schema
 	}
 
-	return schema
+	return nil
 }
