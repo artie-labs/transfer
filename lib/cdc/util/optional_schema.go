71fc8b7bd551651d9e34db79d689761a66865f63
Robin Tang <rtang.cs@gmail.com>
2024-08-30 14:26:08 -0700

    Remove warning from optional schema (#863)

--- lib/cdc/util/optional_schema.go
+++ lib/cdc/util/optional_schema.go
@@ -1,8 +1,6 @@
 package util
 
 import (
-	"log/slog"
-
 	"github.com/artie-labs/transfer/lib/debezium"
 	"github.com/artie-labs/transfer/lib/typing"
 )
@@ -10,19 +8,14 @@ import (
 func (s *SchemaEventPayload) GetOptionalSchema() map[string]typing.KindDetails {
 	fieldsObject := s.Schema.GetSchemaFromLabel(debezium.After)
 	if fieldsObject == nil {
-		// AFTER schema does not exist.
 		return nil
 	}
 
 	schema := make(map[string]typing.KindDetails)
 	for _, field := range fieldsObject.Fields {
-		kd := field.ToKindDetails()
-		if kd == typing.Invalid {
-			slog.Warn("Skipping field from optional schema b/c we cannot determine the data type", slog.String("field", field.FieldName))
-			continue
+		if kd := field.ToKindDetails(); kd != typing.Invalid {
+			schema[field.FieldName] = kd
 		}
-
-		schema[field.FieldName] = kd
 	}
 
 	return schema
