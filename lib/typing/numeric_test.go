aa080bbc3b22fde66d657b708f8a86e05d63affa
Robin Tang <rtang.cs@gmail.com>
2024-09-03 13:31:16 -0700

    Rewriting tests (#867)

--- lib/typing/numeric_test.go
+++ lib/typing/numeric_test.go
@@ -9,82 +9,70 @@ import (
 )
 
 func TestParseNumeric(t *testing.T) {
-	type _testCase struct {
-		parameters          []string
-		expectedKindDetails KindDetails
-		expectedPrecision   int32
-		expectedScale       int32
-	}
-
-	testCases := []_testCase{
+	{
+		// Invalid
 		{
-			parameters:          []string{},
-			expectedKindDetails: Invalid,
-		},
+			result := ParseNumeric([]string{})
+			assert.Equal(t, Invalid, result)
+		}
 		{
-			parameters:          []string{"5", "a"},
-			expectedKindDetails: Invalid,
-		},
+			result := ParseNumeric([]string{"5", "a"})
+			assert.Equal(t, Invalid, result)
+		}
 		{
-			parameters:          []string{"b", "5"},
-			expectedKindDetails: Invalid,
-		},
+			result := ParseNumeric([]string{"b", "5"})
+			assert.Equal(t, Invalid, result)
+		}
 		{
-			parameters:          []string{"a", "b"},
-			expectedKindDetails: Invalid,
-		},
+			result := ParseNumeric([]string{"a", "b"})
+			assert.Equal(t, Invalid, result)
+		}
 		{
-			parameters:          []string{"1", "2", "3"},
-			expectedKindDetails: Invalid,
-		},
+			result := ParseNumeric([]string{"1", "2", "3"})
+			assert.Equal(t, Invalid, result)
+		}
 		{
-			parameters:          []string{"5", " 2"},
-			expectedKindDetails: EDecimal,
-			expectedPrecision:   5,
-			expectedScale:       2,
-		},
-		{
-			parameters:          []string{"5", "2"},
-			expectedKindDetails: EDecimal,
-			expectedPrecision:   5,
-			expectedScale:       2,
-		},
-		{
-			parameters:          []string{"39", "6"},
-			expectedKindDetails: EDecimal,
-			expectedPrecision:   39,
-			expectedScale:       6,
-		},
-		{
-			parameters:          []string{"5"},
-			expectedKindDetails: Integer,
-			expectedPrecision:   5,
-			expectedScale:       0,
-		},
-		{
-			parameters:          []string{"5", "0"},
-			expectedKindDetails: Integer,
-			expectedPrecision:   5,
-			expectedScale:       0,
-		},
-		{
-			parameters:          []string{fmt.Sprint(math.MaxInt32), fmt.Sprint(math.MaxInt32)},
-			expectedKindDetails: EDecimal,
-			expectedPrecision:   math.MaxInt32,
-			expectedScale:       math.MaxInt32,
-		},
-	}
-
-	for _, testCase := range testCases {
-		result := ParseNumeric(testCase.parameters)
-		assert.Equal(t, testCase.expectedKindDetails.Kind, result.Kind, testCase.parameters)
-		if result.ExtendedDecimalDetails != nil {
-			assert.Equal(t, testCase.expectedScale, result.ExtendedDecimalDetails.Scale(), testCase.parameters)
-			assert.Equal(t, testCase.expectedPrecision, result.ExtendedDecimalDetails.Precision(), testCase.parameters)
+			// Test values that are larger than [math.MaxInt32].
+			assert.Equal(t, Invalid, ParseNumeric([]string{"10", fmt.Sprint(math.MaxInt32 + 1)}))
+			assert.Equal(t, Invalid, ParseNumeric([]string{fmt.Sprint(math.MaxInt32 + 1), "10"}))
 		}
 	}
-
-	// Test values that are larger than [math.MaxInt32].
-	assert.Equal(t, "invalid", ParseNumeric([]string{"10", fmt.Sprint(math.MaxInt32 + 1)}).Kind)
-	assert.Equal(t, "invalid", ParseNumeric([]string{fmt.Sprint(math.MaxInt32 + 1), "10"}).Kind)
+	{
+		// Decimals
+		{
+			result := ParseNumeric([]string{"5", "2"})
+			assert.Equal(t, EDecimal.Kind, result.Kind)
+			assert.Equal(t, int32(5), result.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(2), result.ExtendedDecimalDetails.Scale())
+		}
+		{
+			result := ParseNumeric([]string{"5", "  2     "})
+			assert.Equal(t, EDecimal.Kind, result.Kind)
+			assert.Equal(t, int32(5), result.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(2), result.ExtendedDecimalDetails.Scale())
+		}
+		{
+			result := ParseNumeric([]string{"39", "6"})
+			assert.Equal(t, EDecimal.Kind, result.Kind)
+			assert.Equal(t, int32(39), result.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(6), result.ExtendedDecimalDetails.Scale())
+		}
+		{
+			result := ParseNumeric([]string{fmt.Sprint(math.MaxInt32), fmt.Sprint(math.MaxInt32)})
+			assert.Equal(t, EDecimal.Kind, result.Kind)
+			assert.Equal(t, int32(math.MaxInt32), result.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(math.MaxInt32), result.ExtendedDecimalDetails.Scale())
+		}
+	}
+	{
+		// Integer
+		{
+			result := ParseNumeric([]string{"5"})
+			assert.Equal(t, Integer, result)
+		}
+		{
+			result := ParseNumeric([]string{"5", "0"})
+			assert.Equal(t, Integer, result)
+		}
+	}
 }
