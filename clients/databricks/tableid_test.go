f498888e9cd5b0c12d9b54e8e6800c3d8e6a36b0
Robin Tang <rtang.cs@gmail.com>
2024-10-03 11:50:51 -0700

    Supporting Databricks - Part Three (#940)

---
+++ clients/databricks/tableid_test.go
@@ -0,0 +1,33 @@
+package databricks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTableIdentifier_WithTable(t *testing.T) {
+	tableID := NewTableIdentifier("database", "schema", "foo")
+	tableID2 := tableID.WithTable("bar")
+	typedTableID2, ok := tableID2.(TableIdentifier)
+	assert.True(t, ok)
+	assert.Equal(t, "database", typedTableID2.Database())
+	assert.Equal(t, "schema", typedTableID2.Schema())
+	assert.Equal(t, "bar", tableID2.Table())
+}
+
+func TestTableIdentifier_FullyQualifiedName(t *testing.T) {
+	// Table name that is not a reserved word:
+	assert.Equal(t, "`database`.`schema`.`foo`", NewTableIdentifier("database", "schema", "foo").FullyQualifiedName())
+
+	// Table name that is a reserved word:
+	assert.Equal(t, "`database`.`schema`.`table`", NewTableIdentifier("database", "schema", "table").FullyQualifiedName())
+}
+
+func TestTableIdentifier_EscapedTable(t *testing.T) {
+	// Table name that is not a reserved word:
+	assert.Equal(t, "`foo`", NewTableIdentifier("database", "schema", "foo").EscapedTable())
+
+	// Table name that is a reserved word:
+	assert.Equal(t, "`table`", NewTableIdentifier("database", "schema", "table").EscapedTable())
+}
