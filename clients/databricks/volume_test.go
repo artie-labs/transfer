f498279e327f1da02244931f032aa7555fbfe806
Robin Tang <rtang.cs@gmail.com>
2024-10-07 16:02:26 -0700

    Tests.

--- clients/databricks/volume_test.go
+++ clients/databricks/volume_test.go
@@ -1,8 +1,12 @@
 package databricks
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,5 +40,33 @@ func TestNewVolume(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, "name", volume.name)
 		assert.Equal(t, "path", volume.path)
+	}
+}
+
+func newVolume(volName string) Volume {
+	return Volume{
+		name: volName,
+	}
+}
+
+func TestVolume_ShouldDelete(t *testing.T) {
+	{
+		// Should delete
+		volume := Volume{name: "name.csv"}
+		assert.False(t, volume.ShouldDelete())
+	}
+	{
+		// Tables that are eligible to be dropped
+		tablesToDrop := []string{
+			"transactions___ARTIE_48GJC_1723663043",
+			fmt.Sprintf("expired_tbl_%s_suffix_%d", constants.ArtiePrefix, time.Now().Add(-1*constants.TemporaryTableTTL).Unix()),
+			fmt.Sprintf("artie_%s_suffix_%d", constants.ArtiePrefix, time.Now().Add(-1*constants.TemporaryTableTTL).Unix()),
+		}
+
+		for _, tblToDelete := range tablesToDrop {
+			assert.True(t, newVolume(strings.ToLower(tblToDelete)).ShouldDelete(), tblToDelete)
+			assert.True(t, newVolume(strings.ToUpper(tblToDelete)).ShouldDelete(), tblToDelete)
+			assert.True(t, newVolume(tblToDelete).ShouldDelete(), tblToDelete)
+		}
 	}
 }
