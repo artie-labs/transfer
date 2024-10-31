b4158bedc6e5f0960bbb17978b52a44b89c3a1b5
Robin Tang <rtang.cs@gmail.com>
2024-10-30 18:22:42 -0700

    Clean up.

--- clients/mssql/values.go
+++ clients/mssql/values.go
@@ -34,6 +34,13 @@ func parseValue(colVal any, colKind columns.Column) (any, error) {
 		}
 
 		return _time, nil
+	case typing.TimestampNTZ.Kind:
+		_time, err := ext.ParseTimestampNTZFromInterface(colVal)
+		if err != nil {
+			return "", fmt.Errorf("failed to cast colVal as time.Time, colVal: '%v', err: %w", colVal, err)
+		}
+
+		return _time, nil
 	case typing.ETime.Kind:
 		if err := colKind.KindDetails.EnsureExtendedTimeDetails(); err != nil {
 			return "", err
