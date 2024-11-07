af6d277b486019de8439eb6082ca95805cbd4934
Robin Tang <rtang.cs@gmail.com>
2024-11-06 21:32:17 -0800

    Update.

---
+++ clients/bigquery/dialect/typing_test.go
@@ -0,0 +1,170 @@
+package dialect
+
+import (
+	"testing"
+
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBigQueryDialect_DataTypeForKind(t *testing.T) {
+	{
+		// String
+		{
+			assert.Equal(t, "string", BigQueryDialect{}.DataTypeForKind(typing.String, false))
+		}
+		{
+			assert.Equal(t, "string", BigQueryDialect{}.DataTypeForKind(typing.KindDetails{Kind: typing.String.Kind, OptionalStringPrecision: typing.ToPtr(int32(12345))}, true))
+		}
+	}
+}
+
+func TestBigQueryDialect_KindForDataType_NoDataLoss(t *testing.T) {
+	kindDetails := []typing.KindDetails{
+		typing.TimestampTZ,
+		typing.Time,
+		typing.Date,
+		typing.String,
+		typing.Boolean,
+		typing.Struct,
+	}
+
+	for _, kindDetail := range kindDetails {
+		kd, err := BigQueryDialect{}.KindForDataType(BigQueryDialect{}.DataTypeForKind(kindDetail, false), "")
+		assert.NoError(t, err)
+		assert.Equal(t, kindDetail, kd)
+	}
+}
+
+func TestBigQueryDialect_KindForDataType(t *testing.T) {
+	dialect := BigQueryDialect{}
+	{
+		// Booleans
+		for _, boolKind := range []string{"bool", "boolean"} {
+			kd, err := dialect.KindForDataType(boolKind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.Boolean, kd)
+		}
+	}
+	{
+		// Strings
+		for _, stringKind := range []string{"string", "varchar", "string (10)", "STrinG"} {
+			kd, err := dialect.KindForDataType(stringKind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.String, kd)
+		}
+	}
+	{
+		// Numeric
+		{
+			// Invalid
+			{
+				kd, err := dialect.KindForDataType("numeric(1,2,3)", "")
+				assert.ErrorContains(t, err, "invalid number of parts: 3")
+				assert.Equal(t, typing.Invalid, kd)
+			}
+			{
+				_, err := dialect.KindForDataType("numeric(5", "")
+				assert.ErrorContains(t, err, "missing closing parenthesis")
+			}
+		}
+		{
+			// Numeric(5)
+			kd, err := dialect.KindForDataType("NUMERIC(5)", "")
+			assert.NoError(t, err)
+
+			assert.Equal(t, typing.EDecimal.Kind, kd.Kind)
+			assert.Equal(t, int32(5), kd.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(0), kd.ExtendedDecimalDetails.Scale())
+			assert.Equal(t, "NUMERIC(5, 0)", kd.ExtendedDecimalDetails.BigQueryKind())
+
+		}
+		{
+			// Numeric(5, 0)
+			kd, err := dialect.KindForDataType("NUMERIC(5, 0)", "")
+			assert.NoError(t, err)
+
+			assert.Equal(t, typing.EDecimal.Kind, kd.Kind)
+			assert.Equal(t, int32(5), kd.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(0), kd.ExtendedDecimalDetails.Scale())
+			assert.Equal(t, "NUMERIC(5, 0)", kd.ExtendedDecimalDetails.BigQueryKind())
+		}
+		{
+			// Numeric(5, 2)
+			kd, err := dialect.KindForDataType("numeric(5, 2)", "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.EDecimal.Kind, kd.Kind)
+			assert.Equal(t, int32(5), kd.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(2), kd.ExtendedDecimalDetails.Scale())
+		}
+		{
+			// BigNumeric(5, 2)
+			kd, err := dialect.KindForDataType("bignumeric(5, 2)", "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.EDecimal.Kind, kd.Kind)
+			assert.Equal(t, int32(5), kd.ExtendedDecimalDetails.Precision())
+			assert.Equal(t, int32(2), kd.ExtendedDecimalDetails.Scale())
+		}
+	}
+	{
+		// Integers
+		for _, intKind := range []string{"int", "integer", "inT64"} {
+			kd, err := dialect.KindForDataType(intKind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.Integer, kd, intKind)
+		}
+	}
+	{
+		// Arrays
+		for _, arrayKind := range []string{"array<integer>", "array<string>"} {
+			kd, err := dialect.KindForDataType(arrayKind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.Array, kd, arrayKind)
+		}
+	}
+	{
+		// Structs
+		for _, structKind := range []string{"struct<foo STRING>", "record", "json"} {
+			kd, err := dialect.KindForDataType(structKind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.Struct, kd, structKind)
+		}
+	}
+	{
+		// Date
+		kd, err := dialect.KindForDataType("date", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Date, kd)
+	}
+	{
+		// Time
+		kd, err := dialect.KindForDataType("time", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Time, kd)
+	}
+	{
+		// Timestamp (Timestamp TZ)
+		kd, err := dialect.KindForDataType("timestamp", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.TimestampTZ, kd)
+	}
+	{
+		// Datetime (Timestamp NTZ)
+		kd, err := dialect.KindForDataType("datetime", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.TimestampNTZ, kd)
+	}
+	{
+		// Invalid types
+		{
+			kd, err := dialect.KindForDataType("", "")
+			assert.ErrorContains(t, err, `unsupported data type: ""`)
+			assert.Equal(t, typing.Invalid, kd)
+		}
+		{
+			kd, err := dialect.KindForDataType("foo", "")
+			assert.ErrorContains(t, err, `unsupported data type: "foo"`)
+			assert.Equal(t, typing.Invalid, kd)
+		}
+	}
+}
---
+++ clients/mssql/dialect/typing.go
@@ -0,0 +1,114 @@
+package dialect
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/artie-labs/transfer/lib/sql"
+	"github.com/artie-labs/transfer/lib/typing"
+)
+
+func (MSSQLDialect) DataTypeForKind(kindDetails typing.KindDetails, isPk bool) string {
+	// Primary keys cannot exceed 900 chars in length.
+	// https://learn.microsoft.com/en-us/sql/relational-databases/tables/primary-and-foreign-key-constraints?view=sql-server-ver16#PKeys
+	const maxVarCharLengthForPrimaryKey = 900
+
+	switch kindDetails.Kind {
+	case typing.Float.Kind:
+		return "float"
+	case typing.Integer.Kind:
+		return "bigint"
+	case typing.Struct.Kind, typing.Array.Kind:
+		return "NVARCHAR(MAX)"
+	case typing.String.Kind:
+		if kindDetails.OptionalStringPrecision != nil {
+			precision := *kindDetails.OptionalStringPrecision
+			if isPk {
+				precision = min(maxVarCharLengthForPrimaryKey, precision)
+			}
+
+			return fmt.Sprintf("VARCHAR(%d)", precision)
+		}
+
+		if isPk {
+			return fmt.Sprintf("VARCHAR(%d)", maxVarCharLengthForPrimaryKey)
+		}
+
+		return "VARCHAR(MAX)"
+	case typing.Boolean.Kind:
+		return "BIT"
+	case typing.Date.Kind:
+		return "DATE"
+	case typing.Time.Kind:
+		return "TIME"
+	case typing.TimestampNTZ.Kind:
+		// Using datetime2 because it's the recommendation, and it provides more precision: https://stackoverflow.com/a/1884088
+		return "datetime2"
+	case typing.TimestampTZ.Kind:
+		return "datetimeoffset"
+	case typing.EDecimal.Kind:
+		return kindDetails.ExtendedDecimalDetails.MsSQLKind()
+	}
+
+	return kindDetails.Kind
+}
+
+func (MSSQLDialect) KindForDataType(rawType string, stringPrecision string) (typing.KindDetails, error) {
+	rawType = strings.ToLower(rawType)
+	if strings.HasPrefix(rawType, "numeric") {
+		_, parameters, err := sql.ParseDataTypeDefinition(rawType)
+		if err != nil {
+			return typing.Invalid, err
+		}
+		return typing.ParseNumeric(parameters)
+	}
+
+	switch rawType {
+	case
+		"char",
+		"varchar",
+		"nchar",
+		"nvarchar",
+		"ntext":
+		var strPrecision *int32
+		precision, err := strconv.ParseInt(stringPrecision, 10, 32)
+		if err == nil {
+			strPrecision = typing.ToPtr(int32(precision))
+		}
+
+		// precision of -1 means it's MAX.
+		if precision == -1 {
+			strPrecision = nil
+		}
+
+		return typing.KindDetails{
+			Kind:                    typing.String.Kind,
+			OptionalStringPrecision: strPrecision,
+		}, nil
+	case
+		"smallint",
+		"tinyint",
+		"bigint",
+		"int":
+		return typing.Integer, nil
+	case "float", "real":
+		return typing.Float, nil
+	case
+		"datetime",
+		"datetime2":
+		return typing.TimestampNTZ, nil
+	case "datetimeoffset":
+		return typing.TimestampTZ, nil
+	case "time":
+		return typing.Time, nil
+	case "date":
+		return typing.Date, nil
+	case "bit":
+		return typing.Boolean, nil
+	case "text":
+		return typing.String, nil
+	default:
+		return typing.Invalid, fmt.Errorf("unsupported data type: %q", rawType)
+	}
+}
