405c463ca465118200f15bf34eb467c9fa75e5b9
Robin Tang <rtang.cs@gmail.com>
2024-11-14 12:55:15 -0800

    bool

--- lib/typing/ext/variables_test.go
+++ lib/typing/ext/variables_test.go
@@ -3,11 +3,11 @@ package ext
 import "testing"
 
 func TestSupportedDateTimeLayoutsUniqueness(t *testing.T) {
-	layouts := make(map[string]struct{})
+	layouts := make(map[string]bool)
 	for _, layout := range supportedDateTimeLayouts {
 		if _, ok := layouts[layout]; ok {
 			t.Errorf("layout %q is duplicated", layout)
 		}
-		layouts[layout] = struct{}{}
+		layouts[layout] = true
 	}
 }
