637c81f80ad5a4aeafce2a264d3f97736e000a1a
Robin Tang <rtang.cs@gmail.com>
2024-11-20 11:51:36 -0800

    [DDL] Create table pass in columns (#1062)

--- clients/shared/append.go
+++ clients/shared/append.go
@@ -32,7 +32,7 @@ func Append(ctx context.Context, dwh destination.DataWarehouse, tableData *optim
 
 	tableID := dwh.IdentifierFor(tableData.TopicConfig(), tableData.Name())
 	if tableConfig.CreateTable() {
-		if err = CreateTable(ctx, dwh, tableData, tableConfig, opts.ColumnSettings, tableID, false); err != nil {
+		if err = CreateTable(ctx, dwh, tableData, tableConfig, opts.ColumnSettings, tableID, false, targetKeysMissing); err != nil {
 			return fmt.Errorf("failed to create table: %w", err)
 		}
 	} else {
