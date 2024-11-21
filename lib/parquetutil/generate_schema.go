89337c891e45e56954a83eb84a8db9b75c52e078
Robin Tang <rtang.cs@gmail.com>
2024-11-21 11:28:35 -0800

    Clean.

--- lib/parquetutil/generate_schema.go
+++ lib/parquetutil/generate_schema.go
@@ -1,64 +1,8 @@
 package parquetutil
 
 import (
-	"encoding/json"
-	"strings"
-
-	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/artie-labs/transfer/lib/typing/columns"
 )
-
-type ParquetColumn struct {
-	originalName string
-	cleanedName  string
-	column       columns.Column
-}
-
-func (pc ParquetColumn) OriginalName() string {
-	return pc.originalName
-}
-
-func (pc ParquetColumn) CleanedName() string {
-	return pc.cleanedName
-}
-
-func (pc ParquetColumn) Column() columns.Column {
-	return pc.column
-}
-
-func NewParquetColumn(colName string, column columns.Column) ParquetColumn {
-	return ParquetColumn{
-		originalName: colName,
-		cleanedName:  strings.TrimPrefix(colName, "__"),
-		column:       column,
-	}
-}
-
-func GenerateJSONSchema(columns []ParquetColumn) (string, error) {
-	var fields []typing.Field
-	for _, column := range columns {
-		// We don't need to escape the column name here.
-		field, err := column.column.KindDetails.ParquetAnnotation(column.cleanedName)
-		if err != nil {
-			return "", err
-		}
-
-		fields = append(fields, *field)
-	}
-
-	schemaBytes, err := json.Marshal(
-		typing.Field{
-			Tag:    typing.FieldTag{Name: "parquet-go-root", RepetitionType: typing.ToPtr("REQUIRED")}.String(),
-			Fields: fields,
-		},
-	)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(schemaBytes), nil
-}
 
 func GenerateCSVSchema(columns []columns.Column) ([]string, error) {
 	var fields []string
