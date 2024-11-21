307eb539d1c4f260a61ca3e572a9d004b18a3731
Robin Tang <rtang.cs@gmail.com>
2024-11-20 16:01:26 -0800

    Adding log.

--- clients/bigquery/dialect/typing.go
+++ clients/bigquery/dialect/typing.go
@@ -2,6 +2,7 @@ package dialect
 
 import (
 	"fmt"
+	"log/slog"
 	"strings"
 
 	"github.com/artie-labs/transfer/lib/config"
@@ -34,6 +35,7 @@ func (BigQueryDialect) DataTypeForKind(kindDetails typing.KindDetails, _ bool, s
 		// We should be using TIMESTAMP since it's an absolute point in time.
 		return "timestamp"
 	case typing.EDecimal.Kind:
+		slog.Info("####'", kindDetails.ExtendedDecimalDetails.Precision(), kindDetails.ExtendedDecimalDetails.Scale(), "settings", settings.BigQueryNumericForVariableNumeric)
 		return kindDetails.ExtendedDecimalDetails.BigQueryKind(settings.BigQueryNumericForVariableNumeric)
 	}
 
