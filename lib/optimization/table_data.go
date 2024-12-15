b9c2d4a77ac5a48a35070882b61e858168d8a400
Robin Tang <rtang.cs@gmail.com>
2024-12-14 18:06:32 -0800

    Adding logging.

--- lib/optimization/table_data.go
+++ lib/optimization/table_data.go
@@ -2,6 +2,7 @@ package optimization
 
 import (
 	"fmt"
+	"log/slog"
 	"strings"
 	"time"
 
@@ -154,7 +155,7 @@ func (t *TableData) InsertRow(pk string, rowData map[string]any, delete bool) {
 	// If prevRow doesn't exist, it'll be 0, which is a no-op.
 	t.approxSize += newRowSize - prevRowSize
 	t.rowsData[pk] = rowData
-
+	slog.Info("Inserting row", slog.String("pk", pk), slog.Any("rowData", t.rowsData[pk]))
 	if !delete {
 		t.containOtherOperations = true
 	} else if delete && !t.topicConfig.SoftDelete {
