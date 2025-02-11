ba1e2fa7f39fe01d26afb905ad43c29165086019
Robin Tang <rtang.cs@gmail.com>
2025-02-10 19:06:20 -0800

    Supporting Iceberg - Part 2 (#1138)

--- clients/iceberg/dialect/data_types.go
+++ clients/iceberg/dialect/data_types.go
@@ -1,21 +1,77 @@
 package dialect
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/artie-labs/transfer/lib/config"
+	"github.com/artie-labs/transfer/lib/sql"
 	"github.com/artie-labs/transfer/lib/typing"
 )
 
-func (IcebergDialect) DataTypeForKind(
-	kindDetails typing.KindDetails,
-	// Primary key doesn't matter for Iceberg
-	_ bool,
-	settings config.SharedDestinationColumnSettings,
-) string {
-	// TODO:
-	panic("not implemented")
+// Ref: https://iceberg.apache.org/docs/latest/spark-getting-started/#iceberg-type-to-spark-type
+
+func (IcebergDialect) DataTypeForKind(kindDetails typing.KindDetails, _ bool, _ config.SharedDestinationColumnSettings) string {
+	switch kindDetails.Kind {
+	case typing.Boolean.Kind:
+		return "BOOLEAN"
+	case
+		typing.Array.Kind,
+		typing.Struct.Kind,
+		typing.String.Kind,
+		typing.Time.Kind:
+		return "STRING"
+	case typing.Float.Kind:
+		return "DOUBLE"
+	case typing.EDecimal.Kind:
+		return kindDetails.ExtendedDecimalDetails.IcebergKind()
+	case typing.Integer.Kind:
+		if kindDetails.OptionalIntegerKind != nil {
+			switch *kindDetails.OptionalIntegerKind {
+			case typing.SmallIntegerKind, typing.IntegerKind:
+				return "INTEGER"
+			}
+		}
+		return "LONG"
+	case typing.Date.Kind:
+		return "DATE"
+	case typing.TimestampNTZ.Kind:
+		return "TIMESTAMP WITHOUT TIMEZONE"
+	case typing.TimestampTZ.Kind:
+		return "TIMESTAMP WITH TIMEZONE"
+	default:
+		return kindDetails.Kind
+	}
 }
 
 func (IcebergDialect) KindForDataType(rawType string, _ string) (typing.KindDetails, error) {
-	// TODO:
-	panic("not implemented")
+	rawType = strings.ToLower(rawType)
+	if strings.HasPrefix(rawType, "decimal") {
+		_, parameters, err := sql.ParseDataTypeDefinition(rawType)
+		if err != nil {
+			return typing.Invalid, err
+		}
+		return typing.ParseNumeric(parameters)
+	}
+
+	switch rawType {
+	case "boolean":
+		return typing.Boolean, nil
+	case "integer":
+		return typing.KindDetails{Kind: typing.Integer.Kind, OptionalIntegerKind: typing.ToPtr(typing.IntegerKind)}, nil
+	case "long":
+		return typing.KindDetails{Kind: typing.Integer.Kind, OptionalIntegerKind: typing.ToPtr(typing.BigIntegerKind)}, nil
+	case "double", "float":
+		return typing.Float, nil
+	case "string", "binary", "uuid", "fixed":
+		return typing.String, nil
+	case "date":
+		return typing.Date, nil
+	case "timestamp with timezone":
+		return typing.TimestampTZ, nil
+	case "timestamp without timezone":
+		return typing.TimestampNTZ, nil
+	default:
+		return typing.Invalid, fmt.Errorf("unsupported data type: %q", rawType)
+	}
 }
---
+++ clients/iceberg/dialect/data_types_test.go
@@ -0,0 +1,139 @@
+package dialect
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/artie-labs/transfer/lib/config"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/decimal"
+)
+
+func TestIcebergDialect_DataTypeForKind(t *testing.T) {
+	_dialect := IcebergDialect{}
+
+	// Boolean
+	assert.Equal(t, "BOOLEAN", _dialect.DataTypeForKind(typing.Boolean, false, config.SharedDestinationColumnSettings{}))
+
+	{
+		// String and related data types
+		for _, kd := range []typing.KindDetails{typing.String, typing.Time, typing.Array, typing.Struct} {
+			assert.Equal(t, "STRING", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+	}
+	{
+		// Float
+		assert.Equal(t, "DOUBLE", _dialect.DataTypeForKind(typing.Float, false, config.SharedDestinationColumnSettings{}))
+
+		// EDecimal
+		{
+			// DECIMAL(5, 2)
+			kd := typing.NewDecimalDetailsFromTemplate(typing.EDecimal, decimal.NewDetails(5, 2))
+			assert.Equal(t, "DECIMAL(5, 2)", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+		{
+			// DECIMAL(39, 2) - Exceeds the max precision, so it will become a string.
+			kd := typing.NewDecimalDetailsFromTemplate(typing.EDecimal, decimal.NewDetails(39, 2))
+			assert.Equal(t, "STRING", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+
+		// Integers
+		{
+			// Not specified
+			assert.Equal(t, "LONG", _dialect.DataTypeForKind(typing.Integer, false, config.SharedDestinationColumnSettings{}))
+		}
+		{
+			// SmallIntegerKind
+			kd := typing.Integer
+			kd.OptionalIntegerKind = typing.ToPtr(typing.SmallIntegerKind)
+			assert.Equal(t, "INTEGER", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+		{
+			// IntegerKind
+			kd := typing.Integer
+			kd.OptionalIntegerKind = typing.ToPtr(typing.IntegerKind)
+			assert.Equal(t, "INTEGER", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+		{
+			// BigIntegerKind
+			kd := typing.Integer
+			kd.OptionalIntegerKind = typing.ToPtr(typing.BigIntegerKind)
+			assert.Equal(t, "LONG", _dialect.DataTypeForKind(kd, false, config.SharedDestinationColumnSettings{}))
+		}
+	}
+
+	// Date
+	assert.Equal(t, "DATE", _dialect.DataTypeForKind(typing.Date, false, config.SharedDestinationColumnSettings{}))
+
+	// TimestampNTZ
+	assert.Equal(t, "TIMESTAMP WITHOUT TIMEZONE", _dialect.DataTypeForKind(typing.TimestampNTZ, false, config.SharedDestinationColumnSettings{}))
+
+	// TimestampTZ
+	assert.Equal(t, "TIMESTAMP WITH TIMEZONE", _dialect.DataTypeForKind(typing.TimestampTZ, false, config.SharedDestinationColumnSettings{}))
+}
+
+func TestIcebergDialect_KindForDataType(t *testing.T) {
+	_dialect := IcebergDialect{}
+	{
+		// Boolean
+		kd, err := _dialect.KindForDataType("BOOLEAN", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Boolean, kd)
+	}
+	{
+		// Decimal (10, 2)
+		kd, err := _dialect.KindForDataType("DECIMAL(10, 2)", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.EDecimal.Kind, kd.Kind)
+		assert.Equal(t, decimal.NewDetails(10, 2), *kd.ExtendedDecimalDetails)
+	}
+	{
+		// INTEGER
+		kd, err := _dialect.KindForDataType("INTEGER", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Integer.Kind, kd.Kind)
+		assert.Equal(t, typing.ToPtr(typing.IntegerKind), kd.OptionalIntegerKind)
+	}
+	{
+		// LONG
+		kd, err := _dialect.KindForDataType("LONG", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Integer.Kind, kd.Kind)
+		assert.Equal(t, typing.ToPtr(typing.BigIntegerKind), kd.OptionalIntegerKind)
+	}
+	{
+		// Float and Double
+		for _, kind := range []string{"FLOAT", "DOUBLE"} {
+			kd, err := _dialect.KindForDataType(kind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.Float, kd)
+		}
+	}
+	{
+		// Date
+		kd, err := _dialect.KindForDataType("DATE", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.Date, kd)
+	}
+	{
+		// TimestampTZ
+		kd, err := _dialect.KindForDataType("TIMESTAMP WITH TIMEZONE", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.TimestampTZ, kd)
+	}
+	{
+		// TimestampNTZ
+		kd, err := _dialect.KindForDataType("TIMESTAMP WITHOUT TIMEZONE", "")
+		assert.NoError(t, err)
+		assert.Equal(t, typing.TimestampNTZ, kd)
+	}
+	{
+		// String and other data types that map to a string.
+		for _, kind := range []string{"STRING", "BINARY", "UUID", "FIXED"} {
+			kd, err := _dialect.KindForDataType(kind, "")
+			assert.NoError(t, err)
+			assert.Equal(t, typing.String, kd)
+		}
+	}
+}
