52f8c3120d7d4473fae12c45cdd51d48d67b0055
Robin Tang <rtang.cs@gmail.com>
2025-02-11 19:54:33 -0800

    Simplify closing.

--- lib/csvwriter/gzip.go
+++ lib/csvwriter/gzip.go
@@ -38,12 +38,6 @@ func (g *GzipWriter) Flush() error {
 }
 
 func (g *GzipWriter) Close() error {
-	if err := g.writer.Error(); err != nil {
-		// If the writer failed to close, let's try to close the gzip writer and file.
-		_ = g.gzip.Close()
-		_ = g.file.Close()
-		return err
-	}
 	if err := g.gzip.Close(); err != nil {
 		// If gzip fails, we should at least try to close the file
 		_ = g.file.Close()
