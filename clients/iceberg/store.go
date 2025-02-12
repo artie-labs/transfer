d7b48dc32fcb01aa0a0acae3714e3aa2bcfacfbc
Robin Tang <rtang.cs@gmail.com>
2025-02-12 15:13:32 -0800

    Update.

--- clients/iceberg/store.go
+++ clients/iceberg/store.go
@@ -7,14 +7,17 @@ import (
 	"github.com/artie-labs/transfer/clients/iceberg/dialect"
 	"github.com/artie-labs/transfer/lib/apachelivy"
 	"github.com/artie-labs/transfer/lib/config"
+	"github.com/artie-labs/transfer/lib/destination/types"
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/optimization"
 	"github.com/artie-labs/transfer/lib/sql"
+	"github.com/artie-labs/transfer/lib/typing/columns"
 )
 
 type Store struct {
 	catalog          string
 	apacheLivyClient apachelivy.Client
+	cm               *types.DestinationTableConfigMap
 }
 
 func LoadStore(cfg config.Config) (Store, error) {
@@ -27,7 +30,34 @@ func (s Store) Dialect() dialect.IcebergDialect {
 }
 
 func (s Store) Append(ctx context.Context, tableData *optimization.TableData, useTempTable bool) error {
+	if tableData.ShouldSkipUpdate() {
+		return nil
+	}
+
+	tableID := s.IdentifierFor(tableData.TopicConfig(), tableData.Name())
+	_, _ = s.GetTableConfig(tableID, tableData.TopicConfig().DropDeletedColumns)
 	return fmt.Errorf("not implemented")
+}
+
+func (s Store) GetTableConfig(tableID sql.TableIdentifier, dropDeletedColumns bool) (*types.DestinationTableConfig, error) {
+	if tableCfg := s.cm.GetTableConfig(tableID); tableCfg != nil {
+		return tableCfg, nil
+	}
+
+	cols, err := s.describeTable(context.Background(), tableID)
+	if err != nil {
+		if s.Dialect().IsTableDoesNotExistErr(err) {
+			tableCfg := types.NewDestinationTableConfig([]columns.Column{}, dropDeletedColumns)
+			s.cm.AddTable(tableID, tableCfg)
+			return tableCfg, nil
+		}
+
+		return nil, fmt.Errorf("failed to describe table: %w", err)
+	}
+
+	tableCfg := types.NewDestinationTableConfig(cols, dropDeletedColumns)
+	s.cm.AddTable(tableID, tableCfg)
+	return tableCfg, nil
 }
 
 func (s Store) Merge(ctx context.Context, tableData *optimization.TableData) (bool, error) {
