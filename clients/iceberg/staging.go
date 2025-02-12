bfaa38157ecb441a3468dcb1a2055a87baeb3636
Robin Tang <rtang.cs@gmail.com>
2025-02-11 19:18:46 -0800

    WIP.

---
+++ clients/iceberg/staging.go
@@ -0,0 +1,128 @@
+package iceberg
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/artie-labs/transfer/lib/awslib"
+	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/optimization"
+	"github.com/artie-labs/transfer/lib/sql"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/values"
+)
+
+func (s Store) uploadToS3(ctx context.Context, fp string) (string, error) {
+	s3URI, err := awslib.UploadLocalFileToS3(ctx, awslib.UploadArgs{
+		Bucket:                     s.config.Iceberg.S3Tables.Bucket,
+		FilePath:                   fp,
+		OverrideAWSAccessKeyID:     &s.config.Iceberg.S3Tables.AwsAccessKeyID,
+		OverrideAWSAccessKeySecret: &s.config.Iceberg.S3Tables.AwsSecretAccessKey,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("failed to upload to s3: %w", err)
+	}
+
+	// We need to change the prefix from s3:// to s3a://
+	// Ref: https://stackoverflow.com/a/33356421
+	s3URI = "s3a:" + strings.TrimPrefix(s3URI, "s3:")
+	return s3URI, nil
+}
+
+func castColValStaging(colVal any, colKind typing.KindDetails) (string, error) {
+	if colVal == nil {
+		return constants.NullValuePlaceholder, nil
+	}
+
+	return values.ToString(colVal, colKind)
+}
+
+func (s *Store) writeTemporaryTableFile(tableData *optimization.TableData, newTableID sql.TableIdentifier) (string, error) {
+	fp := filepath.Join(os.TempDir(), fmt.Sprintf("%s.csv", newTableID.FullyQualifiedName()))
+	file, err := os.Create(fp)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	writer.Comma = '\t'
+
+	columns := tableData.ReadOnlyInMemoryCols().ValidColumns()
+	headers := make([]string, 0, len(columns))
+	for _, col := range columns {
+		headers = append(headers, col.Name())
+	}
+
+	if err = writer.Write(headers); err != nil {
+		return "", fmt.Errorf("failed to write headers: %w", err)
+	}
+
+	for _, row := range tableData.Rows() {
+		var csvRow []string
+		for _, col := range columns {
+			castedValue, castErr := castColValStaging(row[col.Name()], col.KindDetails)
+			if castErr != nil {
+				return "", fmt.Errorf("failed to cast value '%v': %w", row[col.Name()], castErr)
+			}
+
+			csvRow = append(csvRow, castedValue)
+		}
+
+		if err = writer.Write(csvRow); err != nil {
+			return "", fmt.Errorf("failed to write to csv: %w", err)
+		}
+	}
+
+	writer.Flush()
+	return fp, writer.Error()
+}
+
+func (s Store) prepareTemporaryTable(ctx context.Context, tableData *optimization.TableData, tempTableID sql.TableIdentifier) error {
+	fp, err := s.writeTemporaryTableFile(tableData, tempTableID)
+	if err != nil {
+		return fmt.Errorf("failed to load temporary table: %w", err)
+	}
+
+	defer func() {
+		if deleteErr := os.RemoveAll(fp); deleteErr != nil {
+			slog.Warn("Failed to delete temp file", slog.Any("err", deleteErr), slog.String("filePath", fp))
+		}
+	}()
+
+	// Step 1 - Upload the file to S3
+	s3URI, err := s.uploadToS3(ctx, fp)
+	if err != nil {
+		return fmt.Errorf("failed to upload to s3: %w", err)
+	}
+
+	s3URI = "s3a:" + strings.TrimPrefix(s3URI, "s3:")
+	// Step 2 - Load the CSV into a temporary view
+
+	// CSV options: https://spark.apache.org/docs/3.5.3/sql-data-sources-csv.html
+	command := fmt.Sprintf(`
+CREATE OR REPLACE TEMPORARY VIEW %s
+USING csv
+OPTIONS (
+  path '%s',
+  sep '\t',
+  header 'true',
+  compression 'gzip',
+  nullValue '\\N',
+  inferSchema 'true'
+);
+`, tempTableID.EscapedTable(), s3URI)
+
+	fmt.Println("command", command, "fp", fp)
+	if err := s.apacheLivyClient.ExecContext(ctx, command); err != nil {
+		return fmt.Errorf("failed to load temporary table: %w", err)
+	}
+
+	return nil
+}
