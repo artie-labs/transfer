555371e602343d2345063d7c6a354178680b4ee4
Robin Tang <rtang.cs@gmail.com>
2025-02-11 16:09:07 -0800

    Clean.

--- clients/iceberg/store.go
+++ clients/iceberg/store.go
@@ -1,11 +1,13 @@
 package iceberg
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/artie-labs/transfer/clients/iceberg/dialect"
 	"github.com/artie-labs/transfer/lib/config"
 	"github.com/artie-labs/transfer/lib/kafkalib"
+	"github.com/artie-labs/transfer/lib/optimization"
 	"github.com/artie-labs/transfer/lib/sql"
 )
 
@@ -18,12 +20,12 @@ func LoadStore(cfg config.Config) (Store, error) {
 	return Store{}, nil
 }
 
-func (s Store) Append(data interface{}) error {
+func (s Store) Append(ctx context.Context, tableData *optimization.TableData, useTempTable bool) error {
 	return fmt.Errorf("not implemented")
 }
 
-func (s Store) Merge() error {
-	return fmt.Errorf("not implemented")
+func (s Store) Merge(ctx context.Context, tableData *optimization.TableData) (bool, error) {
+	return false, fmt.Errorf("not implemented")
 }
 
 func (s Store) IsRetryableError(err error) bool {
