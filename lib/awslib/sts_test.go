952f6e35ef9547f3ba5850de6a4ddb05b091d751
Robin Tang <rtang.cs@gmail.com>
2025-03-05 10:06:56 -0500

    Adding sts_test.

---
+++ lib/awslib/sts_test.go
@@ -0,0 +1,36 @@
+package awslib
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCredentials_IsExpired(t *testing.T) {
+	creds := Credentials{
+		awsAccessKeyID:     "test",
+		awsSecretAccessKey: "test",
+		awsSessionToken:    "test",
+	}
+
+	{
+		// Expiration = true, because there's no expiration set
+		assert.True(t, creds.IsExpired())
+	}
+	{
+		// Expiration = true, because is in the past, so it has expired
+		creds.expiresAt = time.Now().Add(-1 * time.Minute)
+		assert.True(t, creds.IsExpired())
+	}
+	{
+		// Expiration = true, because is in the future (but less than the buffer)
+		creds.expiresAt = time.Now().Add(1 * time.Minute)
+		assert.True(t, creds.IsExpired())
+	}
+	{
+		// Expiration = false, because is in the future (but more than the buffer)
+		creds.expiresAt = time.Now().Add(100 * time.Minute)
+		assert.False(t, creds.IsExpired())
+	}
+}
