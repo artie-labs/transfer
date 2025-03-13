683e500f419a0fcf68658c01794362dc008b17da
Robin Tang <rtang.cs@gmail.com>
2025-03-13 10:38:02 -0700

    Update.

--- clients/shared/default_value.go
+++ clients/shared/default_value.go
@@ -85,7 +85,7 @@ func BackfillColumn(dest destination.Destination, column columns.Column, tableID
 		}
 
 		escapedCol := dest.Dialect().QuoteIdentifier(column.Name())
-		query := fmt.Sprintf(`UPDATE %s as t SET t.%s = %v WHERE t.%s IS NULL;`,
+		query := fmt.Sprintf(`UPDATE %s as t SET %s = %v WHERE %s IS NULL;`,
 			// UPDATE table as t SET t.col = default_val WHERE t.col IS NULL
 			tableID.FullyQualifiedName(), escapedCol, defaultVal, escapedCol,
 		)
