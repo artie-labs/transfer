71763f0a96386b0ad60477924eb42ae5a28df210
Robin Tang <rtang.cs@gmail.com>
2025-03-25 11:49:37 -0700

    Rewriting various tests to be block style (#1209)

--- lib/typing/columns/columns_test.go
+++ lib/typing/columns/columns_test.go
@@ -11,272 +11,224 @@ import (
 )
 
 func TestEscapeName(t *testing.T) {
-	type _testCase struct {
-		name         string
-		expectedName string
+	{
+		// Test basic name without any transformations
+		assert.Equal(t, "foo", EscapeName("foo"))
 	}
-
-	testCases := []_testCase{
-		{
-			name:         "foo",
-			expectedName: "foo",
-		},
-		{
-			name:         "FOOO",
-			expectedName: "fooo",
-		},
-		{
-			name:         "col with spaces",
-			expectedName: "col__with__spaces",
-		},
-		{
-			name:         "1abc",
-			expectedName: "col_1abc",
-		},
+	{
+		// Test uppercase to lowercase conversion
+		assert.Equal(t, "fooo", EscapeName("FOOO"))
 	}
-
-	for _, testCase := range testCases {
-		actualName := EscapeName(testCase.name)
-		assert.Equal(t, testCase.expectedName, actualName, testCase.name)
+	{
+		// Test spaces being replaced with double underscores
+		assert.Equal(t, "col__with__spaces", EscapeName("col with spaces"))
+	}
+	{
+		// Test column name starting with number gets col_ prefix
+		assert.Equal(t, "col_1abc", EscapeName("1abc"))
 	}
 }
 
 func TestColumn_ShouldSkip(t *testing.T) {
-	type _testCase struct {
-		name           string
-		col            *Column
-		expectedResult bool
+	{
+		// Test when column is nil
+		var col *Column
+		assert.True(t, col.ShouldSkip())
 	}
-
-	testCases := []_testCase{
-		{
-			name:           "col is nil",
-			expectedResult: true,
-		},
-		{
-			name: "invalid column",
-			col: &Column{
-				KindDetails: typing.Invalid,
-			},
-			expectedResult: true,
-		},
-		{
-			name: "normal column",
-			col: &Column{
-				KindDetails: typing.String,
-			},
-		},
+	{
+		// Test when column is invalid
+		col := Column{
+			KindDetails: typing.Invalid,
+		}
+		assert.True(t, col.ShouldSkip())
 	}
-
-	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expectedResult, testCase.col.ShouldSkip(), testCase.name)
+	{
+		// Test normal column
+		col := Column{KindDetails: typing.String}
+		assert.False(t, col.ShouldSkip())
 	}
 }
 
 func TestColumn_ShouldBackfill(t *testing.T) {
-	type _testCase struct {
-		name                 string
-		column               *Column
-		expectShouldBackfill bool
+	{
+		// Test happy path with basic column
+		col := &Column{
+			name: "id",
+		}
+		assert.False(t, col.ShouldBackfill())
 	}
-
-	testCases := []_testCase{
-		{
-			name: "happy path",
-			column: &Column{
-				name: "id",
-			},
-		},
-		{
-			name: "happy path, primary key",
-			column: &Column{
-				name:       "id",
-				primaryKey: true,
-			},
-		},
-		{
-			name: "happy path, primary key (default value set and not backfilled), but since it's a PK - no backfill",
-			column: &Column{
-				name:         "id",
-				primaryKey:   true,
-				defaultValue: 123,
-			},
-		},
-		{
-			name: "default value set but kind = invalid",
-			column: &Column{
-				name:         "id",
-				defaultValue: "dusty",
-				KindDetails:  typing.Invalid,
-			},
-		},
-		{
-			name: "default value set but backfilled",
-			column: &Column{
-				name:         "id",
-				defaultValue: "dusty",
-				backfilled:   true,
-				KindDetails:  typing.String,
-			},
-		},
-		{
-			name: "default value set and not backfilled",
-			column: &Column{
-				name:         "id",
-				defaultValue: "dusty",
-				KindDetails:  typing.String,
-			},
-			expectShouldBackfill: true,
-		},
+	{
+		// Test primary key column
+		col := &Column{
+			name:       "id",
+			primaryKey: true,
+		}
+		assert.False(t, col.ShouldBackfill())
 	}
-
-	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expectShouldBackfill, testCase.column.ShouldBackfill(), testCase.name)
+	{
+		// Test primary key with default value
+		col := &Column{
+			name:         "id",
+			primaryKey:   true,
+			defaultValue: 123,
+		}
+		assert.False(t, col.ShouldBackfill())
+	}
+	{
+		// Test invalid column with default value
+		col := &Column{
+			name:         "id",
+			defaultValue: "dusty",
+			KindDetails:  typing.Invalid,
+		}
+		assert.False(t, col.ShouldBackfill())
+	}
+	{
+		// Test already backfilled column
+		col := &Column{
+			name:         "id",
+			defaultValue: "dusty",
+			backfilled:   true,
+			KindDetails:  typing.String,
+		}
+		assert.False(t, col.ShouldBackfill())
+	}
+	{
+		// Test column that needs backfilling
+		col := &Column{
+			name:         "id",
+			defaultValue: "dusty",
+			KindDetails:  typing.String,
+		}
+		assert.True(t, col.ShouldBackfill())
 	}
 }
 
 func TestColumns_ValidColumns(t *testing.T) {
-	var happyPathCols = []Column{
-		{
-			name:        "hi",
-			KindDetails: typing.String,
-		},
-		{
-			name:        "bye",
-			KindDetails: typing.String,
-		},
-		{
-			name:        "start",
-			KindDetails: typing.String,
-		},
-	}
+	{
+		// Setup test columns
+		var happyPathCols = []Column{
+			{
+				name:        "hi",
+				KindDetails: typing.String,
+			},
+			{
+				name:        "bye",
+				KindDetails: typing.String,
+			},
+			{
+				name:        "start",
+				KindDetails: typing.String,
+			},
+		}
 
-	extraCols := happyPathCols
-	for i := 0; i < 100; i++ {
-		extraCols = append(extraCols, Column{
-			name:        fmt.Sprintf("hello_%v", i),
-			KindDetails: typing.Invalid,
-		})
+		// Test happy path with valid columns
+		assert.Equal(t, happyPathCols, (&Columns{columns: happyPathCols}).ValidColumns())
 	}
+	{
+		// Test with mix of valid and invalid columns
+		var happyPathCols = []Column{
+			{
+				name:        "hi",
+				KindDetails: typing.String,
+			},
+			{
+				name:        "bye",
+				KindDetails: typing.String,
+			},
+			{
+				name:        "start",
+				KindDetails: typing.String,
+			},
+		}
 
-	testCases := []struct {
-		name         string
-		cols         []Column
-		expectedCols []Column
-	}{
-		{
-			name:         "happy path",
-			cols:         happyPathCols,
-			expectedCols: slices.Clone(happyPathCols),
-		},
-		{
-			name:         "happy path + extra col",
-			cols:         extraCols,
-			expectedCols: slices.Clone(happyPathCols),
-		},
-	}
+		extraCols := slices.Clone(happyPathCols)
+		for i := 0; i < 100; i++ {
+			extraCols = append(extraCols, Column{
+				name:        fmt.Sprintf("hello_%v", i),
+				KindDetails: typing.Invalid,
+			})
+		}
 
-	for _, testCase := range testCases {
-		assert.Equal(t, testCase.expectedCols, (&Columns{columns: testCase.cols}).ValidColumns(), testCase.name)
+		assert.Equal(t, happyPathCols, (&Columns{columns: extraCols}).ValidColumns())
 	}
 }
 
 func TestColumns_UpsertColumns(t *testing.T) {
-	keys := []string{"a", "b", "c", "d", "e"}
-	var cols Columns
-	for _, key := range keys {
-		cols.AddColumn(Column{name: key, KindDetails: typing.String})
-	}
 	{
-		// Now inspect prior to change.
+		// Setup initial columns
+		keys := []string{"a", "b", "c", "d", "e"}
+		var cols Columns
+		for _, key := range keys {
+			cols.AddColumn(Column{name: key, KindDetails: typing.String})
+		}
+
+		// Verify initial state
 		for _, col := range cols.GetColumns() {
 			assert.False(t, col.ToastColumn)
 		}
 	}
 	{
-		// Now update a and b to be toast columns
+		// Test updating toast columns
+		var cols Columns
+		cols.AddColumn(Column{name: "a", KindDetails: typing.String})
+		cols.AddColumn(Column{name: "b", KindDetails: typing.String})
+
 		for _, key := range []string{"a", "b"} {
 			assert.NoError(t, cols.UpsertColumn(key, UpsertColumnArg{
 				ToastCol: typing.ToPtr(true),
 			}))
 
-			// Now inspect.
 			col, _ := cols.GetColumn(key)
 			assert.True(t, col.ToastColumn)
 		}
 	}
 	{
-		// Increase string precision
-		{
-			// Valid - Current column does not have string precision set
-			assert.NoError(t, cols.UpsertColumn("string_precision_a", UpsertColumnArg{}))
+		// Test string precision updates
+		var cols Columns
 
-			colA, _ := cols.GetColumn("string_precision_a")
-			assert.Nil(t, colA.KindDetails.OptionalStringPrecision)
+		// Test when no precision is set initially
+		assert.NoError(t, cols.UpsertColumn("string_precision_a", UpsertColumnArg{}))
+		colA, _ := cols.GetColumn("string_precision_a")
+		assert.Nil(t, colA.KindDetails.OptionalStringPrecision)
 
-			assert.NoError(t,
-				cols.UpsertColumn("string_precision_a",
-					UpsertColumnArg{
-						StringPrecision: typing.ToPtr(int32(55)),
-					},
-				),
-			)
-			colA, _ = cols.GetColumn("string_precision_a")
-			assert.Equal(t, int32(55), *colA.KindDetails.OptionalStringPrecision)
-		}
-		{
-			// Valid - Current column does have string precision set (but it's less)
-			assert.NoError(t,
-				cols.UpsertColumn("string_precision_b",
-					UpsertColumnArg{
-						StringPrecision: typing.ToPtr(int32(5)),
-					},
-				),
-			)
+		// Test setting initial precision
+		assert.NoError(t, cols.UpsertColumn("string_precision_a", UpsertColumnArg{
+			StringPrecision: typing.ToPtr(int32(55)),
+		}))
+		colA, _ = cols.GetColumn("string_precision_a")
+		assert.Equal(t, int32(55), *colA.KindDetails.OptionalStringPrecision)
 
-			colB, _ := cols.GetColumn("string_precision_b")
-			assert.Equal(t, int32(5), *colB.KindDetails.OptionalStringPrecision)
-			assert.NoError(t,
-				cols.UpsertColumn("string_precision_b",
-					UpsertColumnArg{
-						StringPrecision: typing.ToPtr(int32(100)),
-					},
-				),
-			)
+		// Test increasing precision
+		assert.NoError(t, cols.UpsertColumn("string_precision_b", UpsertColumnArg{
+			StringPrecision: typing.ToPtr(int32(5)),
+		}))
+		assert.NoError(t, cols.UpsertColumn("string_precision_b", UpsertColumnArg{
+			StringPrecision: typing.ToPtr(int32(100)),
+		}))
+		colB, _ := cols.GetColumn("string_precision_b")
+		assert.Equal(t, int32(100), *colB.KindDetails.OptionalStringPrecision)
 
-			colB, _ = cols.GetColumn("string_precision_b")
-			assert.Equal(t, int32(100), *colB.KindDetails.OptionalStringPrecision)
-		}
-		{
-			// Invalid - Cannot decrease string precision
-			assert.NoError(t,
-				cols.UpsertColumn("string_precision_b",
-					UpsertColumnArg{
-						StringPrecision: typing.ToPtr(int32(500)),
-					},
-				),
-			)
-
-			assert.ErrorContains(t,
-				cols.UpsertColumn("string_precision_b",
-					UpsertColumnArg{
-						StringPrecision: typing.ToPtr(int32(100)),
-					},
-				),
-				"cannot decrease string precision from 500 to 100",
-			)
-		}
+		// Test decreasing precision (should fail)
+		assert.NoError(t, cols.UpsertColumn("string_precision_b", UpsertColumnArg{
+			StringPrecision: typing.ToPtr(int32(500)),
+		}))
+		assert.ErrorContains(t, cols.UpsertColumn("string_precision_b", UpsertColumnArg{
+			StringPrecision: typing.ToPtr(int32(100)),
+		}), "cannot decrease string precision from 500 to 100")
 	}
 	{
-		// Create a new column zzz
+		// Test creating new columns
+		var cols Columns
+
+		// Test creating basic column
 		assert.NoError(t, cols.UpsertColumn("zzz", UpsertColumnArg{}))
 		zzzCol, _ := cols.GetColumn("zzz")
 		assert.False(t, zzzCol.ToastColumn)
 		assert.False(t, zzzCol.primaryKey)
 		assert.Equal(t, zzzCol.KindDetails, typing.Invalid)
-	}
-	{
-		// Create a new column aaa
+
+		// Test creating column with toast and primary key
 		assert.NoError(t, cols.UpsertColumn("aaa", UpsertColumnArg{
 			ToastCol:   typing.ToPtr(true),
 			PrimaryKey: typing.ToPtr(true),
@@ -286,91 +238,88 @@ func TestColumns_UpsertColumns(t *testing.T) {
 		assert.True(t, aaaCol.primaryKey)
 		assert.Equal(t, aaaCol.KindDetails, typing.Invalid)
 	}
-	// Now selectively update only a, b
-	for _, key := range []string{"a", "b"} {
-		assert.NoError(t, cols.UpsertColumn(key, UpsertColumnArg{
-			ToastCol: typing.ToPtr(true),
-		}))
-
-		// Now inspect.
-		col, _ := cols.GetColumn(key)
-		assert.True(t, col.ToastColumn)
-	}
 	{
-		assert.NoError(t, cols.UpsertColumn("zzz", UpsertColumnArg{}))
-		zzzCol, _ := cols.GetColumn("zzz")
-		assert.False(t, zzzCol.ToastColumn)
-		assert.False(t, zzzCol.primaryKey)
-		assert.Equal(t, zzzCol.KindDetails, typing.Invalid)
+		// Test empty column name
+		var cols Columns
+		assert.ErrorContains(t, cols.UpsertColumn("", UpsertColumnArg{}), "column name is empty")
 	}
-	{
-		assert.NoError(t, cols.UpsertColumn("aaa", UpsertColumnArg{
-			ToastCol:   typing.ToPtr(true),
-			PrimaryKey: typing.ToPtr(true),
-		}))
-
-		aaaCol, _ := cols.GetColumn("aaa")
-		assert.True(t, aaaCol.ToastColumn)
-		assert.True(t, aaaCol.primaryKey)
-		assert.Equal(t, aaaCol.KindDetails, typing.Invalid)
-	}
-	assert.ErrorContains(t, cols.UpsertColumn("", UpsertColumnArg{}), "column name is empty")
 }
 
 func TestColumns_Add_Duplicate(t *testing.T) {
-	var cols Columns
-	duplicateColumns := []Column{{name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}}
-	for _, duplicateColumn := range duplicateColumns {
-		cols.AddColumn(duplicateColumn)
-	}
+	{
+		// Test adding duplicate columns
+		var cols Columns
+		duplicateColumns := []Column{{name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}, {name: "foo"}}
+		for _, duplicateColumn := range duplicateColumns {
+			cols.AddColumn(duplicateColumn)
+		}
 
-	assert.Equal(t, len(cols.GetColumns()), 1, "AddColumn() de-duplicates")
+		assert.Equal(t, len(cols.GetColumns()), 1, "AddColumn() de-duplicates")
+	}
 }
 
 func TestColumns_Mutation(t *testing.T) {
-	var cols Columns
-	colsToAdd := []Column{{name: "foo", KindDetails: typing.String, defaultValue: "bar"}, {name: "bar", KindDetails: typing.Struct}}
-	// Insert
-	for _, colToAdd := range colsToAdd {
-		cols.AddColumn(colToAdd)
+	{
+		// Test column insertion
+		var cols Columns
+		colsToAdd := []Column{
+			{name: "foo", KindDetails: typing.String, defaultValue: "bar"},
+			{name: "bar", KindDetails: typing.Struct},
+		}
+
+		for _, colToAdd := range colsToAdd {
+			cols.AddColumn(colToAdd)
+		}
+
+		assert.Equal(t, len(cols.GetColumns()), 2)
+
+		fooCol, isOk := cols.GetColumn("foo")
+		assert.True(t, isOk)
+		assert.Equal(t, typing.String, fooCol.KindDetails)
+
+		barCol, isOk := cols.GetColumn("bar")
+		assert.True(t, isOk)
+		assert.Equal(t, typing.Struct, barCol.KindDetails)
 	}
+	{
+		// Test column updates
+		var cols Columns
+		cols.AddColumn(Column{name: "foo", KindDetails: typing.String, defaultValue: "bar"})
+		cols.AddColumn(Column{name: "bar", KindDetails: typing.Struct})
 
-	assert.Equal(t, len(cols.GetColumns()), 2)
-	fooCol, isOk := cols.GetColumn("foo")
-	assert.True(t, isOk)
-	assert.Equal(t, typing.String, fooCol.KindDetails)
+		cols.UpdateColumn(Column{
+			name:        "foo",
+			KindDetails: typing.Integer,
+		})
 
-	barCol, isOk := cols.GetColumn("bar")
-	assert.True(t, isOk)
-	assert.Equal(t, typing.Struct, barCol.KindDetails)
+		cols.UpdateColumn(Column{
+			name:         "bar",
+			KindDetails:  typing.Boolean,
+			defaultValue: "123",
+		})
 
-	// Update
-	cols.UpdateColumn(Column{
-		name:        "foo",
-		KindDetails: typing.Integer,
-	})
+		fooCol, isOk := cols.GetColumn("foo")
+		assert.True(t, isOk)
+		assert.Equal(t, typing.Integer, fooCol.KindDetails)
+		assert.Equal(t, nil, fooCol.defaultValue)
 
-	cols.UpdateColumn(Column{
-		name:         "bar",
-		KindDetails:  typing.Boolean,
-		defaultValue: "123",
-	})
+		barCol, isOk := cols.GetColumn("bar")
+		assert.True(t, isOk)
+		assert.Equal(t, typing.Boolean, barCol.KindDetails)
+		assert.Equal(t, "123", barCol.defaultValue)
+	}
+	{
+		// Test column deletion
+		var cols Columns
+		cols.AddColumn(Column{name: "foo", KindDetails: typing.String})
+		cols.AddColumn(Column{name: "bar", KindDetails: typing.String})
 
-	fooCol, isOk = cols.GetColumn("foo")
-	assert.True(t, isOk)
-	assert.Equal(t, typing.Integer, fooCol.KindDetails)
-	assert.Equal(t, nil, fooCol.defaultValue)
+		cols.DeleteColumn("foo")
+		assert.Equal(t, len(cols.GetColumns()), 1)
 
-	barCol, isOk = cols.GetColumn("bar")
-	assert.True(t, isOk)
-	assert.Equal(t, typing.Boolean, barCol.KindDetails)
-	assert.Equal(t, "123", barCol.defaultValue)
-
-	// Delete
-	cols.DeleteColumn("foo")
-	assert.Equal(t, len(cols.GetColumns()), 1)
-	cols.DeleteColumn("bar")
-	assert.Equal(t, len(cols.GetColumns()), 0)
+		cols.DeleteColumn("bar")
+		assert.Equal(t, len(cols.GetColumns()), 0)
+	}
 }
 
 func TestRemoveDeleteColumnMarker(t *testing.T) {
@@ -380,28 +329,34 @@ func TestRemoveDeleteColumnMarker(t *testing.T) {
 	deleteColumnMarkerCol := NewColumn(constants.DeleteColumnMarker, typing.Invalid)
 
 	{
+		// Test empty column list
 		_, err := RemoveDeleteColumnMarker([]Column{})
 		assert.ErrorContains(t, err, "doesn't exist")
 	}
 	{
+		// Test single column without marker
 		_, err := RemoveDeleteColumnMarker([]Column{col1})
 		assert.ErrorContains(t, err, "doesn't exist")
 	}
 	{
+		// Test multiple columns without marker
 		_, err := RemoveDeleteColumnMarker([]Column{col1, col2})
 		assert.ErrorContains(t, err, "doesn't exist")
 	}
 	{
+		// Test only marker column
 		result, err := RemoveDeleteColumnMarker([]Column{deleteColumnMarkerCol})
 		assert.NoError(t, err)
 		assert.Empty(t, result)
 	}
 	{
+		// Test marker between columns
 		result, err := RemoveDeleteColumnMarker([]Column{col1, deleteColumnMarkerCol, col2})
 		assert.NoError(t, err)
 		assert.Equal(t, []Column{col1, col2}, result)
 	}
 	{
+		// Test multiple markers
 		result, err := RemoveDeleteColumnMarker([]Column{col1, deleteColumnMarkerCol, col2, deleteColumnMarkerCol, col3})
 		assert.NoError(t, err)
 		assert.Equal(t, []Column{col1, col2, col3}, result)
@@ -409,12 +364,17 @@ func TestRemoveDeleteColumnMarker(t *testing.T) {
 }
 
 func TestColumnNames(t *testing.T) {
-	assert.Empty(t, ColumnNames(nil))
-
-	cols := []Column{
-		NewColumn("a", typing.Invalid),
-		NewColumn("b", typing.Invalid),
-		NewColumn("c", typing.Invalid),
+	{
+		// Test nil input
+		assert.Empty(t, ColumnNames(nil))
 	}
-	assert.Equal(t, []string{"a", "b", "c"}, ColumnNames(cols))
+	{
+		// Test normal columns
+		cols := []Column{
+			NewColumn("a", typing.Invalid),
+			NewColumn("b", typing.Invalid),
+			NewColumn("c", typing.Invalid),
+		}
+		assert.Equal(t, []string{"a", "b", "c"}, ColumnNames(cols))
+	}
 }
