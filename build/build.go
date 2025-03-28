4c98d89258e52f42c0139e6ddf7a9896023cd14c
Nathan <148575555+nathan-artie@users.noreply.github.com>
2025-03-27 17:03:35 -0700

    Word

--- build/build.go
+++ build/build.go
@@ -1,6 +1,6 @@
 package build
 
 var (
-	// this will be set by the goreleaser configuration to appropriate value for the compiled binary.
+	// this will be set by the goreleaser configuration to an appropriate value for the compiled binary.
 	Version string = "dev"
 )
