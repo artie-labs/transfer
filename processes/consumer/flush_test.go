e30ac3f4757187e23888061950e4d131235c5889
Robin Tang <rtang.cs@gmail.com>
2025-04-04 17:53:23 -0700

    Update.

--- processes/consumer/flush_test.go
+++ processes/consumer/flush_test.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/segmentio/kafka-go"
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,7 @@ import (
 	"github.com/artie-labs/transfer/lib/config/constants"
 	"github.com/artie-labs/transfer/lib/kafkalib"
 	"github.com/artie-labs/transfer/lib/mocks"
+	"github.com/artie-labs/transfer/lib/telemetry/metrics"
 	"github.com/artie-labs/transfer/models/event"
 )
 
@@ -81,53 +83,53 @@ func (f *FlushTestSuite) TestShouldFlush() {
 	assert.True(f.T(), flush, "Flush successfully triggered via pool size.")
 }
 
-// func (f *FlushTestSuite) TestMemoryConcurrency() {
-// 	tableNames := []string{"dusty", "snowflake", "postgres"}
-// 	var wg sync.WaitGroup
+func (f *FlushTestSuite) TestMemoryConcurrency() {
+	tableNames := []string{"dusty", "snowflake", "postgres"}
+	var wg sync.WaitGroup
 
-// 	// Inserted a bunch of data
-// 	for idx := range tableNames {
-// 		wg.Add(1)
-// 		go func(tableName string) {
-// 			defer wg.Done()
-// 			for i := 0; i < 5; i++ {
-// 				mockEvent := &mocks.FakeEvent{}
-// 				mockEvent.GetTableNameReturns(tableName)
-// 				mockEvent.GetDataReturns(map[string]any{
-// 					"id":                                fmt.Sprintf("pk-%d", i),
-// 					constants.DeleteColumnMarker:        true,
-// 					constants.OnlySetDeleteColumnMarker: true,
-// 					"pk":                                fmt.Sprintf("pk-%d", i),
-// 					"foo":                               "bar",
-// 					"cat":                               "dog",
-// 				}, nil)
+	// Inserted a bunch of data
+	for idx := range tableNames {
+		wg.Add(1)
+		go func(tableName string) {
+			defer wg.Done()
+			for i := 0; i < 5; i++ {
+				mockEvent := &mocks.FakeEvent{}
+				mockEvent.GetTableNameReturns(tableName)
+				mockEvent.GetDataReturns(map[string]any{
+					"id":                                fmt.Sprintf("pk-%d", i),
+					constants.DeleteColumnMarker:        true,
+					constants.OnlySetDeleteColumnMarker: true,
+					"pk":                                fmt.Sprintf("pk-%d", i),
+					"foo":                               "bar",
+					"cat":                               "dog",
+				}, nil)
 
-// 				evt, err := event.ToMemoryEvent(mockEvent, map[string]any{"id": fmt.Sprintf("pk-%d", i)}, kafkalib.TopicConfig{}, config.Replication)
-// 				assert.NoError(f.T(), err)
+				evt, err := event.ToMemoryEvent(mockEvent, map[string]any{"id": fmt.Sprintf("pk-%d", i)}, kafkalib.TopicConfig{}, config.Replication)
+				assert.NoError(f.T(), err)
 
-// 				kafkaMsg := kafka.Message{Partition: 1, Offset: int64(i)}
-// 				_, _, err = evt.Save(f.cfg, f.db, topicConfig, artie.NewMessage(&kafkaMsg, kafkaMsg.Topic))
-// 				assert.Nil(f.T(), err)
-// 			}
-// 		}(tableNames[idx])
-// 	}
+				kafkaMsg := kafka.Message{Partition: 1, Offset: int64(i)}
+				_, _, err = evt.Save(f.cfg, f.db, topicConfig, artie.NewMessage(&kafkaMsg, kafkaMsg.Topic))
+				assert.Nil(f.T(), err)
+			}
+		}(tableNames[idx])
+	}
 
-// 	wg.Wait()
+	wg.Wait()
 
-// 	// Verify all the tables exist.
-// 	for idx := range tableNames {
-// 		td := f.db.GetOrCreateTableData(tableNames[idx])
-// 		assert.Len(f.T(), td.Rows(), 5)
-// 	}
+	// Verify all the tables exist.
+	for idx := range tableNames {
+		td := f.db.GetOrCreateTableData(tableNames[idx])
+		assert.Len(f.T(), td.Rows(), 5)
+	}
 
-// 	assert.Nil(f.T(), Flush(f.T().Context(), f.db, f.dest, metrics.NullMetricsProvider{}, Args{}), "flush failed")
-// 	assert.Equal(f.T(), f.fakeConsumer.CommitMessagesCallCount(), len(tableNames)) // Commit 3 times because 3 topics.
+	assert.NoError(f.T(), Flush(f.T().Context(), f.db, f.dest, metrics.NullMetricsProvider{}, Args{}), "flush failed")
+	assert.Equal(f.T(), f.fakeConsumer.CommitMessagesCallCount(), len(tableNames)) // Commit 3 times because 3 topics.
 
-// 	for i := 0; i < len(tableNames); i++ {
-// 		_, kafkaMessages := f.fakeConsumer.CommitMessagesArgsForCall(i)
-// 		assert.Equal(f.T(), len(kafkaMessages), 1) // There's only 1 partition right now
+	for i := 0; i < len(tableNames); i++ {
+		_, kafkaMessages := f.fakeConsumer.CommitMessagesArgsForCall(i)
+		assert.Equal(f.T(), len(kafkaMessages), 1) // There's only 1 partition right now
 
-// 		// Within each partition, the offset should be 4 (i < 5 from above).
-// 		assert.Equal(f.T(), kafkaMessages[0].Offset, int64(4))
-// 	}
-// }
+		// Within each partition, the offset should be 4 (i < 5 from above).
+		assert.Equal(f.T(), kafkaMessages[0].Offset, int64(4))
+	}
+}
