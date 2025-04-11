700c35355dd9309d1080aeaf6e20102923c27877
Robin Tang <rtang.cs@gmail.com>
2025-04-11 14:35:41 -0700

    Update code.

--- clients/bigquery/bigquery.go
+++ clients/bigquery/bigquery.go
@@ -209,10 +209,21 @@ func (s *Store) putTable(ctx context.Context, bqTableID dialect.TableIdentifier,
 
 		resp, err := result.FullResponse(ctx)
 		if err != nil {
-			if rowErrs := resp.GetRowErrors(); len(rowErrs) > 0 {
-				for _, rowErr := range rowErrs {
-					fmt.Println("rowErr", rowErr)
+			if resp != nil {
+				if rowErrs := resp.GetRowErrors(); len(rowErrs) > 0 {
+					// Just log the first few errors
+					var errors []any
+					for i, rowErr := range rowErrs {
+						if i > 5 {
+							break
+						}
+
+						errors = append(errors, rowErr)
+					}
+
+					return fmt.Errorf("failed to append rows, encountered %d errors: %v", len(rowErrs), errors)
 				}
+
 			}
 
 			return fmt.Errorf("failed to get response: %w", err)
