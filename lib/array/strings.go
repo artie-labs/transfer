e467e3cde278cb616a773ea430cdd60c55e0bdb9
Robin Tang <rtang.cs@gmail.com>
2025-04-23 10:46:50 -0700

    Update.

--- lib/array/strings.go
+++ lib/array/strings.go
@@ -28,12 +28,13 @@ func InterfaceToArrayString(val any, recastAsArray bool) ([]string, error) {
 	for i := 0; i < list.Len(); i++ {
 		kind := list.Index(i).Kind()
 		value := list.Index(i).Interface()
-		var shouldParse bool
+
 		if stringValue, ok := value.(string); ok {
 			vals = append(vals, stringValue)
 			continue
 		}
 
+		var shouldParse bool
 		if kind == reflect.Interface {
 			valMap, isOk := value.(map[string]any)
 			if isOk {
