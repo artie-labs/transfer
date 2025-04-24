3fd5e39224e6ad01e783e32788de44b199a3077c
Robin Tang <rtang.cs@gmail.com>
2025-04-24 10:48:07 -0700

    Remove comment.

--- lib/jsonutil/jsonutil.go
+++ lib/jsonutil/jsonutil.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 )
 
-// ParsePayload will take in a JSON string, and return a JSON string that has been sanitized (removed duplicate keys)
 func ParsePayload(val string) (any, error) {
 	// There are edge cases for when this may happen
 	// Example: JSONB column in a table in Postgres where the table replica identity is set to `default` and it was a delete event.
