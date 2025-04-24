3828af93a89670af7e08772be27c8e0a9a465fb8
Robin Tang <rtang.cs@gmail.com>
2025-04-24 12:34:22 -0700

    WIP,.

--- integration_tests/destination_append/main.go
+++ integration_tests/destination_append/main.go
@@ -59,7 +59,7 @@ func (at *AppendTest) Run() error {
 		return fmt.Errorf("failed to verify row count: %w", err)
 	}
 
-	if err := at.framework.VerifyDataContentWithRows(at.framework.GetContext(), at.framework.GetTableID().FullyQualifiedName(), appendRows*appendEvery); err != nil {
+	if err := at.framework.VerifyDataContent(appendRows * appendEvery); err != nil {
 		return fmt.Errorf("failed to verify data content: %w", err)
 	}
 
