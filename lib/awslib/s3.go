929b8c78bf829a21cf10476d8e77e0c2fc6799e1
Robin Tang <rtang.cs@gmail.com>
2025-05-04 15:13:01 -0700

    Update.

--- lib/awslib/s3.go
+++ lib/awslib/s3.go
@@ -70,3 +70,42 @@ func UploadLocalFileToS3(ctx context.Context, args UploadArgs) (string, error) {
 
 	return fmt.Sprintf("s3://%s/%s", args.Bucket, objectKey), nil
 }
+
+type DeleteObjectArgs struct {
+	Bucket string
+	Key    string
+
+	OverrideAWSAccessKeyID     string
+	OverrideAWSAccessKeySecret string
+	OverrideAWSSessionToken    string
+	Region                     string
+}
+
+func DeleteObjectFromS3(ctx context.Context, args DeleteObjectArgs) error {
+	var cfg aws.Config
+	var err error
+
+	awsRegion := cmp.Or(args.Region, os.Getenv("AWS_REGION"))
+	if args.OverrideAWSAccessKeyID != "" && args.OverrideAWSAccessKeySecret != "" {
+		creds := credentials.NewStaticCredentialsProvider(args.OverrideAWSAccessKeyID, args.OverrideAWSAccessKeySecret, args.OverrideAWSSessionToken)
+		cfg, err = config.LoadDefaultConfig(ctx, config.WithCredentialsProvider(creds), config.WithRegion(awsRegion))
+	} else {
+		cfg, err = config.LoadDefaultConfig(ctx, config.WithRegion(awsRegion))
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed loading s3 config: %w", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+	_, err = s3Client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(args.Bucket),
+		Key:    aws.String(args.Key),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to delete object: %w", err)
+	}
+
+	return nil
+}
