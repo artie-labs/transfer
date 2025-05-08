a331a17e494e3e7a2396a9ea1d98236d7b03f783
Robin Tang <rtang.cs@gmail.com>
2025-05-08 10:13:55 -0700

    Adding mutex.

--- lib/awslib/sts_test.go
+++ lib/awslib/sts_test.go
@@ -16,21 +16,21 @@ func TestCredentials_IsExpired(t *testing.T) {
 
 	{
 		// Expiration = true, because there's no expiration set
-		assert.True(t, creds.IsExpired())
+		assert.True(t, creds.isExpired())
 	}
 	{
 		// Expiration = true, because is in the past, so it has expired
 		creds.expiresAt = time.Now().Add(-1 * time.Minute)
-		assert.True(t, creds.IsExpired())
+		assert.True(t, creds.isExpired())
 	}
 	{
 		// Expiration = true, because is in the future (but less than the buffer)
 		creds.expiresAt = time.Now().Add(1 * time.Minute)
-		assert.True(t, creds.IsExpired())
+		assert.True(t, creds.isExpired())
 	}
 	{
 		// Expiration = false, because is in the future (but more than the buffer)
 		creds.expiresAt = time.Now().Add(100 * time.Minute)
-		assert.False(t, creds.IsExpired())
+		assert.False(t, creds.isExpired())
 	}
 }
