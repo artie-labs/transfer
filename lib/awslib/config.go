b3143ea4147b5227d35db11b55f0bae89ae37d7e
Robin Tang <rtang.cs@gmail.com>
2025-05-08 14:13:44 -0700

    Update.

--- lib/awslib/config.go
+++ lib/awslib/config.go
@@ -4,12 +4,17 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 )
 
-func NewConfigWithCredentialsAndRegion(ctx context.Context, credentials credentials.StaticCredentialsProvider, region string) aws.Config {
+func NewConfigWithCredentialsAndRegion(credentials credentials.StaticCredentialsProvider, region string) aws.Config {
 	return aws.Config{
 		Region:      region,
 		Credentials: credentials,
 	}
+}
+
+func NewDefaultConfig(ctx context.Context, region string) (aws.Config, error) {
+	return config.LoadDefaultConfig(ctx, config.WithRegion(region))
 }
