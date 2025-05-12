7c73ae460640e8a43678d5e46faad4d2ad76430d
Robin Tang <rtang.cs@gmail.com>
2025-05-12 15:12:22 -0700

    Update.

--- clients/bigquery/dialect/typing.go
+++ clients/bigquery/dialect/typing.go
@@ -73,12 +73,10 @@ func (BigQueryDialect) KindForDataType(rawBqType string, _ string) (typing.KindD
 		return typing.ParseNumeric(parameters)
 	case "bignumeric":
 		if len(parameters) == 0 {
-			fmt.Println("bignumeric default")
 			// BigQuery [BIGNUMERIC] type will default to BIGNUMERIC(76, 38)
 			return typing.NewDecimalDetailsFromTemplate(typing.EDecimal, decimal.NewDetails(76, 38)), nil
 		}
 
-		fmt.Println("bignumeric", parameters)
 		return typing.ParseNumeric(parameters)
 	case "decimal", "float", "float64", "bigdecimal":
 		return typing.Float, nil
