862913a40b44ce86b8af3093b513df5f2e930ffd
Robin Tang <rtang.cs@gmail.com>
2025-06-18 18:46:55 +0200

    Update.

--- lib/heartbeats_test.go
+++ lib/heartbeats_test.go
@@ -7,20 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const oneHundredMillisecond = 100 * time.Millisecond
+
 func TestHeartbeats(t *testing.T) {
 	{
 		// Nothing kicked off because the initial delay is not passed
-		heartbeats := NewHeartbeats(1*time.Second, 1*time.Second, "test", map[string]any{"test": "test"})
+		heartbeats := NewHeartbeats(5*oneHundredMillisecond, oneHundredMillisecond, "test", map[string]any{"test": "test"})
 		heartbeats.test = true
-		cancel := heartbeats.Start()
+		done := heartbeats.Start()
 
-		time.Sleep(300 * time.Millisecond)
-		cancel()
-		assert.Equal(t, 0, heartbeats.ticks)
+		time.Sleep(3 * oneHundredMillisecond)
+		done()
+		assert.Zero(t, heartbeats.ticks)
 	}
 	{
-		// Nothing kicked off because the initial delay is not passed
-		heartbeats := NewHeartbeats(1*time.Second, 1*time.Second, "test", map[string]any{"test": "test"})
-		heartbeats.Start()
+		heartbeats := NewHeartbeats(oneHundredMillisecond, 2*oneHundredMillisecond, "test", map[string]any{"test": "test"})
+		heartbeats.test = true
+		done := heartbeats.Start()
+
+		// Sleep 500 ms + buffer of 25 ms to make sure we hit the second tick
+		time.Sleep(5*oneHundredMillisecond + 25*time.Millisecond)
+		done()
+		assert.Equal(t, 2, heartbeats.ticks)
 	}
 }
