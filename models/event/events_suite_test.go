12ed06fc983284450243dca7ddbbd33fe464b22b
Robin Tang <rtang.cs@gmail.com>
2025-07-03 11:30:58 +0200

    imports.

--- models/event/events_suite_test.go
+++ models/event/events_suite_test.go
@@ -3,15 +3,11 @@ package event
 import (
 	"testing"
 
-	"github.com/artie-labs/transfer/lib/typing/columns"
-
-	"github.com/artie-labs/transfer/lib/config/constants"
-
-	"github.com/artie-labs/transfer/lib/mocks"
-
-	"github.com/artie-labs/transfer/models"
-
 	"github.com/artie-labs/transfer/lib/config"
+	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/mocks"
+	"github.com/artie-labs/transfer/lib/typing/columns"
+	"github.com/artie-labs/transfer/models"
 
 	"github.com/stretchr/testify/suite"
 )
