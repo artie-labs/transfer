364fdcd86c03e50bda523265ff715fcb38c9f37a
Robin Tang <rtang.cs@gmail.com>
2025-07-22 18:11:12 -0700

    WIP.

--- clients/postgres/dialect/dialect.go
+++ clients/postgres/dialect/dialect.go
@@ -12,6 +12,17 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
+const describeTableQuery = `
+SELECT
+    c.column_name,
+    pg_catalog.format_type(a.atttypid, a.atttypmod) as data_type,
+    c.udt_name
+FROM information_schema.columns c
+LEFT JOIN pg_catalog.pg_namespace pn ON pn.nspname = c.table_schema
+LEFT JOIN pg_catalog.pg_class cl ON cl.relname = c.table_name AND cl.relnamespace = pn.oid
+LEFT JOIN pg_catalog.pg_attribute a ON a.attname = c.column_name AND a.attrelid = cl.oid
+WHERE c.table_schema = $1 AND c.table_name = $2;`
+
 type PostgresDialect struct{}
 
 func (PostgresDialect) QuoteIdentifier(identifier string) string {
@@ -54,18 +65,22 @@ func (PostgresDialect) BuildTruncateTableQuery(tableID sql.TableIdentifier) stri
 }
 
 func (PostgresDialect) BuildDedupeQueries(tableID, stagingTableID sql.TableIdentifier, primaryKeys []string, includeArtieUpdatedAt bool) []string {
-	// TODO: To implement
-	return nil
+	// We don't need to dedupe in Postgres.
+	panic("not implemented")
 }
 
 func (PostgresDialect) BuildDedupeTableQuery(tableID sql.TableIdentifier, primaryKeys []string) string {
-	// TODO: To implement
-	return ""
+	// We don't need to dedupe in Postgres.
+	panic("not implemented")
 }
 
 func (PostgresDialect) BuildDescribeTableQuery(tableID sql.TableIdentifier) (string, []any, error) {
-	// TODO: To implement
-	return "", nil, fmt.Errorf("not implemented")
+	pgTableID, err := typing.AssertType[TableIdentifier](tableID)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return describeTableQuery, []any{pgTableID.Schema(), pgTableID.Table()}, nil
 }
 
 func (PostgresDialect) BuildIsNotToastValueExpression(tableAlias constants.TableAlias, column columns.Column) string {
@@ -86,8 +101,7 @@ func (PostgresDialect) BuildDropColumnQuery(tableID sql.TableIdentifier, colName
 }
 
 func (PostgresDialect) BuildMergeQueryIntoStagingTable(tableID sql.TableIdentifier, subQuery string, primaryKeys []columns.Column, additionalEqualityStrings []string, cols []columns.Column) []string {
-	// TODO: To implement
-	return nil
+	panic("not implemented")
 }
 
 func (PostgresDialect) BuildMergeQueries(
