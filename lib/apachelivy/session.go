63464cdf312f17fbd920746f95af84c5219b5468
William Haw <me@williamhaw.com>
2025-07-23 14:45:43 -0700

    [Iceberg] Fix for Livy Dead Session (#1395)
    Co-authored-by: williamhaw <william@artie.com>

--- lib/apachelivy/session.go
+++ lib/apachelivy/session.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net/http"
 	"slices"
+	"strings"
 	"time"
 
 	"github.com/artie-labs/transfer/lib/jitter"
@@ -58,9 +59,21 @@ func (c *Client) newSession(ctx context.Context, kind SessionKind, blockUntilRea
 	}
 
 	for _, session := range sessions.Sessions {
-		if session.Name == c.sessionName {
+		// there can only be one session with this name - Livy enforces this
+		if session.Name == c.sessionName && !session.TerminalState() {
 			c.sessionID = session.ID
 			return nil
+		} else if session.Name == c.sessionName && session.TerminalState() {
+			sleepTime := jitter.Jitter(sleepBaseMs, sleepMaxMs, 0)
+			slog.Warn("Session is in a terminal state, deleting",
+				slog.Int("sessionID", session.ID),
+				slog.String("sessionName", c.sessionName),
+				slog.Duration("sleepTime", sleepTime),
+				slog.String("logs", strings.Join(session.Logs, "\n")),
+			)
+			c.DeleteSession(ctx, session.ID)
+			time.Sleep(sleepTime)
+			return c.newSession(ctx, kind, blockUntilReady)
 		}
 	}
 
