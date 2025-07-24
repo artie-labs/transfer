fcb7c284bbce280a32ec93b458e82a7884e7ca4c
Robin Tang <rtang.cs@gmail.com>
2025-07-24 16:56:06 -0700

    Update.

--- lib/parquetutil/generate_schema.go
+++ lib/parquetutil/generate_schema.go
@@ -1,6 +1,8 @@
 package parquetutil
 
 import (
+	"fmt"
+
 	"github.com/apache/arrow/go/v17/arrow"
 	"github.com/artie-labs/transfer/lib/typing/columns"
 )
@@ -11,7 +13,7 @@ func BuildArrowSchemaFromColumns(columns []columns.Column) (*arrow.Schema, error
 	for _, column := range columns {
 		arrowType, err := column.KindDetails.ToArrowType()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to convert kind details to arrow type: %w", err)
 		}
 
 		fields = append(fields, arrow.Field{
