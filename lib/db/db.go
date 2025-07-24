4aafdbd5a7d418c62930635de6bdb97816935306
Robin Tang <rtang.cs@gmail.com>
2025-07-24 10:00:45 -0700

    [postgres] Loading a temporary table (#1399)
    Signed-off-by: Robin Tang <rtang.cs@gmail.com>

--- lib/db/db.go
+++ lib/db/db.go
@@ -19,6 +19,7 @@ type Store interface {
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+	Conn(ctx context.Context) (*sql.Conn, error)
 	Begin() (*sql.Tx, error)
 	IsRetryableError(err error) bool
 }
