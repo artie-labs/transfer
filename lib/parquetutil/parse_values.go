cf888bfdd5e5260ec5ec8c5a7baa471e11d77823
Robin Tang <rtang.cs@gmail.com>
2025-07-24 17:35:11 -0700

    Update.

--- lib/parquetutil/parse_values.go
+++ lib/parquetutil/parse_values.go
@@ -21,63 +21,63 @@ func ConvertValueForArrowBuilder(builder array.Builder, value any) error {
 		return nil
 	}
 
-	switch b := builder.(type) {
+	switch castedBuilder := builder.(type) {
 	case *array.StringBuilder:
 		castedValue, err := typing.AssertType[string](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to string: %w", err)
 		}
-		b.Append(castedValue)
+		castedBuilder.Append(castedValue)
 	case *array.Int64Builder:
 		castedValue, err := typing.AssertType[int64](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to int64: %w", err)
 		}
-		b.Append(castedValue)
+		castedBuilder.Append(castedValue)
 	case *array.BooleanBuilder:
 		castedValue, err := typing.AssertType[bool](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to boolean: %w", err)
 		}
-		b.Append(castedValue)
+		castedBuilder.Append(castedValue)
 	case *array.Float32Builder:
 		castedValue, err := typing.AssertType[float32](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to float32: %w", err)
 		}
-		b.Append(castedValue)
+		castedBuilder.Append(castedValue)
 	case *array.Time32Builder:
 		castedValue, err := typing.AssertType[int32](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to int32: %w", err)
 		}
-		b.Append(arrow.Time32(castedValue))
+		castedBuilder.Append(arrow.Time32(castedValue))
 	case *array.Date32Builder:
 		castedValue, err := typing.AssertType[int32](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to int32: %w", err)
 		}
-		b.Append(arrow.Date32(castedValue))
+		castedBuilder.Append(arrow.Date32(castedValue))
 	case *array.TimestampBuilder:
 		castedValue, err := typing.AssertType[int64](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to int64: %w", err)
 		}
-		b.Append(arrow.Timestamp(castedValue))
+		castedBuilder.Append(arrow.Timestamp(castedValue))
 	case *array.Decimal128Builder:
 		castedValue, err := typing.AssertType[decimal128.Num](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to decimal128.Num: %w", err)
 		}
-		b.Append(castedValue)
+		castedBuilder.Append(castedValue)
 	case *array.ListBuilder:
 		castedValue, err := typing.AssertType[[]string](value)
 		if err != nil {
 			return fmt.Errorf("failed to cast value to []string: %w", err)
 		}
 
-		b.Append(true)
-		valueBuilder, ok := b.ValueBuilder().(*array.StringBuilder)
+		castedBuilder.Append(true)
+		valueBuilder, ok := castedBuilder.ValueBuilder().(*array.StringBuilder)
 		if !ok {
 			return fmt.Errorf("failed to cast value builder to array.StringBuilder")
 		}
