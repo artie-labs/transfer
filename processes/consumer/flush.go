f03a3c7a10a0d91489dec95743c86265b9f8108c
Robin Tang <rtang.cs@gmail.com>
2025-09-09 10:04:43 -0700

    More.

--- processes/consumer/flush.go
+++ processes/consumer/flush.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/artie-labs/transfer/lib/cdc"
 	"github.com/artie-labs/transfer/lib/config"
 	"github.com/artie-labs/transfer/lib/destination"
 	"github.com/artie-labs/transfer/lib/kafkalib"
@@ -88,16 +87,24 @@ func FlushSingleTopic(ctx context.Context, inMemDB *models.DatabaseData, dest de
 					"reason":   args.Reason,
 				}
 
-				what, err := retry.WithRetriesAndResult(retryCfg, func(_ int, _ error) (string, error) {
+				result, err := retry.WithRetriesAndResult(retryCfg, func(_ int, _ error) (flushResult, error) {
 					slog.Info("Flushing table", slog.String("tableID", table.GetTableID().String()), slog.String("reason", args.Reason))
-					return flush(ctx, dest, table, action, inMemDB.ClearTableConfig, consumer)
+					return flush(ctx, dest, table, action, consumer)
 				})
 
 				if err != nil {
 					slog.Error(fmt.Sprintf("Failed to %s", action), slog.Any("err", err), slog.String("tableID", table.GetTableID().String()))
 				}
 
-				tags["what"] = what
+				if result.CommitOffset {
+					consumer.CommitMessage(ctx, result.Message)
+					inMemDB.ClearTableConfig(table.GetTableID())
+				} else {
+					slog.Info(fmt.Sprintf("%s success, not committing offset yet", stringutil.CapitalizeFirstLetter(action)), slog.String("tableID", table.GetTableID().String()))
+
+				}
+
+				tags["what"] = result.What
 				metricsClient.Timing("flush", time.Since(start), tags)
 			}(table)
 		}
@@ -109,37 +116,32 @@ func FlushSingleTopic(ctx context.Context, inMemDB *models.DatabaseData, dest de
 	return err
 }
 
-func flush(ctx context.Context, dest destination.Baseline, _tableData *models.TableData, action string, clearTableConfig func(cdc.TableID), consumer *kafkalib.ConsumerProvider) (string, error) {
+type flushResult struct {
+	What         string
+	CommitOffset bool
+}
+
+func flush(ctx context.Context, dest destination.Baseline, _tableData *models.TableData, action string, consumer *kafkalib.ConsumerProvider) (flushResult, error) {
 	// This is added so that we have a new temporary table suffix for each merge / append.
 	_tableData.ResetTempTableSuffix()
 
 	// Merge or Append depending on the mode.
-	var err error
-	commitTransaction := true
-	if _tableData.Mode() == config.History {
-		err = dest.Append(ctx, _tableData.TableData, false)
-	} else {
-		commitTransaction, err = dest.Merge(ctx, _tableData.TableData)
-	}
-
-	if err != nil {
-		return "merge_fail", fmt.Errorf("failed to flush %q: %w", _tableData.GetTableID().String(), err)
-	}
-
-	if commitTransaction {
-		for _, msg := range _tableData.PartitionsToLastMessage {
-			if err = consumer.CommitMessage(ctx, msg.GetMessage()); err != nil {
-				return "commit_fail", fmt.Errorf("failed to commit kafka offset: %w", err)
-			}
-
-			slog.Info("Successfully committed Kafka offset", slog.String("topic", msg.Topic()), slog.Int("partition", msg.Partition()), slog.Int64("offset", msg.Offset()))
+	switch _tableData.Mode() {
+	case config.History:
+		err := dest.Append(ctx, _tableData.TableData, false)
+		if err != nil {
+			return flushResult{What: "merge_fail"}, fmt.Errorf("failed to append: %w", err)
 		}
 
-		slog.Info(fmt.Sprintf("%s success, clearing memory...", stringutil.CapitalizeFirstLetter(action)), slog.String("tableID", _tableData.GetTableID().String()))
-		clearTableConfig(_tableData.GetTableID())
-	} else {
-		slog.Info(fmt.Sprintf("%s success, not committing offset yet", stringutil.CapitalizeFirstLetter(action)), slog.String("tableID", _tableData.GetTableID().String()))
+		return flushResult{What: "success", CommitOffset: true}, nil
+	case config.Replication:
+		commitTransaction, err := dest.Merge(ctx, _tableData.TableData)
+		if err != nil {
+			return flushResult{What: "merge_fail"}, fmt.Errorf("failed to merge: %w", err)
+		}
+
+		return flushResult{What: "success", CommitOffset: commitTransaction}, nil
 	}
 
-	return "success", nil
+	return flushResult{}, fmt.Errorf("invalid mode: %q", _tableData.Mode())
 }
