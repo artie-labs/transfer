9d94999f9c8bcbe53dbc09a3e665ae903818a808
Robin Tang <rtang.cs@gmail.com>
2025-10-15 20:41:42 -0700

    update.

--- lib/sql/rows.go
+++ lib/sql/rows.go
@@ -3,6 +3,7 @@ package sql
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 )
 
 func RowsToObjects(rows *sql.Rows) ([]map[string]any, error) {
@@ -28,6 +29,41 @@ func RowsToObjects(rows *sql.Rows) ([]map[string]any, error) {
 		object := make(map[string]any)
 		for i, column := range columns {
 			object[column] = row[i]
+		}
+
+		objects = append(objects, object)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed to iterate over rows: %w", err)
+	}
+
+	return objects, nil
+}
+
+func RowsToObjectsLowercase(rows *sql.Rows) ([]map[string]any, error) {
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	var objects []map[string]any
+	for rows.Next() {
+		row := make([]any, len(columns))
+		rowPointers := make([]any, len(columns))
+		for i := range row {
+			rowPointers[i] = &row[i]
+		}
+
+		if err = rows.Scan(rowPointers...); err != nil {
+			return nil, err
+		}
+
+		object := make(map[string]any)
+		for i, column := range columns {
+			object[strings.ToLower(column)] = row[i]
 		}
 
 		objects = append(objects, object)
