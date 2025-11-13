39d04f587918249d7576121ae128465cf8cdf121
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-11-12 17:05:44 -0800

    Add event tracking CDC format (#1521)

---
+++ lib/cdc/eventtracking/event.go
@@ -0,0 +1,85 @@
+package eventtracking
+
+import (
+	"maps"
+	"time"
+
+	"github.com/artie-labs/transfer/lib/config/constants"
+	"github.com/artie-labs/transfer/lib/kafkalib"
+	"github.com/artie-labs/transfer/lib/typing"
+	"github.com/artie-labs/transfer/lib/typing/columns"
+)
+
+// EventPayload implements the [cdc.Event] interface
+type EventPayload struct {
+	Event       string         `json:"event"`
+	Timestamp   time.Time      `json:"timestamp"`
+	MessageID   string         `json:"messageID"`
+	Properties  map[string]any `json:"properties"`
+	ExtraFields map[string]any `json:"extraFields"`
+}
+
+func (e *EventPayload) GetExecutionTime() time.Time {
+	return e.Timestamp.UTC()
+}
+
+func (e *EventPayload) Operation() constants.Operation {
+	return constants.Create
+}
+
+func (e *EventPayload) DeletePayload() bool {
+	return false
+}
+
+func (e *EventPayload) GetTableName() string {
+	return e.Event
+}
+
+func (e *EventPayload) GetFullTableName() string {
+	return e.GetTableName()
+}
+
+func (e *EventPayload) GetSourceMetadata() (string, error) {
+	return "{}", nil
+}
+
+func (e *EventPayload) GetData(tc kafkalib.TopicConfig) (map[string]any, error) {
+	// The table data consists of the properties, additional top-level fields, and the ID & timestamp.
+	retMap := make(map[string]any)
+	maps.Copy(retMap, e.Properties)
+	maps.Copy(retMap, e.ExtraFields)
+	retMap["id"] = e.MessageID
+	retMap["timestamp"] = e.Timestamp
+
+	// Add Artie-specific metadata columns
+	retMap[constants.DeleteColumnMarker] = false
+	retMap[constants.OnlySetDeleteColumnMarker] = false
+	if tc.IncludeArtieUpdatedAt {
+		retMap[constants.UpdateColumnMarker] = time.Now().UTC()
+	}
+	if tc.IncludeDatabaseUpdatedAt {
+		retMap[constants.DatabaseUpdatedColumnMarker] = e.GetExecutionTime().UTC()
+	}
+
+	return retMap, nil
+}
+
+func (e *EventPayload) GetOptionalSchema() (map[string]typing.KindDetails, error) {
+	// Event tracking format doesn't have a schema
+	return nil, nil
+}
+
+func (e *EventPayload) GetColumns(reservedColumns map[string]bool) (*columns.Columns, error) {
+	var cols columns.Columns
+	for k := range e.Properties {
+		cols.AddColumn(columns.NewColumn(columns.EscapeName(k, reservedColumns), typing.Invalid))
+	}
+	for k := range e.ExtraFields {
+		cols.AddColumn(columns.NewColumn(columns.EscapeName(k, reservedColumns), typing.Invalid))
+	}
+
+	cols.AddColumn(columns.NewColumn(columns.EscapeName("id", reservedColumns), typing.Invalid))
+	cols.AddColumn(columns.NewColumn(columns.EscapeName("timestamp", reservedColumns), typing.Invalid))
+
+	return &cols, nil
+}
--- lib/cdc/format/format.go
+++ lib/cdc/format/format.go
@@ -4,13 +4,14 @@ import (
 	"log/slog"
 
 	"github.com/artie-labs/transfer/lib/cdc"
+	"github.com/artie-labs/transfer/lib/cdc/eventtracking"
 	"github.com/artie-labs/transfer/lib/cdc/mongo"
 	"github.com/artie-labs/transfer/lib/cdc/relational"
 	"github.com/artie-labs/transfer/lib/logger"
 )
 
 func GetFormatParser(label, topic string) cdc.Format {
-	for _, validFormat := range []cdc.Format{relational.Debezium{}, mongo.Debezium{}} {
+	for _, validFormat := range []cdc.Format{relational.Debezium{}, mongo.Debezium{}, eventtracking.Format{}} {
 		for _, fmtLabel := range validFormat.Labels() {
 			if fmtLabel == label {
 				slog.Info("Loaded CDC Format parser...",
