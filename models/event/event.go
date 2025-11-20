e66ea5f3b31a2218cc5f8cc149717834fbb03c5d
Robin Tang <rtang.cs@gmail.com>
2025-11-20 14:11:04 -0800

    Update.

--- models/event/event.go
+++ models/event/event.go
@@ -84,46 +84,6 @@ func transformData(data map[string]any, tc kafkalib.TopicConfig) map[string]any 
 	return data
 }
 
-func buildFilteredColumns(event cdc.Event, tc kafkalib.TopicConfig, reservedColumns map[string]bool) (*columns.Columns, error) {
-	cols, err := event.GetColumns(reservedColumns)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, col := range tc.ColumnsToExclude {
-		cols.DeleteColumn(col)
-	}
-
-	if len(tc.ColumnsToInclude) > 0 {
-		var filteredColumns columns.Columns
-		for _, col := range tc.ColumnsToInclude {
-			if existingColumn, ok := cols.GetColumn(col); ok {
-				filteredColumns.AddColumn(existingColumn)
-			}
-		}
-
-		for _, col := range constants.ArtieColumns {
-			if existingColumn, ok := cols.GetColumn(col); ok {
-				filteredColumns.AddColumn(existingColumn)
-			}
-		}
-
-		// If columns to include is specified, we should always include static columns.
-		for _, col := range tc.StaticColumns {
-			filteredColumns.AddColumn(columns.NewColumn(col.Name, typing.String))
-		}
-
-		return &filteredColumns, nil
-	}
-
-	// Include static columns
-	for _, col := range tc.StaticColumns {
-		cols.AddColumn(columns.NewColumn(col.Name, typing.String))
-	}
-
-	return cols, nil
-}
-
 func buildPrimaryKeys(tc kafkalib.TopicConfig, pkMap map[string]any, reservedColumns map[string]bool) []string {
 	var pks []string
 	if len(tc.PrimaryKeysOverride) > 0 {
@@ -151,25 +111,23 @@ func buildPrimaryKeys(tc kafkalib.TopicConfig, pkMap map[string]any, reservedCol
 
 func ToMemoryEvent(ctx context.Context, dest destination.Baseline, event cdc.Event, pkMap map[string]any, tc kafkalib.TopicConfig, cfgMode config.Mode) (Event, error) {
 	reservedColumns := destination.BuildReservedColumnNames(dest)
-	cols, err := buildFilteredColumns(event, tc, reservedColumns)
+	cols, err := buildColumns(event, tc, reservedColumns)
 	if err != nil {
 		return Event{}, fmt.Errorf("failed to build filtered columns: %w", err)
 	}
 
 	pks := buildPrimaryKeys(tc, pkMap, reservedColumns)
 
-	if cols != nil {
-		// All keys in pks are already escaped, so don't escape again
-		for _, pk := range pks {
-			err = cols.UpsertColumn(
-				pk,
-				columns.UpsertColumnArg{
-					PrimaryKey: typing.ToPtr(true),
-				},
-			)
-			if err != nil {
-				return Event{}, fmt.Errorf("failed to upsert column: %w", err)
-			}
+	// All keys in pks are already escaped, so don't escape again
+	for _, pk := range pks {
+		err = cols.UpsertColumn(
+			pk,
+			columns.UpsertColumnArg{
+				PrimaryKey: typing.ToPtr(true),
+			},
+		)
+		if err != nil {
+			return Event{}, fmt.Errorf("failed to upsert column: %w", err)
 		}
 	}
 
@@ -253,7 +211,7 @@ func ToMemoryEvent(ctx context.Context, dest destination.Baseline, event cdc.Eve
 		table:          tblName,
 		tableID:        cdc.NewTableID(tc.Schema, tblName),
 		optionalSchema: optionalSchema,
-		columns:        cols,
+		columns:        &cols,
 		data:           transformData(evtData, tc),
 		deleted:        event.DeletePayload(),
 	}, nil
