47805dde2e95e43ee2315a8de2ae23cf276b2805
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-12-18 07:59:36 -0800

    Handle float ts with fractional ms

--- lib/typing/parse_timestamp.go
+++ lib/typing/parse_timestamp.go
@@ -3,8 +3,6 @@ package typing
 import (
 	"fmt"
 	"time"
-
-	"github.com/artie-labs/transfer/lib/typing/converters/primitives"
 )
 
 // ParseTimeExactMatch will return an error if it was not an exact match.
@@ -97,12 +95,12 @@ func ParseTimestampTZFromAny(val any) (time.Time, error) {
 	case string:
 		return parseTimestampTZ(convertedVal)
 	case float64:
-		value, err := primitives.Int64Converter{}.Convert(val)
-		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to convert float64 to int64: %w", err)
-		}
-
-		return parseTimestampTZInt64(value)
+		// Handle float64 timestamps that may include fractional milliseconds.
+		msInt := int64(convertedVal)
+		fracMs := convertedVal - float64(msInt)
+		// Convert fractional milliseconds to nanoseconds (1 ms = 1,000,000 ns)
+		additionalNanos := int64(fracMs * 1e6)
+		return time.UnixMilli(msInt).Add(time.Duration(additionalNanos) * time.Nanosecond), nil
 	case int64:
 		return parseTimestampTZInt64(convertedVal)
 	default:
