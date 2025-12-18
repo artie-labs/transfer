01fc6bd22f7009cab2bc42cda9efc05369d97cb6
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-12-17 18:10:00 -0800

    Pull in bad numeric type changes

--- lib/typing/converters/string_converter.go
+++ lib/typing/converters/string_converter.go
@@ -13,6 +13,7 @@ import (
 	"github.com/artie-labs/transfer/lib/stringutil"
 	"github.com/artie-labs/transfer/lib/typing"
 	"github.com/artie-labs/transfer/lib/typing/decimal"
+	"github.com/cockroachdb/apd/v3"
 )
 
 type Converter interface {
@@ -252,9 +253,14 @@ func (FloatConverter) Convert(value any) (string, error) {
 	case *decimal.Decimal:
 		return parsedVal.String(), nil
 	case string:
+		// If it's a string, verify it can be parsed as a float
+		if _, err := strconv.ParseFloat(parsedVal, 64); err != nil {
+			return "", typing.NewParseError(fmt.Sprintf("unexpected value: '%v', type: %T", value, value), typing.UnexpectedValue)
+		}
+
 		return parsedVal, nil
 	default:
-		return "", fmt.Errorf("unexpected value: '%v', type: %T", value, value)
+		return "", typing.NewParseError(fmt.Sprintf("unexpected value: '%v', type: %T", value, value), typing.UnexpectedValue)
 	}
 }
 
@@ -269,11 +275,14 @@ func (DecimalConverter) Convert(value any) (string, error) {
 	case int, int8, int16, int32, int64:
 		return fmt.Sprint(castedColVal), nil
 	case string:
+		if _, _, err := apd.NewFromString(castedColVal); err != nil {
+			return "", typing.NewParseError(fmt.Sprintf("unexpected value: '%v', type: %T", value, value), typing.UnexpectedValue)
+		}
 		return castedColVal, nil
 	case *decimal.Decimal:
 		return castedColVal.String(), nil
 	default:
-		return "", fmt.Errorf("unexpected value: '%v' type: %T", value, value)
+		return "", typing.NewParseError(fmt.Sprintf("unexpected value: '%v', type: %T", value, value), typing.UnexpectedValue)
 	}
 }
 
