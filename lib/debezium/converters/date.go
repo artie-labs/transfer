378a04906dd52efbc0641980601ba96b777a0130
Robin Tang <rtang.cs@gmail.com>
2026-01-04 11:35:48 -0800

    Update.

--- lib/debezium/converters/date.go
+++ lib/debezium/converters/date.go
@@ -15,17 +15,23 @@ func (d Date) ToKindDetails() typing.KindDetails {
 }
 
 func (d Date) Convert(value any) (any, error) {
-	valueInt64, ok := value.(int64)
-	if !ok {
-		return nil, fmt.Errorf("expected int64 got '%v' with type %T", value, value)
-	}
+	switch castedValue := value.(type) {
+	case int64:
+		date := time.UnixMilli(0).In(time.UTC).AddDate(0, 0, int(castedValue))
+		if date.Year() > 9999 {
+			slog.Warn("Date exceeds 9999 year, setting this to null to avoid encoding errors", slog.Int("year", date.Year()))
+			return nil, nil
+		}
 
-	date := time.UnixMilli(0).In(time.UTC).AddDate(0, 0, int(valueInt64))
-	if date.Year() > 9999 {
-		slog.Warn("Date exceeds 9999 year, setting this to null to avoid encoding errors", slog.Int("year", date.Year()))
-		return nil, nil
-	}
+		// Represents the number of days since the epoch.
+		return date.Format(time.DateOnly), nil
+	case string:
+		if _, err := time.Parse(time.DateOnly, castedValue); err != nil {
+			return nil, fmt.Errorf("failed to parse date: %w", err)
+		}
 
-	// Represents the number of days since the epoch.
-	return date.Format(time.DateOnly), nil
+		return castedValue, nil
+	default:
+		return nil, fmt.Errorf("expected int64 or string got '%v' with type %T", value, value)
+	}
 }
