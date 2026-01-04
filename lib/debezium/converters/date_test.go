4e5b0039cde84f48a4b01f51277b34116642fc60
Robin Tang <rtang.cs@gmail.com>
2026-01-04 11:39:03 -0800

    Fix tests.

--- lib/debezium/converters/date_test.go
+++ lib/debezium/converters/date_test.go
@@ -9,8 +9,19 @@ import (
 func TestDate_Convert(t *testing.T) {
 	{
 		// Invalid data type
+		_, err := Date{}.Convert(false)
+		assert.ErrorContains(t, err, "expected int64 or string got 'false' with type bool")
+	}
+	{
+		// Invalid string date
 		_, err := Date{}.Convert("invalid")
-		assert.ErrorContains(t, err, "expected int64 got 'invalid' with type string")
+		assert.ErrorContains(t, err, "failed to parse date")
+	}
+	{
+		// Valid string date
+		val, err := Date{}.Convert("2023-02-13")
+		assert.NoError(t, err)
+		assert.Equal(t, "2023-02-13", val)
 	}
 	{
 		val, err := Date{}.Convert(int64(19401))
