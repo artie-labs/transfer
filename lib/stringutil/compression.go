4e86ae49dc63168ffa51eb7b9d79fd1c1bcb209d
Robin Tang <rtang.cs@gmail.com>
2026-03-09 10:15:27 -0700

    Adding compression.

---
+++ lib/stringutil/compression.go
@@ -0,0 +1,31 @@
+package stringutil
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+)
+
+func GZipCompress(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	if _, err := w.Write(data); err != nil {
+		return nil, err
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func GZipDecompress(data []byte) ([]byte, error) {
+	r, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	defer r.Close()
+	return io.ReadAll(r)
+}
