3032f6e85b0ec5111ae5de16a7e9269272e9c731
Robin Tang <rtang.cs@gmail.com>
2026-03-29 22:36:48 -0700

    WIP.

--- lib/config/destination_types.go
+++ lib/config/destination_types.go
@@ -46,11 +46,12 @@ type Postgres struct {
 }
 
 type MSSQL struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+	Host           string `yaml:"host"`
+	Port           int    `yaml:"port"`
+	Username       string `yaml:"username"`
+	Password       string `yaml:"password"`
+	Database       string `yaml:"database"`
+	ReadOnlyIntent bool   `yaml:"readOnlyIntent,omitempty"`
 }
 
 type MySQL struct {
