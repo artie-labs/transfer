eb458df458edd3fdd05b7325319db947693729f3
Robin Tang <rtang.cs@gmail.com>
2026-04-16 12:43:49 -0700

    Update.

--- lib/system/shutdown.go
+++ lib/system/shutdown.go
@@ -13,6 +13,7 @@ func ShutdownHook(cleanUpHandlers func(), cancel context.CancelFunc) {
 	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
 		sig := <-sigCh
+		signal.Stop(sigCh) // allow a second signal to terminate if cleanup blocks
 		slog.Info("Received shutdown signal, initiating graceful shutdown...", slog.String("signal", sig.String()))
 
 		cleanUpHandlers()
