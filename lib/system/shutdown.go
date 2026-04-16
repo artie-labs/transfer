3427e1830952bfe2cc7189002826e483ab5ba090
Robin Tang <rtang.cs@gmail.com>
2026-04-16 12:33:10 -0700

    Update.

---
+++ lib/system/shutdown.go
@@ -0,0 +1,20 @@
+package system
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func ShutdownHook(logger *slog.Logger, cleanUpHandlers func(), cancel context.CancelFunc) {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigCh
+		slog.Info("Received shutdown signal, initiating graceful shutdown...", slog.String("signal", sig.String()))
+		cleanUpHandlers()
+		cancel()
+	}()
+}
