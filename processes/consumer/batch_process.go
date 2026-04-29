827b3e61a3f2b82873898e3e34fdeecd694edbb7
Joshua Hight <josh@artie.com>
2026-04-29 13:41:39 -0700

    fix list handling

--- processes/consumer/batch_process.go
+++ processes/consumer/batch_process.go
@@ -41,11 +41,7 @@ func (p batchProcessArgs) process(ctx context.Context, cfg config.Config, inMemD
 		metricsClient.Timing("process.batch", time.Since(st), tags)
 	}()
 
-	topicConfig, ok := p.TopicToConfigFormatMap.GetTopicFmt(p.Msgs[0].Topic())
-	if !ok {
-		tags["what"] = "failed_topic_lookup"
-		return fmt.Errorf("failed to get topic name: %q", p.Msgs[0].Topic())
-	}
+	var topicConfigPtr *TopicConfigFormatter = nil
 
 	for _, msg := range p.Msgs {
 		// handle tombstone
@@ -53,23 +49,32 @@ func (p batchProcessArgs) process(ctx context.Context, cfg config.Config, inMemD
 			continue
 		}
 
-		tags["database"] = topicConfig.tc.Database
-		tags["schema"] = topicConfig.tc.Schema
+		if topicConfigPtr == nil {
+			topicConfig, ok := p.TopicToConfigFormatMap.GetTopicFmt(p.Msgs[0].Topic())
+			if !ok {
+				tags["what"] = "failed_topic_lookup"
+				return fmt.Errorf("failed to get topic name: %q", p.Msgs[0].Topic())
+			}
+			topicConfigPtr = &topicConfig
+		}
 
-		pkMap, err := topicConfig.buildPKMap(msg.Key(), reservedColumns)
+		tags["database"] = topicConfigPtr.tc.Database
+		tags["schema"] = topicConfigPtr.tc.Schema
+
+		pkMap, err := topicConfigPtr.buildPKMap(msg.Key(), reservedColumns)
 		if err != nil {
 			tags["what"] = "marshall_pk_err"
 			return fmt.Errorf("cannot unmarshal key %q: %w", string(msg.Key()), err)
 		}
 
-		_event, err := topicConfig.GetEventFromBytes(msg.Value())
+		_event, err := topicConfigPtr.GetEventFromBytes(msg.Value())
 		if err != nil {
 			tags["what"] = "marshal_value_err"
 			return fmt.Errorf("cannot unmarshal event: %w", err)
 		}
 
 		tags["op"] = string(_event.Operation())
-		evt, err := event.ToMemoryEvent(ctx, dest, _event, pkMap, topicConfig.tc, cfg.Mode, cfg.SharedDestinationSettings, p.EncryptionKey)
+		evt, err := event.ToMemoryEvent(ctx, dest, _event, pkMap, topicConfigPtr.tc, cfg.Mode, cfg.SharedDestinationSettings, p.EncryptionKey)
 		if err != nil {
 			tags["what"] = "to_mem_event_err"
 			return fmt.Errorf("cannot convert to memory event: %w", err)
@@ -77,7 +82,7 @@ func (p batchProcessArgs) process(ctx context.Context, cfg config.Config, inMemD
 
 		// Table name is only available after event has been cast
 		tags["table"] = evt.GetTable()
-		if topicConfig.ShouldSkip(string(_event.Operation())) {
+		if topicConfigPtr.ShouldSkip(string(_event.Operation())) {
 			continue
 		}
 
@@ -85,16 +90,20 @@ func (p batchProcessArgs) process(ctx context.Context, cfg config.Config, inMemD
 			evt.EmitExecutionTimeLag(metricsClient)
 		}
 
-		_, _, err = evt.Save(cfg, inMemDB, topicConfig.tc, reservedColumns)
+		_, _, err = evt.Save(cfg, inMemDB, topicConfigPtr.tc, reservedColumns)
 		if err != nil {
 			tags["what"] = "save_fail"
 			return fmt.Errorf("event failed to save: %w", err)
 		}
 	}
 
-	err := FlushSingleTopic(ctx, inMemDB, dest, metricsClient, p.WhClient, Args{Reason: "flushOnReceive", ReportDBExecutionTime: cfg.Reporting.EmitDBExecutionTime}, topicConfig.tc.Topic, false)
-	if err != nil {
-		tags["what"] = "flush_fail"
+	// if topicConfigPtr is nil it means there's nothing to flush anyways
+	if topicConfigPtr != nil {
+		err := FlushSingleTopic(ctx, inMemDB, dest, metricsClient, p.WhClient, Args{Reason: "flushOnReceive", ReportDBExecutionTime: cfg.Reporting.EmitDBExecutionTime}, topicConfigPtr.tc.Topic, false)
+		if err != nil {
+			tags["what"] = "flush_fail"
+		}
+		return err
 	}
-	return err
+	return nil
 }
